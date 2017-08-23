d235a4efdde7274df8d64f69ce38ca43ecdcf6c4
yhira <info@nelog.jp>
2017-08-23 23:04:17 +0900

    「次のページ」ページネーションの作成

--- tmp/pagenation.php
+++ tmp/pagenation.php
@@ -1,4 +1,36 @@
-<!--ページャー-->
+
+<?php
+//現在のページ番号
+global $paged;
+if(empty($paged)) $paged = 1;
+
+//ページ情報の取得
+global $wp_query;
+//全ページ数
+$pages = $wp_query->max_num_pages;
+if(!$pages){
+  $pages = 1;
+}
+
+if(1 != $pages) {
+  //次のページ番号
+  if ( $pages == $paged ) {
+    $next_page_num = $paged;
+  } else {
+    $next_page_num = $paged + 1;
+  }
+  // var_dump($pages);
+  // var_dump($paged);
+  // var_dump($next_page_num);
+  //現在のページ番号が全ページ数よりも少ないときは「次のページ」タグを出力
+  if ( $paged < $pages ) {
+    echo '<div class="pagenation-next"><a href="'.get_pagenum_link($next_page_num).'" class="pagenation-next-link">'.__( '次のページ', THEME_NAME ).'</a></div>';
+  }
+
+}
+?>
+
+
 <div class="pagenation">
   <?php global $wp_rewrite;
   $paginate_base = get_pagenum_link(1);
