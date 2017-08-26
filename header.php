f15b2b7e752e03386b0d579ddfa385643d8f74ed
yhira <info@nelog.jp>
2017-08-26 20:26:39 +0900

    グローバルメニューある程度完成

--- header.php
+++ header.php
@@ -29,31 +29,13 @@
       <header id="header" class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
         <div id="header-in" class="header-in wrap cf">
-          <div class="header-top"><span class="tagline"><?php bloginfo('description') ?></span></div>
+          <div class="header-top"><div class="tagline"><?php bloginfo('description') ?></div></div>
 
           <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
           <div id="logo" class="logo" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" class="site-name"><?php bloginfo('name'); ?></a></div>
 
           <?php // if you'd like to use the site description you can un-comment it below ?>
           <?php // bloginfo('description'); ?>
-
-
-          <nav id="navi" class="navi" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-            <?php wp_nav_menu(array(
-                       'container' => false,                           // remove nav container
-                       'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-                       'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-                       'menu_class' => 'nav top-nav cf',               // adding custom nav class
-                       'theme_location' => 'main-nav',                 // where it's located in the theme
-                       'before' => '',                                 // before the menu
-                             'after' => '',                                  // after the menu
-                             'link_before' => '',                            // before each link
-                             'link_after' => '',                             // after each link
-                             'depth' => 0,                                   // limit the depth of the nav
-                       'fallback_cb' => ''                             // fallback function (if there is one)
-            )); ?>
-
-          </nav>
 
         </div>
 
--- tmp/navi.php
+++ tmp/navi.php
@@ -1,7 +1,12 @@
 <!-- Navigation -->
 <nav id="navi" class="navi cf" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 	<div id="navi-in" class="navi-in wrap cf">
-    <?php wp_nav_menu( array ( 'theme_location' => 'header-navi' ) ); ?>
+    <?php wp_nav_menu(
+      array (
+        'theme_location' => 'header-navi',
+        //'menu_class' => 'menu' ,
+      )
+    ); ?>
   </div><!-- /#navi-in -->
 </nav>
 <!-- /Navigation -->
