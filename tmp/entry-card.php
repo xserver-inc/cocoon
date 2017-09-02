913a9a6d12e69d8885deacf4e05cffa4e73c6801
yhira <info@nelog.jp>
2017-09-02 16:01:52 +0900

    エントリーカードの日付のスタイリング

--- tmp/entry-card.php
+++ tmp/entry-card.php
@@ -1,21 +1,21 @@
 <a href="<?php the_permalink(); ?>" class="entry-card-wrap a-wrap cf" title="<?php the_title() ?>">
-  <article id="post-<?php the_ID(); ?>" class="post-<?php the_ID(); ?> entry-card cf">
-    <figure class="entry-card-thumb">
+  <article id="post-<?php the_ID(); ?>" class="post-<?php the_ID(); ?> entry-card e-card cf">
+    <figure class="entry-card-thumb card-thumb">
       <?php if ( has_post_thumbnail() ): // サムネイルを持っているとき?>
-        <?php the_post_thumbnail('thumb320' , array('class' => 'entry-card-thumb-image', 'alt' => '') ); ?>
+        <?php the_post_thumbnail('thumb320' , array('class' => 'entry-card-thumb-image card-thumb-image', 'alt' => '') ); ?>
       <?php else: // サムネイルを持っていないとき ?>
         <img src="<?php echo get_template_directory_uri(); ?>/images/no-image-320.png" alt="NO IMAGE" class="entry-card-thumb-image no-image list-no-image" />
       <?php endif; ?>
       <?php the_nolink_category(); //カテゴリラベルの取得 ?>
     </figure><!-- /.entry-card-thumb -->
 
-    <div class="entry-card-content">
-      <h2 class="entry-card-title" itemprop="headline"><?php the_title() ?></h2>
-      <div class="entry-card-snippet">
+    <div class="entry-card-content card-content">
+      <h2 class="entry-card-title card-title" itemprop="headline"><?php the_title() ?></h2>
+      <div class="entry-card-snippet card-snippet">
         <?php echo get_content_excerpt( get_the_content(''), 100 ); //カスタマイズで指定した文字の長さだけ本文抜粋?>
       </div>
-      <div class="entry-card-meta">
-        <div class="entry-card-day"><span class="published"><?php the_time('Y/m/d'); ?></span></div>
+      <div class="entry-card-meta card-meta">
+        <div class="entry-card-day"><span class="published"><?php the_time('Y.m.d'); ?></span></div>
         <div class="entry-card-categorys"><?php the_nolink_categories() ?></div>
       </div>
     </div><!-- /.entry-card-content -->
--- tmp/related-entry-card.php
+++ tmp/related-entry-card.php
@@ -1,22 +1,29 @@
 <a href="<?php the_permalink(); ?>" class="related-entry-card-wrap a-wrap cf" title="<?php the_title() ?>">
-<article class="related-entry-card cf">
+<article class="related-entry-card e-card cf">
 
-  <figure class="related-entry-card-thumb">
+  <figure class="related-entry-card-thumb card-thumb">
     <?php if ( has_post_thumbnail() ): // サムネイルを持っているとき ?>
-    <?php echo get_the_post_thumbnail($post->ID, array(160, 90), array('class' => 'related-entry-thumb-image', 'alt' => '') ); //サムネイルを呼び出す?>
+    <?php echo get_the_post_thumbnail($post->ID, array(160, 90), array('class' => 'related-entry-card-thumb-image card-thumb-image', 'alt' => '') ); //サムネイルを呼び出す?>
     <?php else: // サムネイルを持っていないとき ?>
     <img src="<?php echo get_template_directory_uri(); ?>/images/no-image-320.png" alt="NO IMAGE" class="no-image related-entry-card-no-image" width="160" height="90" />
     <?php endif; ?>
     <?php the_nolink_category(); //カテゴリラベルの取得 ?>
   </figure><!-- /.related-entry-thumb -->
 
-  <div class="related-entry-card-content">
-    <h3 class="related-entry-card-title">
+  <div class="related-entry-card-content card-content">
+    <h3 class="related-entry-card-title card-title">
       <?php the_title(); //記事のタイトル?>
     </h3>
-    <div class="related-entry-card-snippet">
+    <div class="related-entry-card-snippet card-snippet">
    <?php echo get_content_excerpt( $post->post_content, 100 ); //カスタマイズで指定した文字の長さだけ本文抜粋?></div>
+
+    <div class="related-entry-card-meta card-meta">
+      <div class="related-entry-card-day"><span class="published"><?php the_time('Y.m.d'); ?></span></div>
+    </div>
+
   </div><!-- /.related-entry-card-content -->
+
+
 
 </article><!-- /.related-entry-card -->
 </a><!-- /.related-entry-card-wrap -->
