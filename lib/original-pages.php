39da55de4a0e3e03b091d47d2d2976b5b987b5b0
yhira <info@nelog.jp>
2017-09-07 14:21:20 +0900

    オリジナル広告設定ページのリファクタリング

--- lib/original-pages.php
+++ lib/original-pages.php
@@ -5,17 +5,21 @@ define('HIDDEN_FIELD_NAME', strtolower(THEME_NAME).'_submit_hidden');
 
 //広告を全て表示するか
 define('AD_ADS_VISIBLE_NAME', 'ads_visible');
-$ads_visible_val = get_option(AD_ADS_VISIBLE_NAME);
+function is_ads_visible(){
+  return get_option(AD_ADS_VISIBLE_NAME);
+}
+
 //広告コード
 define('AD_AD_CODE_NAME', 'ad_code');
-$ad_code_val = stripslashes_deep(get_option(AD_AD_CODE_NAME));
-//var_dump(htmlspecialchars($ad_code_val));
+function get_ad_code(){
+  return stripslashes_deep(get_option(AD_AD_CODE_NAME));
+}
 
 
 // ユーザーが何か情報を POST したかどうかを確認
 // POST していれば、隠しフィールドに 'Y' が設定されている
 if( isset($_POST[HIDDEN_FIELD_NAME]) &&
-    $_POST[HIDDEN_FIELD_NAME] == 'Y' ) {
+    $_POST[HIDDEN_FIELD_NAME] == 'Y' ):
 
     //広告表示設定
     $ads_visible_val = isset($_POST[AD_ADS_VISIBLE_NAME]) ? $_POST[AD_ADS_VISIBLE_NAME] : null;
@@ -26,44 +30,44 @@ if( isset($_POST[HIDDEN_FIELD_NAME]) &&
 
 //画面に「設定は保存されました」メッセージを表示
 ?>
-<div class="updated"><p><strong><?php _e('設定は保存されました。', THEME_NAME ); ?></strong></p></div>
+<div class="updated">
+  <p>
+    <strong>
+      <?php _e('設定は保存されました。', THEME_NAME ); ?>
+    </strong>
+  </p>
+</div>
 <?php
-}
+endif;
 ?>
 <div class="wrap">
 <h1><?php _e( SETTING_NAME_TOP, THEME_NAME ) ?></h1>
 <?php var_dump($_POST) ?>
 <form name="form1" method="post" action="" class="admin-settings">
+
+<!-- タブ機能の実装 -->
 <div id="tabs">
-<ul>
-  <li class="ads"><?php _e( '広告', THEME_NAME ) ?></li>
-  <li><?php _e( 'タイトル', THEME_NAME ) ?></li>
-  <li><?php _e( 'SEO', THEME_NAME ) ?></li>
-</ul>
+  <ul>
+    <li class="ads"><?php _e( '広告', THEME_NAME ) ?></li>
+    <li><?php _e( 'タイトル', THEME_NAME ) ?></li>
+    <li><?php _e( 'SEO', THEME_NAME ) ?></li>
+  </ul>
 
-<div class="ads">
-  <?php require_once 'original-pages/ads.php'; ?>
-</div>
+  <!-- 広告タブ -->
+  <div class="ads">
+    <?php require_once 'original-pages/ads.php'; ?>
+  </div>
 
-<div>
-JavaScript is a prototype-based scripting language with dynamic typing and
-has first-class functions. Its syntax was influenced by C.
-JavaScript copies many names and naming conventions from Java,
-but the two languages are otherwise unrelated and have very different semantics.
-The key design principles within JavaScript are taken from the Self and Scheme programming languages.
-It is a multi-paradigm language, supporting object-oriented, imperative,
-and functional programming styles.
-</div>
+  <!-- タイトルタブ -->
+  <div>
 
-<div>
-The application of JavaScript to uses outside of web pages—for example,
-in PDF documents, site-specific browsers, and desktop widgets—is also significant.
-Newer and faster JavaScript VMs and frameworks built upon them (notably Node.js)
-have also increased the popularity of JavaScript for server-side web applications.
-</div>
-</div>
+  </div>
 
+  <!-- SEOタブ -->
+  <div>
 
+  </div>
+</div><!-- /#tabs -->
 
 <input type="hidden" name="<?php echo HIDDEN_FIELD_NAME; ?>" value="Y">
 <p class="submit">
--- lib/original-pages/ads.php
+++ lib/original-pages/ads.php
@@ -5,7 +5,7 @@
         <label for="<?php echo AD_ADS_VISIBLE_NAME; ?>">広告の表示</label>
       </th>
       <td>
-        <input type="checkbox" name="<?php echo AD_ADS_VISIBLE_NAME; ?>" id="<?php echo AD_ADS_VISIBLE_NAME; ?>" value="1"<?php echo($ads_visible_val ? ' checked="checked"' : ''); ?>><?php _e("全ての広告を表示する",THEME_NAME ); ?>
+        <input type="checkbox" name="<?php echo AD_ADS_VISIBLE_NAME; ?>" id="<?php echo AD_ADS_VISIBLE_NAME; ?>" value="1"<?php echo(is_ads_visible() ? ' checked="checked"' : ''); ?>><?php _e("全ての広告を表示する",THEME_NAME ); ?>
         <p class="tips"><?php _e( '全ての広告の表示を切り替えます。', THEME_NAME ) ?></p>
       </td>
 
@@ -15,7 +15,7 @@
         <label for="<?php echo AD_AD_CODE_NAME; ?>">広告コード</label>
       </th>
       <td>
-        <textarea id="<?php echo AD_AD_CODE_NAME; ?>" name="<?php echo AD_AD_CODE_NAME; ?>" cols="46" rows="6" placeholder="AdSenseのレスポンシブコードを入力してください"><?php echo stripslashes_deep($ad_code_val); ?></textarea>
+        <textarea id="<?php echo AD_AD_CODE_NAME; ?>" name="<?php echo AD_AD_CODE_NAME; ?>" cols="46" rows="6" placeholder="AdSenseのレスポンシブコードを入力してください"><?php echo get_ad_code(); ?></textarea>
         <p class="tips"><?php _e( 'AdSenseのレスポンシブ広告コードを入力してください。', THEME_NAME ) ?></p>
       </td>
 
