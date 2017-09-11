98a43cc96d5328daee14da1c0acaa6b104ae168a
yhira <info@nelog.jp>
2017-09-11 21:01:42 +0900

    Google Search Consoleサイト認証ID設定の実装

--- footer.php
+++ footer.php
@@ -1,53 +1,53 @@
 
-            </main>
+          </main>
 
-          <?php get_sidebar(); ?>
-
-        </div>
+        <?php get_sidebar(); ?>
 
       </div>
-      <footer id="footer" class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-
-        <div id="footer-in" class="footer-in wrap cf">
-
-          <?php //フッターにウィジェットが一つも入っていない時とモバイルの時は表示しない
-          if ( (is_active_sidebar('footer-left') ||
-            is_active_sidebar('footer-center') ||
-            is_active_sidebar('footer-right') )  ): ?>
-            <div class="footer-widgets cf">
-               <div class="footer-left">
-               <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-left') ) : else : ?>
-               <?php endif; ?>
-               </div>
-               <div class="footer-center">
-               <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-center') ) : else : ?>
-               <?php endif; ?>
-               </div>
-               <div class="footer-right">
-               <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-right') ) : else : ?>
-               <?php endif; ?>
-               </div>
-            </div>
-          <?php endif; ?>
-
-          <div class="footer-bottom cf">
-            <?php get_template_part('tmp/navi-footer') ?>
-
-            <div class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</div>
-          </div>
-
-
-        </div>
-
-      </footer>
 
     </div>
+    <footer id="footer" class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
-    <?php wp_footer(); ?>
+      <div id="footer-in" class="footer-in wrap cf">
 
-    <?php //アクセス解析フッタータグの取得
-    get_template_part('tmp/analytics-footer'); ?>
+        <?php //フッターにウィジェットが一つも入っていない時とモバイルの時は表示しない
+        if ( (is_active_sidebar('footer-left') ||
+          is_active_sidebar('footer-center') ||
+          is_active_sidebar('footer-right') )  ): ?>
+          <div class="footer-widgets cf">
+             <div class="footer-left">
+             <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-left') ) : else : ?>
+             <?php endif; ?>
+             </div>
+             <div class="footer-center">
+             <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-center') ) : else : ?>
+             <?php endif; ?>
+             </div>
+             <div class="footer-right">
+             <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('footer-right') ) : else : ?>
+             <?php endif; ?>
+             </div>
+          </div>
+        <?php endif; ?>
 
-  </body>
+        <div class="footer-bottom cf">
+          <?php get_template_part('tmp/navi-footer') ?>
+
+          <div class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</div>
+        </div>
+
+
+      </div>
+
+    </footer>
+
+  </div>
+
+  <?php wp_footer(); ?>
+
+  <?php //アクセス解析フッタータグの取得
+  get_template_part('tmp/analytics-footer'); ?>
+
+</body>
 
 </html>
--- header.php
+++ header.php
@@ -1,49 +1,55 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
-  <head>
-    <meta charset="utf-8">
+<head>
+<?php //Google Search Consoleのサイト認証IDの表示
+if ( get_google_search_console_id() ): ?>
+<!-- Google Search Console -->
+<meta name="google-site-verification" content="<?php echo get_google_search_console_id() ?>" />
+<!-- /Google Search Console -->
+<?php endif;//ウェブマスターツールID終了 ?>
+<meta charset="utf-8">
 
-    <?php // force Internet Explorer to use the latest rendering engine available ?>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php // force Internet Explorer to use the latest rendering engine available ?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <?php // mobile meta (hooray!) ?>
-    <meta name="HandheldFriendly" content="True">
-    <meta name="MobileOptimized" content="320">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+<?php // mobile meta (hooray!) ?>
+<meta name="HandheldFriendly" content="True">
+<meta name="MobileOptimized" content="320">
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-    <?php wp_head(); ?>
+<?php wp_head(); ?>
 
-  </head>
+</head>
 
-  <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-    <?php //アクセス解析ヘッダータグの取得
-    get_template_part('tmp/analytics-header'); ?>
+  <?php //アクセス解析ヘッダータグの取得
+  get_template_part('tmp/analytics-header'); ?>
 
-    <div id="container" class="container cf">
-      <div class="header-container">
-        <header id="header" class="header cf" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+  <div id="container" class="container cf">
+    <div class="header-container">
+      <header id="header" class="header cf" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-          <div id="header-in" class="header-in wrap cf">
-            <div class="header-top"><div class="tagline"><?php bloginfo('description') ?></div></div>
-            <?php //サイト名の見出し
-            $tag = 'div';
-            if (!is_singular()) {
-              $tag = 'h1';
-            }
-             ?>
-            <<?php echo $tag; ?> id="logo" class="logo" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" class="site-name"><?php bloginfo('name'); ?></a></<?php echo $tag; ?>>
+        <div id="header-in" class="header-in wrap cf">
+          <div class="header-top"><div class="tagline"><?php bloginfo('description') ?></div></div>
+          <?php //サイト名の見出し
+          $tag = 'div';
+          if (!is_singular()) {
+            $tag = 'h1';
+          }
+           ?>
+          <<?php echo $tag; ?> id="logo" class="logo" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" class="site-name"><?php bloginfo('name'); ?></a></<?php echo $tag; ?>>
 
-          </div>
+        </div>
 
-        </header>
+      </header>
 
-        <?php get_template_part('tmp/navi'); ?>
-      </div><!-- /.header-container -->
+      <?php get_template_part('tmp/navi'); ?>
+    </div><!-- /.header-container -->
 
-      <div id="content" class="content cf">
+    <div id="content" class="content cf">
 
-        <div id="content-in" class="content-in wrap cf">
+      <div id="content-in" class="content-in wrap cf">
 
-            <main id="main" class="main<?php echo get_additional_main_classes(); ?>" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+          <main id="main" class="main<?php echo get_additional_main_classes(); ?>" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
