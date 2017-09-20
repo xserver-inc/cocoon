7ded277a5f82e7d81261f7d4341eb5a48a110b38
yhira <info@nelog.jp>
2017-09-20 22:57:52 +0900

    検索ボックスと広告スタイルの調整

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+<form method="get" class="search-box" action="<?php echo home_url('/'); ?>">
 	<input type="text" placeholder="<?php _e( 'ブログ内を検索', THEME_NAME ) ?>" name="s" class="search-edit">
 	<button type="submit" class="search-submit"></button>
 </form>
