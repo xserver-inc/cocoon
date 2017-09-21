03b9fb5cccfeae08337f705e28f96df71968b073
yhira <info@nelog.jp>
2017-09-21 22:14:15 +0900

    SEO関係の関数追加

--- lib/original-pages/seo-funcs.php
+++ lib/original-pages/seo-funcs.php
@@ -1,150 +1,25 @@
 <?php //アクセス解析設定に必要な定数や関数
 
-//タイトルセパレーター
-define('OP_TITLE_SEPARATOR', 'title_separator');
-if ( !function_exists( 'get_title_separator' ) ):
-function get_title_separator(){
-  return get_option(OP_TITLE_SEPARATOR, 'pipe');
-}
-endif;
-//タイトルセパレーターのキャプションを取得する
-if ( !function_exists( 'get_title_separator_caption' ) ):
-function get_title_separator_caption(){
-  switch (get_title_separator()) {
-    case 'hyphen':
-      $separator = __( ' - ', THEME_NAME );
-      break;
-    default:
-      $separator = __( ' | ', THEME_NAME );
-      break;
-  }
-  return $separator;
+//canonicalタグの追加
+define('OP_CANONICAL_TAG_ENABLE', 'canonical_tag_enable');
+if ( !function_exists( 'is_canonical_tag_enable' ) ):
+function is_canonical_tag_enable(){
+  return get_option(OP_CANONICAL_TAG_ENABLE, 1);
 }
 endif;
 
-//フロントページのタイトル
-define('OP_FRONT_PAGE_TITLE_FORMAT', 'front_page_title_format');
-if ( !function_exists( 'get_front_page_title_format' ) ):
-function get_front_page_title_format(){
-  return get_option(OP_FRONT_PAGE_TITLE_FORMAT, 'sitename_tagline');
-}
-endif;
-//フロントページタイトルのキャプションを取得する
-if ( !function_exists( 'get_front_page_title_caption' ) ):
-function get_front_page_title_caption(){
-  switch (get_front_page_title_format()) {
-    case 'sitename_tagline':
-      $title = get_bloginfo('name').get_title_separator_caption().get_bloginfo('description');
-      break;
-    default:
-      $title = get_bloginfo('name');
-      break;
-  }
-  return $title;
-}
-endif;
-//キャッチフレーズをフロントページタイトルに含める場合
-if ( !function_exists( 'is_tagline_to_front_page_title' ) ):
-function is_tagline_to_front_page_title(){
-  return get_front_page_title_format() == 'sitename_tagline';
+//分割ページにrel="next"/"prev"タグの追加
+define('OP_PREV_NEXT_ENABLE', 'prev_next_enable');
+if ( !function_exists( 'is_prev_next_enable' ) ):
+function is_prev_next_enable(){
+  return get_option(OP_PREV_NEXT_ENABLE, 1);
 }
 endif;
 
-//フロントページのメタディスクリプション
-define('OP_FRONT_PAGE_META_DESCRIPTION', 'front_page_meta_description');
-if ( !function_exists( 'get_front_page_meta_description' ) ):
-function get_front_page_meta_description(){
-  return get_option(OP_FRONT_PAGE_META_DESCRIPTION);
-}
-endif;
-
-//フロントページのメタディスクリプション
-define('OP_FRONT_PAGE_META_KEYWORDS', 'front_page_meta_keywords');
-if ( !function_exists( 'get_front_page_meta_keywords' ) ):
-function get_front_page_meta_keywords(){
-  return get_option(OP_FRONT_PAGE_META_KEYWORDS);
-}
-endif;
-
-//投稿・固定ページのタイトル
-define('OP_SINGULAR_PAGE_TITLE_FORMAT', 'singular_page_title_format');
-if ( !function_exists( 'get_singular_page_title_format' ) ):
-function get_singular_page_title_format(){
-  return get_option(OP_SINGULAR_PAGE_TITLE_FORMAT, 'pagetitle_sitename');
-}
-endif;
-//投稿・固定ページタイトルのキャプションを取得する
-if ( !function_exists( 'get_singular_title_caption' ) ):
-function get_singular_title_caption($post){
-  switch (get_singular_page_title_format()) {
-    case 'pagetitle_sitename':
-      $title = $post->post_title.get_title_separator_caption().get_bloginfo('name');
-      break;
-    case 'sitename_pagetitle':
-      $title = get_bloginfo('name').get_title_separator_caption().$post->post_title;
-      break;
-    default:
-      $title = $post->post_title;
-      break;
-  }
-  return $title;
-}
-endif;
-
-//投稿・固定ページにメタディスクリプションを含める
-define('OP_META_DESCRIPTION_TO_SINGULAR', 'meta_description_to_singular');
-if ( !function_exists( 'is_meta_description_to_singular' ) ):
-function is_meta_description_to_singular(){
-  return get_option(OP_META_DESCRIPTION_TO_SINGULAR, 1);
-}
-endif;
-
-//投稿・固定ページにメタキーワードを含める
-define('OP_META_KEYWORDS_TO_SINGULAR', 'meta_keywords_to_singular');
-if ( !function_exists( 'is_meta_keywords_to_singular' ) ):
-function is_meta_keywords_to_singular(){
-  return get_option(OP_META_KEYWORDS_TO_SINGULAR);
-}
-endif;
-
-//カテゴリページのタイトル
-define('OP_CATEGORY_PAGE_TITLE_FORMAT', 'category_page_title_format');
-if ( !function_exists( 'get_category_page_title_format' ) ):
-function get_category_page_title_format(){
-  return get_option(OP_CATEGORY_PAGE_TITLE_FORMAT, 'category_sitename');
-}
-endif;
-//カテゴリーページタイトルのキャプションを取得する
-if ( !function_exists( 'get_category_title_caption' ) ):
-function get_category_title_caption($category){
-  switch (get_category_page_title_format()) {
-    case 'category_sitename':
-      $title = $category->name.get_title_separator_caption().get_bloginfo('name');
-      break;
-    case 'sitename_category':
-      $title = get_bloginfo('name').get_title_separator_caption().$category->name;
-      break;
-    default:
-      $title = $category->name;
-      break;
-  }
-  return $title;
-}
-endif;
-
-
-//カテゴリページにメタディスクリプションを含める
-define('OP_META_DESCRIPTION_TO_CATEGORY', 'meta_description_to_category');
-if ( !function_exists( 'is_meta_description_to_category' ) ):
-function is_meta_description_to_category(){
-  return get_option(OP_META_DESCRIPTION_TO_CATEGORY, 1);
-}
-endif;
-
-//カテゴリページにメタキーワードを含める
-define('OP_META_KEYWORDS_TO_CATEGORY', 'meta_keywords_to_category');
-if ( !function_exists( 'is_meta_keywords_to_category' ) ):
-function is_meta_keywords_to_category(){
-  return get_option(OP_META_KEYWORDS_TO_CATEGORY);
+//カテゴリページの2ページ目以降をnoindexとする 
+define('OP_PAGED_CATEGORY_PAGE_NOINDEX', 'paged_category_page_noindex');
+if ( !function_exists( 'is_paged_category_page_noindex' ) ):
+function is_paged_category_page_noindex(){
+  return get_option(OP_PAGED_CATEGORY_PAGE_NOINDEX);
 }
 endif;
--- lib/seo.php
+++ lib/seo.php
@@ -1,6 +1,5 @@
 <?php //SEO関係の関数
 
-
 //Wordpress4.1からのタイトル自動作成
 //https://www.nxworld.net/wordpress/wp-custom-title-tag.html
 if ( !function_exists( 'setup_theme_custum' ) ):
@@ -72,6 +71,7 @@ function title_parts_custom( $title ){
 endif;
 add_filter( 'document_title_parts', 'title_parts_custom' );
 
+
 //noindexページの判別関数
 if ( !function_exists( 'is_noindex_page' ) ):
 function is_noindex_page(){
@@ -83,3 +83,132 @@ function is_noindex_page(){
   is_attachment(); //添付ファイルページも含めない
 }
 endif;
+
+
+////ページネーションと分割ページ（マルチページ）タグを出力
+if ( !function_exists( 'the_prev_next_link_tag' ) ):
+function the_prev_next_link_tag() {
+  //1ページを複数に分けた分割ページ
+  if(is_single() || is_page()) {
+    global $wp_query;
+    $multipage = check_multi_page();
+    if($multipage[0] > 1) {
+      $prev = generate_multipage_url('prev');
+      $next = generate_multipage_url('next');
+      if($prev) {
+        echo '<link rel="prev" href="'.$prev.'" />'.PHP_EOL;
+      }
+      if($next) {
+        echo '<link rel="next" href="'.$next.'" />'.PHP_EOL;
+      }
+    }
+  } else{
+    //トップページやカテゴリページなどの分割ページの設定
+    global $paged;
+    if ( get_previous_posts_link() ){
+      echo '<link rel="prev" href="'.get_pagenum_link( $paged - 1 ).'" />'.PHP_EOL;
+    }
+    if ( get_next_posts_link() ){
+      echo '<link rel="next" href="'.get_pagenum_link( $paged + 1 ).'" />'.PHP_EOL;
+    }
+  }
+}
+endif;
+
+if ( is_prev_next_enable() ) {
+  //デフォルトのrel="next"/"prev"を消す
+  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+  //分割ページのみnext/prevを表示
+  add_action( 'wp_head', 'the_prev_next_link_tag' );
+}
+
+//分割ページ（マルチページ）URLの取得
+//参考ページ：
+//http://seophp.net/wordpress-fix-rel-prev-and-rel-next-without-plugin/
+if ( !function_exists( 'generate_multipage_url' ) ):
+function generate_multipage_url($rel='prev') {
+  global $post;
+  $url = '';
+  $multipage = check_multi_page();
+  if($multipage[0] > 1) {
+    $numpages = $multipage[0];
+    $page = $multipage[1] == 0 ? 1 : $multipage[1];
+    $i = 'prev' == $rel? $page - 1: $page + 1;
+    if($i && $i > 0 && $i <= $numpages) {
+      if(1 == $i) {
+        $url = get_permalink();
+      } else {
+        if ('' == get_option('permalink_structure') || in_array($post->post_status, array('draft', 'pending'))) {
+          $url = add_query_arg('page', $i, get_permalink());
+        } else {
+          $url = trailingslashit(get_permalink()).user_trailingslashit($i, 'single_paged');
+        }
+      }
+    }
+  }
+  return $url;
+}
+endif;
+
+
+//分割ページ（マルチページ）かチェックする
+if ( !function_exists( 'check_multi_page' ) ):
+function check_multi_page() {
+  $num_pages    = substr_count(
+      $GLOBALS['post']->post_content,
+      '<!--nextpage-->'
+  ) + 1;
+  $current_page = get_query_var( 'page' );
+  return array ( $num_pages, $current_page );
+}
+endif;
+
+
+//デフォルトのcanonicalタグ削除
+//remove_action('wp_head', 'rel_canonical');
+
+//canonical URLの生成
+if ( !function_exists( 'generate_canonical_url' ) ):
+function generate_canonical_url(){
+  global $paged;
+  global $page;
+
+  //canonicalの疑問点
+  //アーカイブはnoindexにしているけどcanonicalタグは必要か？
+  //タグページはnoindexにしているけどcanonicalタグは必要か？
+  //404ページはAll in One SEO Packはcanonicalタグを出力していないようだけど必要か？
+  $canonical_url = home_url();
+  if (is_home()) {
+    $canonical_url = home_url();
+  } elseif (is_category()) {
+    $canonical_url = get_category_link(get_query_var('cat'));
+  } elseif (is_tag()) {
+    $postTag = get_the_tags();
+    $canonical_url = get_tag_link( $postTag[0]->term_id );
+  } elseif (is_page() || is_single()) {
+    $canonical_url = get_permalink();
+  } elseif(is_404()) {
+    $canonical_url =  home_url()."/404";
+  }
+
+  if ($canonical_url && ( $paged >= 2 || $page >= 2)) {
+    $canonical_url = $canonical_url.'/page/'.max( $paged, $page ).'';
+  }
+
+  return $canonical_url;
+
+}
+endif;
+
+
+//canonicalタグの取得
+//取得条件；http://bazubu.com/seo101/how-to-use-canonical
+if ( !function_exists( 'the_canonical_tag' ) ):
+function the_canonical_tag(){
+  $canonical_url = generate_canonical_url();
+  if ( $canonical_url ) {
+    echo '<link rel="canonical" href="'.$canonical_url.'">'.PHP_EOL;
+  }
+}
+endif;
+
