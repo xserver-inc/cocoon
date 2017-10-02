a57b05677221859c73df2ecf6f1514909d137941
yhira <info@nelog.jp>
2017-10-02 16:49:38 +0900

    カテゴリの説明文をビジュアルエディターにする

--- lib/admin.php
+++ lib/admin.php
@@ -445,3 +445,41 @@ function tiny_mce_before_init_custom( $mceInit ) {
   return $mceInit;
 }
 endif;
+
+// remove the html filtering
+remove_filter( 'pre_term_description', 'wp_filter_kses' );
+remove_filter( 'term_description', 'wp_kses_data' );
+
+add_filter('edit_category_form_fields', 'cat_description');
+function cat_description($tag){
+  ?>
+    <table class="form-table">
+      <tr class="form-field">
+        <th scope="row" valign="top"><label for="description"><?php _e('Description'); ?></label></th>
+        <td>
+        <?php
+          $settings = array('wpautop' => true, 'media_buttons' => true, 'quicktags' => true, 'textarea_rows' => '15', 'textarea_name' => 'description' );
+          wp_editor(wp_kses_post($tag->description , ENT_QUOTES, 'UTF-8'), 'cat_description', $settings);
+        ?>
+        <br />
+        <span class="description"><?php _e('The description is not prominent by default; however, some themes may show it.'); ?></span>
+        </td>
+      </tr>
+    </table>
+  <?php
+}
+
+add_action('admin_head', 'remove_default_category_description');
+function remove_default_category_description(){
+  global $current_screen;
+  if ( $current_screen->id == 'edit-category' )
+  {
+  ?>
+    <script type="text/javascript">
+    jQuery(function($) {
+        $('textarea#description').closest('tr.form-field').remove();
+    });
+    </script>
+  <?php
+  }
+}
