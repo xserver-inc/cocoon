fa706f8a9eb46a152ea3adb2daf864d948b3432a
yhira <info@nelog.jp>
2017-10-07 21:41:29 +0900

    アクセス解析項目のリファクタリング

--- lib/original-pages/analytics-forms.php
+++ lib/original-pages/analytics-forms.php
@@ -13,11 +13,13 @@
         <!-- Google AnalyticsトラッキングID -->
         <tr>
           <th scope="row">
-            <label for="<?php echo OP_GOOGLE_ANALYTICS_TRACKING_ID; ?>"><?php _e( 'Google AnalyticsトラッキングID', THEME_NAME ) ?></label>
+            <?php genelate_label_tag(OP_GOOGLE_ANALYTICS_TRACKING_ID, __( 'Google AnalyticsトラッキングID', THEME_NAME )); ?>
           </th>
           <td>
-            <input type="text" name="<?php echo OP_GOOGLE_ANALYTICS_TRACKING_ID; ?>" size="<?php echo DEFAULT_INPUT_COLS; ?>" value="<?php echo get_google_analytics_tracking_id(); ?>" placeholder="<?php _e( 'UA-00000000-0', THEME_NAME ); ?>">
-            <p class="tips"><?php _e( 'Google AnalyticsのトラッキングIDを入力してください。', THEME_NAME ) ?></p>
+            <?php
+            genelate_textbox_tag(OP_GOOGLE_ANALYTICS_TRACKING_ID, get_google_analytics_tracking_id(), __( 'UA-00000000-0', THEME_NAME ));
+            genelate_tips_tag(__( 'Google AnalyticsのトラッキングIDを入力してください。', THEME_NAME ));
+            ?>
           </td>
         </tr>
 
@@ -32,7 +34,7 @@
 <div id="analytics" class="postbox">
   <h2 class="hndle"><?php _e( 'Google Search Console設定', THEME_NAME ) ?></h2>
   <div class="inside">
-    
+
     <p><?php _e( 'Google Search Consoleのサイト認証タグの設定です。', THEME_NAME ) ?></p>
 
     <table class="form-table">
@@ -41,11 +43,13 @@
         <!-- Google Search Console ID -->
         <tr>
           <th scope="row">
-            <label for="<?php echo OP_GOOGLE_SEARCH_CONSOLE_ID; ?>"><?php _e( 'Google Search Console ID', THEME_NAME ) ?></label>
+            <?php genelate_label_tag(OP_GOOGLE_SEARCH_CONSOLE_ID, __( 'Google Search Console ID', THEME_NAME )); ?>
           </th>
           <td>
-            <input type="text" name="<?php echo OP_GOOGLE_SEARCH_CONSOLE_ID; ?>" size="<?php echo DEFAULT_INPUT_COLS; ?>" value="<?php echo get_google_search_console_id(); ?>" placeholder="<?php _e( 'サイト認証IDのみ入力', THEME_NAME ); ?>">
-            <p class="tips"><?php _e( 'Google Search Consoleのサイト認証IDを入力してください。', THEME_NAME ) ?></p>
+            <?php
+            genelate_textbox_tag(OP_GOOGLE_SEARCH_CONSOLE_ID, get_google_search_console_id(), __( 'サイト認証IDのみ入力', THEME_NAME ));
+            genelate_tips_tag(__( 'Google Search Consoleのサイト認証IDを入力してください。', THEME_NAME ));
+            ?>
           </td>
         </tr>
 
@@ -60,7 +64,7 @@
 <div id="ptengine" class="postbox">
   <h2 class="hndle"><?php _e( 'Ptengine設定', THEME_NAME ) ?></h2>
   <div class="inside">
-    
+
     <p><?php _e( 'Ptengineの解析タグの設定です。', THEME_NAME ) ?></p>
 
     <table class="form-table">
@@ -69,11 +73,13 @@
         <!-- PtengineのトラッキングID -->
         <tr>
           <th scope="row">
-            <label for="<?php echo OP_PTENGINE_TRACKING_ID; ?>"><?php _e( 'PtengineのトラッキングID', THEME_NAME ) ?></label>
+            <?php genelate_label_tag(OP_PTENGINE_TRACKING_ID, __( 'PtengineのトラッキングID', THEME_NAME )); ?>
           </th>
           <td>
-            <input type="text" name="<?php echo OP_PTENGINE_TRACKING_ID; ?>" size="<?php echo DEFAULT_INPUT_COLS; ?>" value="<?php echo get_ptengine_tracking_id(); ?>" placeholder="<?php _e( 'PtengineのトラッキングIDのみ入力', THEME_NAME ); ?>">
-            <p class="tips"><?php _e( 'PtengineのトラッキングIDを入力してください。', THEME_NAME ) ?></p>
+            <?php
+            genelate_textbox_tag(OP_PTENGINE_TRACKING_ID, get_ptengine_tracking_id(), __( 'PtengineのトラッキングIDのみ入力', THEME_NAME ));
+            genelate_tips_tag(__( 'PtengineのトラッキングIDを入力してください。', THEME_NAME ));
+            ?>
           </td>
         </tr>
 
@@ -88,7 +94,7 @@
 <div id="other" class="postbox">
   <h2 class="hndle"><?php _e( 'その他のアクセス解析設定', THEME_NAME ) ?></h2>
   <div class="inside">
-    
+
     <p><?php _e( 'ヘッダーやフッターに、その他サービスのアクセス解析タグをそのまま貼り付けます。', THEME_NAME ) ?></p>
 
     <table class="form-table">
@@ -97,22 +103,26 @@
         <!-- アクセス解析ヘッダータグ -->
         <tr>
           <th scope="row">
-            <label for="<?php echo OP_OTHER_ANALYTICS_HEADER_TAGS; ?>"><?php _e( 'アクセス解析タグ（ヘッダー用）', THEME_NAME ) ?></label>
+            <?php genelate_label_tag(OP_OTHER_ANALYTICS_HEADER_TAGS, __( 'アクセス解析タグ（ヘッダー用）', THEME_NAME )); ?>
           </th>
           <td>
-            <textarea name="<?php echo OP_OTHER_ANALYTICS_HEADER_TAGS; ?>" cols="<?php echo DEFAULT_INPUT_COLS; ?>" rows="<?php echo DEFAULT_INPUT_ROWS; ?>" placeholder="<?php _e( 'ヘッダー用のアクセス解析タグの入力', THEME_NAME ) ?>"><?php echo get_other_analytics_header_tags(); ?></textarea>
-            <p class="tips"><?php _e( 'ヘッダーに挿入する必要のある、その他アクセス解析タグを入力してください。', THEME_NAME ); ?></p>
+            <?php
+            genelate_textarea_tag(OP_OTHER_ANALYTICS_HEADER_TAGS, get_other_analytics_header_tags(), __( 'ヘッダー用のアクセス解析タグの入力', THEME_NAME )) ;
+            genelate_tips_tag(__( 'ヘッダーに挿入する必要のある、その他アクセス解析タグを入力してください。', THEME_NAME ));
+            ?>
           </td>
         </tr>
 
         <!-- アクセス解析フッタータグ -->
         <tr>
           <th scope="row">
-            <label for="<?php echo OP_OTHER_ANALYTICS_FOOTER_TAGS; ?>"><?php _e( 'アクセス解析タグ（フッター用）', THEME_NAME ) ?></label>
+            <?php genelate_label_tag(OP_OTHER_ANALYTICS_FOOTER_TAGS, __( 'アクセス解析タグ（フッター用）', THEME_NAME )); ?>
           </th>
           <td>
-            <textarea name="<?php echo OP_OTHER_ANALYTICS_FOOTER_TAGS; ?>" cols="<?php echo DEFAULT_INPUT_COLS; ?>" rows="<?php echo DEFAULT_INPUT_ROWS; ?>" placeholder="<?php _e( 'フッター用のアクセス解析タグの入力', THEME_NAME ) ?>"><?php echo get_other_analytics_footer_tags(); ?></textarea>
-            <p class="tips"><?php _e( 'フッターに挿入する必要のある、その他アクセス解析タグを入力してください。', THEME_NAME ); ?></p>
+            <?php
+            genelate_textarea_tag(OP_OTHER_ANALYTICS_FOOTER_TAGS, get_other_analytics_footer_tags(), __( 'フッター用のアクセス解析タグの入力', THEME_NAME )) ;
+            genelate_tips_tag(__( 'フッターに挿入する必要のある、その他アクセス解析タグを入力してください。', THEME_NAME ));
+            ?>
           </td>
         </tr>
 
