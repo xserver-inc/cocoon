4d911406f6fbd97b654b3c59090f0b0af778cb82
yhira <info@nelog.jp>
2017-10-07 22:15:54 +0900

    ツイート設定項目のリファクタリング

--- lib/original-pages/sns-share-forms.php
+++ lib/original-pages/sns-share-forms.php
@@ -18,22 +18,26 @@ require_once 'sns-share-forms-bottom.php';
         <!-- メンション -->
         <tr>
           <th scope="row">
-            <label for="<?php echo OP_TWITTER_ID_INCLUDE; ?>"><?php _e( 'メンション', THEME_NAME ) ?></label>
+            <?php genelate_label_tag(OP_TWITTER_ID_INCLUDE, __( 'メンション', THEME_NAME )); ?>
           </th>
           <td>
-             <input type="checkbox" name="<?php echo OP_TWITTER_ID_INCLUDE; ?>" value="1"<?php the_checkbox_checked(is_twitter_id_include()); ?>><?php _e("ツイートにメンションを含める",THEME_NAME ); ?>
-            <p class="tips"><?php _e( 'シェアされたツイートに著者のTwitter IDを含めます。', THEME_NAME ) ?></p>
+            <?php
+            genelate_checkbox_tag( OP_TWITTER_ID_INCLUDE, is_twitter_id_include(), __( 'ツイートにメンションを含める', THEME_NAME ));
+            genelate_tips_tag(__( 'シェアされたツイートに著者のTwitter', THEME_NAME ));
+            ?>
           </td>
         </tr>
 
         <!-- プロモーション -->
         <tr>
           <th scope="row">
-            <label for="<?php echo OP_TWITTER_RELATED_FOLLOW_ENABLE; ?>"><?php _e( 'プロモーション', THEME_NAME ) ?></label>
+            <?php genelate_label_tag(OP_TWITTER_RELATED_FOLLOW_ENABLE, __( 'プロモーション', THEME_NAME )); ?>
           </th>
           <td>
-             <input type="checkbox" name="<?php echo OP_TWITTER_RELATED_FOLLOW_ENABLE; ?>" value="1"<?php the_checkbox_checked(is_twitter_related_follow_enable()); ?>><?php _e("ツイート後にフォローを促す",THEME_NAME ); ?>
-            <p class="tips"><?php _e( 'ツイート後に著者のフォローボタンを表示します。', THEME_NAME ) ?></p>
+            <?php
+            genelate_checkbox_tag( OP_TWITTER_RELATED_FOLLOW_ENABLE, is_twitter_related_follow_enable(), __( 'ツイート後にフォローを促す', THEME_NAME ));
+            genelate_tips_tag(__( 'ツイート後に著者のフォローボタンを表示します。', THEME_NAME ));
+            ?>
           </td>
         </tr>
 
