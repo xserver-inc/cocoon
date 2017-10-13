ee7a2c6b535ed71f1e81861b2a87efc6afb6a441
yhira <info@nelog.jp>
2017-10-13 20:32:45 +0900

    投稿ページ日付のスタイル不具合修正

--- lib/original-pages/single-forms.php
+++ lib/original-pages/single-forms.php
@@ -115,4 +115,32 @@
   </div>
 </div>
 
+<!-- ページ送りナビ -->
+<div id="single-page" class="postbox">
+  <h2 class="hndle"><?php _e( 'ページ送りナビ設定', THEME_NAME ) ?></h2>
+  <div class="inside">
+
+    <p><?php _e( '[前ページ][次ページ]へと送るナビゲーションの設定です。', THEME_NAME ) ?></p>
+
+    <table class="form-table">
+      <tbody>
+        <!-- プレビュー画面 -->
+        <tr>
+          <th scope="row">
+            <label><?php _e( 'プレビュー', THEME_NAME ) ?></label>
+          </th>
+          <td>
+            <div class="demo">
+              <?php //get_template_part('tmp/') ?>
+            </div>
+            <?php genelate_tips_tag(__( 'デモの関連記事はランダムです。', THEME_NAME )); ?>
+          </td>
+        </tr>
+
+      </tbody>
+    </table>
+
+  </div>
+</div>
+
 </div><!-- /.metabox-holder -->
