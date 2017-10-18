bf37833476f9059065bf98f1d253c96960ea00eb
yhira <info@nelog.jp>
2017-10-18 19:29:36 +0900

    モバイル用グローバルナビの実装

--- tmp/navi.php
+++ tmp/navi.php
@@ -1,12 +1,30 @@
 <!-- Navigation -->
 <nav id="navi" class="navi cf" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-	<div id="navi-in" class="navi-in wrap cf">
-    <?php wp_nav_menu(
+  <div id="navi-in" class="navi-in wrap cf">
+    <?php //ヘッダーナビ
+    wp_nav_menu(
       array (
         //カスタムメニュー名
         'theme_location' => 'navi-header',
         //ul 要素に適用するCSS クラス名
         'menu_class' => 'menu-header',
+        //コンテナを表示しない
+        'container' => false,
+        //カスタムメニューを設定しない際に固定ページでメニューを作成しない
+        'fallback_cb' => false,
+        //出力されるulに対してidやclassを表示しない
+        //'items_wrap' => '<ul>%3$s</ul>',
+        //説明出力用
+        'walker' => new menu_description_walker()
+      )
+    ); ?>
+    <?php //モバイルヘッダーナビ
+    wp_nav_menu(
+      array (
+        //カスタムメニュー名
+        'theme_location' => 'navi-mobile',
+        //ul 要素に適用するCSS クラス名
+        'menu_class' => 'menu-mobile',
         //コンテナを表示しない
         'container' => false,
         //カスタムメニューを設定しない際に固定ページでメニューを作成しない
