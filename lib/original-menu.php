ead23a81144c6ac5a02fa2c5f3afa7ebc2dfc50e
yhira <info@nelog.jp>
2017-10-19 23:41:36 +0900

    Simplicityの独自設定サブメニュー追加

--- lib/original-menu.php
+++ lib/original-menu.php
@@ -8,24 +8,21 @@ function add_original_menu_in_admin_page() {
     add_menu_page(SETTING_NAME_TOP, SETTING_NAME_TOP, 'manage_options', 'theme-settings', 'add_theme_settings_page' );
     //var_dump('aaaaaaaa');
 
-    // カスタムトップレベルメニューにサブメニューを追加
+    //吹き出しサブメニューを追加
+    add_submenu_page('theme-settings', __('吹き出し', THEME_NAME), __('吹き出し', THEME_NAME), 'manage_options', 'theme-balloon', 'add_theme_balloon_page');
+
+    //使いまわしテキストサブメニューを追加
+    add_submenu_page('theme-settings', __('使いまわしテキスト', THEME_NAME), __('使いまわしテキスト', THEME_NAME), 'manage_options', 'theme-text-func', 'add_theme_text_func_page');
+
+    //アクセス分析サブメニューを追加
+    add_submenu_page('theme-settings', __('アクセス分析', THEME_NAME), __('アクセス分析', THEME_NAME), 'manage_options', 'theme-analytics', 'add_theme_analytics_page');
+
+    //ソーシャルカウントサブメニューを追加
+    add_submenu_page('theme-settings', __('ソーシャルカウント', THEME_NAME), __('ソーシャルカウント', THEME_NAME), 'manage_options', 'theme-sns-count', 'add_theme_sns_count_page');
+
+    //バックアップサブメニューを追加
     add_submenu_page('theme-settings', __('バックアップ', THEME_NAME), __('バックアップ', THEME_NAME), 'manage_options', 'theme-backup', 'add_theme_backup_page');
 
-    // // 新しいトップレベルメニューを追加 (あれだけ言ったのに...)
-    // add_menu_page(__('Test Toplevel','menu-test'), __('Test Toplevel','menu-test'), 'manage_options', 'mt-top-level-handle', 'mt_toplevel_page' );
-
-    // // カスタムトップレベルメニューにサブメニューを追加
-    // add_submenu_page('mt-top-level-handle', __('Test Sublevel','menu-test'), __('Test Sublevel','menu-test'), 'manage_options', 'sub-page', 'mt_sublevel_page');
-
-
-    // // 新しいトップレベルメニューを追加 (あれだけ言ったのに...)
-    // add_menu_page(__('Test Toplevel','menu-test'), __('Test Toplevel','menu-test'), 'manage_options', 'mt-top-level-handle', 'mt_toplevel_page' );
-
-    // // カスタムトップレベルメニューにサブメニューを追加
-    // add_submenu_page('mt-top-level-handle', __('Test Sublevel','menu-test'), __('Test Sublevel','menu-test'), 'manage_options', 'sub-page', 'mt_sublevel_page');
-
-    // // カスタムトップレベルメニューに2番目のサブメニューを追加
-    // add_submenu_page('mt-top-level-handle', __('Test Sublevel 2','menu-test'), __('Test Sublevel 2','menu-test'), 'manage_options', 'sub-page2', 'mt_sublevel_page2');
 }
 endif;
 
