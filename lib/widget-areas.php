b6528fff2a1f76295a048be6b593b81208fa3c8d
yhira <info@nelog.jp>
2017-10-31 17:12:45 +0900

    投稿本文中ウィジェットの実装

--- lib/widget-areas.php
+++ lib/widget-areas.php
@@ -391,3 +391,33 @@ register_404_page_widget_area();
 
 
 
+/////////////////////////////////////
+// 投稿本文中にウィジェットを表示する
+/////////////////////////////////////
+add_filter('the_content','add_widget_area_before_1st_h2');
+if ( !function_exists( 'add_widget_area_before_1st_h2' ) ):
+function add_widget_area_before_1st_h2($the_content) {
+  // if ( is_amp() ) {
+  //   return $the_content;
+  // }
+  if ( is_single() && //投稿ページのとき、固定ページも表示する場合はis_singular()にする
+       is_active_sidebar( 'single-content-middle' ) //ウィジェットが設定されているとき
+  ) {
+    //広告（AdSense）タグを記入
+    ob_start();//バッファリング
+    //echo '<div id="single-content-middle" class="widgets">';
+    dynamic_sidebar( 'single-content-middle' );;//本文中ウィジェットの表示
+    //echo '</div>';
+    $ad_template = ob_get_clean();
+    $h2result = get_h2_included_in_body( $the_content );//本文にH2タグが含まれていれば取得
+    if ( $h2result ) {//H2見出しが本文中にある場合のみ
+      //最初のH2の手前に広告を挿入（最初のH2を置換）
+      $count = 1;
+      $the_content = preg_replace(H2_REG, $ad_template.$h2result, $the_content, 1);
+    }
+  }
+  return $the_content;
+}
+endif;
+
+
