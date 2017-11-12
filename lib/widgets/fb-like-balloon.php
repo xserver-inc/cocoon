7a5ab06230d5d6e95c59a959875e8a92779beac7
yhira <info@nelog.jp>
2017-11-12 22:04:02 +0900

    Facebookバルーンウィジェットの実装

--- lib/widgets/fb-like-balloon.php
+++ lib/widgets/fb-like-balloon.php
@@ -7,7 +7,8 @@ class FBLikeBallooneWidgetItem extends WP_Widget {
     parent::__construct(
       'facebook_page_like',
       WIDGET_NAME_PREFIX.__( 'Facebookバルーン', THEME_NAME ), //ウイジェット名
-      array('description' => __( '投稿・個別ページのアイキャッチを利用したFacebookページへの「いいね！」ボタンを表示するウィジェットです。', THEME_NAME ))
+      array('description' => __( '投稿・個別ページのアイキャッチを利用したFacebookページへの「いいね！」ボタンを表示するウィジェットです。', THEME_NAME )),
+      array( 'width' => 400, 'height' => 350 )
     );
   }
   function widget($args, $instance) {
@@ -15,6 +16,8 @@ class FBLikeBallooneWidgetItem extends WP_Widget {
     //タイトル名を取得
     $title = apply_filters( 'widget_title_facebook_page_like', $instance['title_facebook_page_like'] );
     $text = apply_filters( 'widget_text_facebook_page_like', $instance['text_facebook_page_like'] );
+    $facebook_url = !empty( $instance['facebook_url'] ) ? $instance['facebook_url'] : get_the_author_facebook_url();
+
     //classにwidgetと一意となるクラス名を追加する
     if ( is_singular() ): //投稿・固定ページのトップ表示
       echo $args['before_widget'];
@@ -22,6 +25,7 @@ class FBLikeBallooneWidgetItem extends WP_Widget {
         echo $args['before_title'].$title.$args['after_title'];//タイトルが設定されている場合は使用する
       }
       set_query_var('_FACEBOOK_PAGE_LIKE_TEXT', $text);
+      set_query_var('_FACEBOOK_URL', $facebook_url);
       get_template_part('tmp/fb-like-balloon');
       echo $args['after_widget'];
     endif;//is_singular ?>
@@ -29,8 +33,9 @@ class FBLikeBallooneWidgetItem extends WP_Widget {
   }
   function update($new_instance, $old_instance) {
     $instance = $old_instance;
-    $instance['title_facebook_page_like'] = strip_tags($new_instance['title_facebook_page_like']);
-    $instance['text_facebook_page_like'] = $new_instance['text_facebook_page_like'];
+    $instance['title_facebook_page_like'] = strip_tags(!empty($new_instance['title_facebook_page_like']) ? $new_instance['title_facebook_page_like'] : '');
+    $instance['text_facebook_page_like'] = !empty($new_instance['text_facebook_page_like']) ? $new_instance['text_facebook_page_like'] : '';
+    $instance['facebook_url'] = strip_tags(!empty($new_instance['facebook_url']) ? $new_instance['facebook_url'] : '');
       return $instance;
   }
   function form($instance) {
@@ -38,15 +43,17 @@ class FBLikeBallooneWidgetItem extends WP_Widget {
       $instance = array(
         'title_facebook_page_like' => null,
         'text_facebook_page_like' => null,
+        'facebook_url' => null,
       );
     }
     $title = esc_attr($instance['title_facebook_page_like']);
     $text = esc_attr($instance['text_facebook_page_like']);
+    $facebook_url = esc_attr($instance['facebook_url']);
     ?>
     <?php //タイトル入力フォーム ?>
     <p>
       <label for="<?php echo $this->get_field_id('title_facebook_page_like'); ?>">
-      <?php _e( 'タイトル（未入力で非表示）', THEME_NAME ) ?>
+      <?php _e( 'タイトル', THEME_NAME ) ?>
       </label>
       <input class="widefat" id="<?php echo $this->get_field_id('title_facebook_page_like'); ?>" name="<?php echo $this->get_field_name('title_facebook_page_like'); ?>" type="text" value="<?php echo $title; ?>" />
     </p>
@@ -59,7 +66,15 @@ class FBLikeBallooneWidgetItem extends WP_Widget {
           $text = sprintf( __( 'この記事をお届けした<br>%sの最新ニュース情報を、<br><span class="bold-red">いいね</span>してチェックしよう！', THEME_NAME ), get_bloginfo('name') );
         }?>
       </label>
-      <textarea class="widefat" id="<?php echo $this->get_field_id('text_facebook_page_like'); ?>" name="<?php echo $this->get_field_name('text_facebook_page_like'); ?>" cols="20" rows="16"><?php echo $text; ?></textarea>
+      <textarea class="widefat" id="<?php echo $this->get_field_id('text_facebook_page_like'); ?>" name="<?php echo $this->get_field_name('text_facebook_page_like'); ?>" cols="20" rows="4"><?php echo $text; ?></textarea>
+    </p>
+    <?php //FacebookページURL ?>
+    <p>
+      <label for="<?php echo $this->get_field_id('facebook_url'); ?>">
+        <?php _e( 'FacebookページURL', THEME_NAME ) ?>
+      </label>
+      <input class="widefat" id="<?php echo $this->get_field_id('facebook_url'); ?>" name="<?php echo $this->get_field_name('facebook_url'); ?>" type="text" value="<?php echo $facebook_url; ?>" placeholder="<?php _e( 'https://www.facebook.com/XXXXXXX', THEME_NAME ) ?>" /><br>
+      <?php _e( '※未入力だとプロフィールページで設定されているものを利用。', THEME_NAME ) ?>
     </p>
     <?php
   }
--- tmp/fb-like-balloon.php
+++ tmp/fb-like-balloon.php
@@ -1,22 +1,30 @@
-<?php //フェイスブックページのURLを設定↓
-$facebook_page_url = 'https://www.facebook.com/' . get_facebook_follow_id(); ?>
-<div class="article-like">
-  <div class="article-like-thumb">
+<?php //フェイスブックバルーン ?>
+<div class="fb-like-balloon">
+  <div class="fb-like-balloon-thumb">
     <?php if ( has_post_thumbnail() ): // サムネイルを持っているとき ?>
-      <?php the_post_thumbnail( 'thumb100', array('class' => 'article-like-entry-thumnail', 'alt' => '') ); ?>
+      <?php the_post_thumbnail( 'thumb100', array('class' => 'fb-like-balloon-entry-thumnail', 'alt' => '') ); ?>
     <?php else: // サムネイルを持っていない ?>
-      <img src="<?php echo get_template_directory_uri();//get_stylesheet_directory_uri();子テーマに画像を置いた場合 ?>/images/no-image.png" alt="NO IMAGE" class="article-like-entry-thumnail no-image" />
+      <img src="<?php echo get_template_directory_uri();//get_stylesheet_directory_uri();子テーマに画像を置いた場合 ?>/images/no-image.png" alt="NO IMAGE" class="fb-like-balloon-entry-thumnail no-image" />
     <?php endif; ?>
   </div>
-  <div class="article-like-arrow-box">
-    <div class="article-like-arrow-box-in">
-      <div class="article-like-button">
-        <div class="fb-like fb-like-pc" data-href="<?php echo $facebook_page_url; ?>" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
+  <div class="fb-like-balloon-arrow-box">
+    <div class="fb-like-balloon-arrow-box-in">
+      <div class="fb-like-balloon-button">
+        <div class="fb-like fb-like-pc" data-href="<?php echo $_FACEBOOK_URL; ?>" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
 
-        <div class="fb-like fb-like-mobile" data-href="<?php echo $facebook_page_url; ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+        <div class="fb-like fb-like-mobile" data-href="<?php echo $_FACEBOOK_URL; ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.11&appId=569002053185714';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
 
       </div>
-      <div class="article-like-body">
+      <div class="fb-like-balloon-body">
         <?php //メッセージの呼び出し
           echo $_FACEBOOK_PAGE_LIKE_TEXT; ?>
       </div>
