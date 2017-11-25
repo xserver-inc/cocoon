7b5ad34ded1821e18433199db7f3050d471120de
yhira <info@nelog.jp>
2017-11-25 18:10:04 +0900

    CSS縮小化でコード出現順番を保つように修正

--- lib/page-speed-up/minfify-css.php
+++ lib/page-speed-up/minfify-css.php
@@ -6,6 +6,137 @@
 ///////////////////////////////////////
 if ( !function_exists( 'tag_code_to_mintify_css' ) ):
 function tag_code_to_mintify_css($buffer) {
+
+  if (is_css_mintify_enable()) {
+    //最終出力縮小化CSSコード
+    $last_minfified_css = null;
+
+    //CSSファイル
+    $link_pattern = '<link[^>]+?stylesheet[^>]+?href=[\'"]([^\'"]+?)[\'"][^>]*?>';
+    $style_pattern = '<style[^>]*?>(.*?)</style>';
+    $pattern = '{'.$link_pattern.'|'.$style_pattern.'}is';
+
+    $subject = $buffer;
+    $res = preg_match_all($pattern, $subject, $m);
+    $all = 0;
+    $file = 1;
+    $code = 2;
+    if ($res && isset($m[$all], $m[$file], $m[$code])) {
+      //_v($m);
+      $i = 0;
+
+      //return $buffer;
+      foreach ($m[$all] as $match) {
+        //CSS用のタグ
+        $tag = $match;
+        //CSSファイルURL
+        $url = $m[$file][$i];
+        //_v($url);
+        //CSSコード
+        $css_inline_code = $m[$code][$i];
+        //_v($css_inline_code);
+
+        ++$i;
+
+        ////////////////////////////////
+        //ファイルタイプのCSSのとき
+        ////////////////////////////////
+        if ($url) {
+          //サイトのURLが含まれているものだけ処理
+          if (strpos($url, site_url()) !== false) {
+            if (
+              //アドミンバースタイルは除外
+              (strpos($url, 'admin-bar.min.css') !== false) ||
+              //ダッシュアイコンは除外
+              (strpos($url, 'dashicons.min.css') !== false)) {
+              continue;
+            }
+            //?var=4.9のようなURLクエリを除去(remove_query_arg( 'ver', $url ))
+            $url = preg_replace('/\?.*$/m', '', $url);
+            //_v($url);//CSSコード変換するURL
+
+            //CSS URLからCSSコードの取得
+            $css = css_url_to_css_mintify_code( $url );
+            //縮小化可能ななCSSだと時
+            if ($css) {
+              //_v($css);//変換したCSSコード
+
+              //CSSを縮小化したCSSファイルURL linkタグを削除する
+              $buffer = str_replace($tag, '', $buffer);
+
+
+              $last_minfified_css .= $css;
+            }
+
+          }//strpos($url, site_url()) !== false
+        }//$url
+
+        ////////////////////////////////
+        //CSSがソースコードのとき
+        ////////////////////////////////
+        if ($css_inline_code) {
+          //除外インラインCSSコード（プリント用のスタイルは除外）
+          if (preg_match('{media=[\'"].*?print.*?[\'"]}i', $tag)) {
+            continue;
+          }
+
+          //空の場合は除外
+          if (empty($css_inline_code)) {
+            continue;
+          }
+          //最終出力縮小化CSSコードに縮小化したCSSコードを加える
+          $last_minfified_css .= minify_css($css_inline_code);
+          //ヘッダー出力コードからstyleタグを削除
+          $buffer = str_replace($tag, '', $buffer);
+          //_v($match);
+        }//$css
+
+
+      }//foreach
+    }
+    //_v($m);
+
+    /*
+    //CSSインラインスタイル
+    $pattern = '{<style[^>]*?>(.*?)</style>}is';
+    $subject = $buffer;
+    $res = preg_match_all($pattern, $subject, $m);
+    //_v($m[1]);
+    if ($res && isset($m[1])) {
+      $i = 0;
+      foreach ($m[1] as $match) {
+        //CSSコード
+        $css = $match;
+        //CSSタグ（<style type="text/css"></style>）
+        $style_tag = $m[0][$i];
+        //_v($style_tag);
+        ++$i;
+
+        //除外インラインCSSコード（プリント用のスタイルは除外）
+        if (preg_match('{media=[\'"].*?print.*?[\'"]}i', $style_tag)) {
+          continue;
+        }
+
+        //空の場合は除外
+        if (empty($css)) {
+          continue;
+        }
+        //最終出力縮小化CSSコードに縮小化したCSSコードを加える
+        $last_minfified_css .= minify_css($css);
+        //ヘッダー出力コードからstyleタグを削除
+        $buffer = str_replace($style_tag, '', $buffer);
+        //_v($match);
+      }
+    }
+    */
+
+    //縮小化したCSSをデータの最後に付け加える
+    if ($last_minfified_css) {
+      $buffer = $buffer.PHP_EOL.'<style type="text/css">'.$last_minfified_css.'</style>';
+    }
+
+  }//is_css_mintify_enable()
+  /*
   if (is_css_mintify_enable()) {
     //最終出力縮小化CSSコード
     $last_minfified_css = null;
@@ -90,7 +221,8 @@ function tag_code_to_mintify_css($buffer) {
     //縮小化したCSSをデータの最後に付け加える
     $buffer = $buffer.PHP_EOL.'<style type="text/css">'.$last_minfified_css.'</style>';
   }//is_css_mintify_enable()
-
+  */
+  //_v($buffer);
   return $buffer;
 }
 endif;
