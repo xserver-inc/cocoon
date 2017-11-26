24e8c1c2e9729079b9405a34104a44fcde555518
yhira <info@nelog.jp>
2017-11-26 23:46:45 +0900

    関数テキストテーブルの作成

--- lib/page-func-text/func-text-func.php
+++ lib/page-func-text/func-text-func.php
@@ -1,8 +1,10 @@
 <?php //関数テキスト関係の関数
 
+//関数テキストテーブルの作成
+if ( !function_exists( 'create_function_text_table' ) ):
 function create_function_text_table() {
    global $wpdb;
-
+   //_v('$wpdb');
    $sql = "";
    $charset_collate = "";
 
@@ -17,16 +19,20 @@ function create_function_text_table() {
    if ( !empty($wpdb->collate) )
       $charset_collate .= "COLLATE {$wpdb->collate}";
 
-   // SQL文でテーブルを作る
-   $sql = "
-      CREATE TABLE {$table_name} (
-        text TEXT,
-        categories VARCHAR(64),
-        date datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-        modified datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-        PRIMARY KEY  (postid)
-      ) {$charset_collate};";
+    // SQL文でテーブルを作る
+    $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+      modified datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+      text text NOT NULL,
+      #categories varchar(200),
+      PRIMARY KEY (id)
+    ) $charset_collate;";
 
    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-   dbDelta( $sql );
+   _v($sql);
+   $res = dbDelta( $sql );
+   _v($res);
 }
+endif;
+create_function_text_table();
