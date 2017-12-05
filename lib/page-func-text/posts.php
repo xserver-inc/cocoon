29a8f3cd0e598ca94334daa3b9606c0447990e20
yhira <info@nelog.jp>
2017-12-05 22:37:10 +0900

    関数テキスト新規作成時の不具合修正

--- lib/page-func-text/posts.php
+++ lib/page-func-text/posts.php
@@ -20,6 +20,13 @@ if (!empty($_POST['title']) && !empty($_POST['text']) && !empty($_POST['action']
       '%s',
     );
     $result = $wpdb->insert( $table, $data, $format );
+    //_v($wpdb->insert_id);
+    //編集モードに変更
+    if ($result) {
+      $_GET['action'] = 'edit';
+      $_GET['id'] = $wpdb->insert_id;
+      generate_notice_message_tag(__( 'テキストを新規作成しました。', THEME_NAME ));
+    }
     //_v($result);
   } else {
     $id = isset($_POST['id']) ? intval($_POST['id']) : '';
@@ -37,12 +44,15 @@ if (!empty($_POST['title']) && !empty($_POST['text']) && !empty($_POST['action']
       );
       $where_format = array('%d');
       $result = $wpdb->update( $table, $data, $where, $format, $where_format );
+      if ($result) {
+        generate_notice_message_tag(__( 'テキストを更新しました。', THEME_NAME ));
+      }
     }
 
   }
   //設定保存メッセージ
   if ($result) {
-    generate_notice_message_tag(__( '内容を保存しました。', THEME_NAME ));
+    //generate_notice_message_tag(__( '内容を保存しました。', THEME_NAME ));
   } else {
     generate_notice_message_tag(__( 'データベースに保存されませんでした。', THEME_NAME ));
   }
