2facf3e0f48cce4982acdb373fedbd908c1c4734
yhira <info@nelog.jp>
2017-12-09 19:38:57 +0900

    吹き出し入力フォームの作成

---
+++ lib/page-speech-balloon/form.php
@@ -0,0 +1,89 @@
+<p><?php _e( '吹き出し用の設定です。', THEME_NAME ) ?></p>
+<form name="form1" method="post" action="">
+  <?php
+
+  if (isset($_GET['id'])) {
+    $action = 'edit';
+    $id = isset($_GET['id']) ? intval($_GET['id']) : '';
+    $recode = get_speech_balloon($id);
+    $name = $recode->name;
+    $icon = $recode->icon;
+    $style = $recode->style;
+    $posision = $recode->posision;
+
+  } else {
+    $action = 'new';
+    $id = '';
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $icon = isset($_POST['icon']) ? $_POST['icon'] : '';
+    $style = isset($_POST['style']) ? $_POST['style'] : '';
+    $posision = isset($_POST['posision']) ? $_POST['posision'] : '';
+  }?>
+
+  <table class="form-table speech-balloon">
+    <tbody>
+      <tr>
+        <th scope="row">
+          <?php generate_label_tag('name', __( '名前', THEME_NAME )); ?>
+        </th>
+        <td>
+          <?php
+          generate_textbox_tag('name', $name,  __('推奨：6文字以下',THEME_NAME ));
+          generate_tips_tag(__( 'アイコン下に表示される名前を入力してください。', THEME_NAME ));
+          ?>
+        </td>
+      </tr>
+
+      <tr>
+        <th scope="row">
+          <?php generate_label_tag('icon', __( 'アイコン画像', THEME_NAME )); ?>
+        </th>
+        <td>
+          <?php
+          generate_upload_image_tag('icon', $icon);
+          generate_tips_tag(__( 'アイコンとなる画像を選択してください。200pxの正方形画像を設定してください。', THEME_NAME ));
+          ?>
+        </td>
+      </tr>
+
+      <tr>
+        <th scope="row">
+          <?php generate_label_tag('style', __( '吹き出しスタイル', THEME_NAME )); ?>
+        </th>
+        <td>
+          <?php
+          $options = array(
+            SBS_STANDARD => __( 'デフォルト', THEME_NAME ),
+            SBS_FLAT => __( 'フラット', THEME_NAME ),
+            SBS_LINE => __( 'LINE', THEME_NAME ),
+            SBS_THINK => __( '考え事', THEME_NAME ),
+          );
+          generate_selectbox_tag('style', $options, $style);
+          generate_tips_tag(__( '吹き出しのスタイルを設定します。', THEME_NAME ));
+          ?>
+        </td>
+      </tr>
+
+      <tr>
+        <th scope="row">
+          <?php generate_label_tag('posision', __( '人物位置', THEME_NAME )); ?>
+        </th>
+        <td>
+          <?php
+          $options = array(
+            SBP_LEFT => __( '左', THEME_NAME ),
+            SBP_RIGHT => __( '右', THEME_NAME ),
+          );
+          generate_selectbox_tag('posision', $options, $posision);
+          generate_tips_tag(__( 'アイコンを表示するポジションを設定します。', THEME_NAME ));
+          ?>
+        </td>
+      </tr>
+
+    </tbody>
+  </table>
+  <input type="hidden" name="action" value="<?php echo $action; ?>">
+  <input type="hidden" name="id" value="<?php echo $id; ?>">
+  <input type="hidden" name="<?php echo HIDDEN_FIELD_NAME; ?>" value="Y">
+   <?php submit_button(__( '保存', THEME_NAME )); ?>
+</form>
--- lib/page-speech-balloon/speech-balloon-func.php
+++ lib/page-speech-balloon/speech-balloon-func.php
@@ -1,14 +1,27 @@
 <?php //吹き出し関係の関数
 
 //関数テキストテーブルのバージョン
-define('SPEECH_BALLOONS_TABLE_VERSION', '0.0');
+define('SPEECH_BALLOONS_TABLE_VERSION', '0.1');
 define('SPEECH_BALLOONS_TABLE_NAME',  $wpdb->prefix . THEME_NAME . '_speech_balloons');
 
 //関数テキスト移動用URL
 define('SB_LIST_URL',   add_query_arg(array('action' => false,   'id' => false)));
 define('SB_NEW_URL',    add_query_arg(array('action' => 'new',   'id' => false)));
 
+//デフォルトの名前
+define('SB_DEFAULT_NAME', __( '匿名', THEME_NAME ));
+//デフォルトアイコン
+define('SB_DEFAULT_ICON', get_template_directory_uri().'images/man.png');
 
+//スタイル
+define('SBS_STANDARD', 'standard');
+define('SBS_FLAT', 'flat');
+define('SBS_LINE', 'line');
+define('SBS_THINK', 'think');
+
+//サブタイプ
+define('SBP_LEFT', 'L');
+define('SBP_RIGHT', 'R');
 
 //吹き出しテーブルのバージョン取得
 define('OP_SPEECH_BALLOONS_TABLE_VERSION', 'speech_balloons_table_version');
@@ -29,8 +42,8 @@ function create_speech_balloons_table() {
     title varchar(126),
     name varchar(36),
     icon varchar(256),
-    type varchar(20),
-    subtype varchar(20),
+    style varchar(20),
+    position varchar(20),
     PRIMARY KEY (id),
     INDEX (title),
     INDEX (name)
@@ -94,14 +107,12 @@ endif;
 //吹き出しテーブルレコードの取得
 if ( !function_exists( 'get_speech_balloon' ) ):
 function get_speech_balloon( $id ) {
-  // global $wpdb;
-  // //$query = ("SELECT * FROM {$table_name}  WHERE id = {intval($id)}");
-  // $query = $wpdb->prepare("SELECT * FROM {$table_name}  WHERE id = %d", $id);
-  // //_v($query);
-  // $record = $wpdb->get_row( $query );
-
-  $table_name = FUNCTION_TEXTS_TABLE_NAME;
+  $table_name = SPEECH_BALLOONS_TABLE_NAME;
   $record = get_db_table_record( $table_name, $id );
+  $recode->name = !empty($recode->name) ? $recode->name : SB_DEFAULT_NAME;
+  $recode->icon = !empty($recode->icon) ? $recode->icon : SB_DEFAULT_ICON;
+  $recode->style = !empty($recode->style) ? $recode->style : SBS_FLAT;
+  $recode->position = !empty($recode->position) ? $recode->position : SBP_LEFT;
 
   return $record;
 }
