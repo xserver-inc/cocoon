f102d946fc5a39a2b4a6a2a4180f9926227690a7
yhira <info@nelog.jp>
2017-12-12 23:26:18 +0900

    吹き出し編集画面のデモ表示機能追加

--- lib/page-speech-balloon/form.php
+++ lib/page-speech-balloon/form.php
@@ -12,7 +12,21 @@
     $style = $record->style;
     $position = $record->position;
     $iconstyle = $record->iconstyle;
-
+    ?>
+    <div id="sb-list" class="postbox" style="max-width: 800px; margin-top: 20px;">
+      <h2 class="hndle"><?php _e( 'デモ', THEME_NAME ) ?></h2>
+      <div class="inside sb-list">
+        <div class="demo">
+        <?php
+          generate_speech_balloon_tag(
+            $record,
+            'VOICE'
+          );
+        ?>
+        </div>
+      </div>
+    </div>
+    <?php
   } else {
     $action = 'new';
     $id = '';
