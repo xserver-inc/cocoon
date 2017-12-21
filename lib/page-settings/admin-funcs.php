66a327d72eabde795a90f7c9dddc124e23ac4bc1
yhira <info@nelog.jp>
2017-12-21 18:29:03 +0900

    管理者かれる設定項目追加

--- lib/page-settings/admin-funcs.php
+++ lib/page-settings/admin-funcs.php
@@ -24,3 +24,71 @@ function is_admin_editor_counter_visible(){
 }
 endif;
 
+
+///////////////////////////////////////
+// 管理者パネル
+///////////////////////////////////////
+
+//管理者パネルを表示
+define('OP_ADMIN_PANEL_VISIBLE', 'admin_panel_visible');
+if ( !function_exists( 'is_admin_panel_visible' ) ):
+function is_admin_panel_visible(){
+  return get_theme_option(OP_ADMIN_PANEL_VISIBLE, 1);
+}
+endif;
+
+//管理者パネルのPVを表示
+define('OP_ADMIN_PANEL_PV_AREA_VISIBLE', 'admin_panel_pv_area_visible');
+if ( !function_exists( 'is_admin_panel_pv_area_visible' ) ):
+function is_admin_panel_pv_area_visible(){
+  return get_theme_option(OP_ADMIN_PANEL_PV_AREA_VISIBLE, 1);
+}
+endif;
+
+//管理者パネルのPV取得方法
+define('OP_ADMIN_PANEL_PV_TYPE', 'admin_panel_pv_type');
+if ( !function_exists( 'get_admin_panel_pv_type' ) ):
+function get_admin_panel_pv_type(){
+  return get_theme_option(OP_ADMIN_PANEL_PV_TYPE, THEME_NAME);
+}
+endif;
+
+//管理者パネル編集エリアの表示
+define('OP_ADMIN_PANEL_EDIT_AREA_VISIBLE', 'admin_panel_edit_area_visible');
+if ( !function_exists( 'is_admin_panel_edit_area_visible' ) ):
+function is_admin_panel_edit_area_visible(){
+  return get_theme_option(OP_ADMIN_PANEL_EDIT_AREA_VISIBLE, 1);
+}
+endif;
+
+//管理者パネル編集エリアの表示
+define('OP_ADMIN_PANEL_EDIT_AREA_VISIBLE', 'admin_panel_edit_area_visible');
+if ( !function_exists( 'is_admin_panel_edit_area_visible' ) ):
+function is_admin_panel_edit_area_visible(){
+  return get_theme_option(OP_ADMIN_PANEL_EDIT_AREA_VISIBLE, 1);
+}
+endif;
+
+//管理者パネルWordpress編集の表示
+define('OP_ADMIN_PANEL_WP_EDIT_VISIBLE', 'admin_panel_wp_edit_visible');
+if ( !function_exists( 'is_admin_panel_wp_edit_visible' ) ):
+function is_admin_panel_wp_edit_visible(){
+  return get_theme_option(OP_ADMIN_PANEL_WP_EDIT_VISIBLE, 1);
+}
+endif;
+
+//管理者パネルWindows Live Writer編集の表示
+define('OP_ADMIN_PANEL_WLW_EDIT_VISIBLE', 'admin_panel_wlw_edit_visible');
+if ( !function_exists( 'is_admin_panel_wlw_edit_visible' ) ):
+function is_admin_panel_wlw_edit_visible(){
+  return get_theme_option(OP_ADMIN_PANEL_WLW_EDIT_VISIBLE);
+}
+endif;
+
+//管理者パネルAMPエリアの表示
+define('OP_ADMIN_PANEL_AMP_AREA_VISIBLE', 'admin_panel_amp_area_visible');
+if ( !function_exists( 'is_admin_panel_amp_area_visible' ) ):
+function is_admin_panel_amp_area_visible(){
+  return get_theme_option(OP_ADMIN_PANEL_AMP_AREA_VISIBLE, 1);
+}
+endif;
--- lib/page-settings/admin-posts.php
+++ lib/page-settings/admin-posts.php
@@ -8,3 +8,30 @@ update_theme_option(OP_CONFIRMATION_BEFORE_PUBLISH);
 
 //タイトル等の文字数カウンター表示
 update_theme_option(OP_ADMIN_EDITOR_COUNTER_VISIBLE);
+
+///////////////////////////////////////
+// 管理者パネル
+///////////////////////////////////////
+//管理者パネルを表示
+update_theme_option(OP_ADMIN_PANEL_VISIBLE);
+
+//管理者パネルのPVを表示
+update_theme_option(OP_ADMIN_PANEL_PV_AREA_VISIBLE);
+
+//管理者パネルのPV取得方法
+update_theme_option(OP_ADMIN_PANEL_PV_TYPE);
+
+//管理者パネル編集エリアの表示
+update_theme_option(OP_ADMIN_PANEL_EDIT_AREA_VISIBLE);
+
+//管理者パネル編集エリアの表示
+update_theme_option(OP_ADMIN_PANEL_EDIT_AREA_VISIBLE);
+
+//管理者パネルWordpress編集の表示
+update_theme_option(OP_ADMIN_PANEL_WP_EDIT_VISIBLE);
+
+//管理者パネルWindows Live Writer編集の表示
+update_theme_option(OP_ADMIN_PANEL_WLW_EDIT_VISIBLE);
+
+//管理者パネルAMPエリアの表示
+update_theme_option(OP_ADMIN_PANEL_AMP_AREA_VISIBLE);
