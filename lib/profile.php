8aa01fad586b26ff68f5c600b698e8810cc056e4
yhira <info@nelog.jp>
2017-12-22 21:42:44 +0900

    プロフィール設定部分のリファクタリング

--- lib/profile.php
+++ lib/profile.php
@@ -1,9 +1,15 @@
 <?php //プロフィールプロフィールに関連する関数
 
+///////////////////////////////////////
+// 自前でプロフィール画像のアップロード
+///////////////////////////////////////
 //プロフィール画面で設定したプロフィール画像
-if ( !function_exists( 'get_the_author_upladed_avatar' ) ):
-function get_the_author_upladed_avatar(){
-  return esc_html(get_the_author_meta('upladed_avatar', get_the_posts_author_id()));
+if ( !function_exists( 'get_the_author_upladed_avatar_url' ) ):
+function get_the_author_upladed_avatar_url($user_id){
+  if (!$user_id) {
+    $user_id = get_the_posts_author_id();
+  }
+  return esc_html(get_the_author_meta('upladed_avatar', $user_id));
 }
 endif;
 
@@ -21,9 +27,9 @@ function add_avatar_to_user_profile($user) {
       </th>
       <td>
       <?php
-        generate_upload_image_tag('upladed_avatar', get_the_author_upladed_avatar());
+        generate_upload_image_tag('upladed_avatar', get_the_author_upladed_avatar_url($user->ID));
        ?>
-       <p class="description"><?php _e( '自前でプロフィール画像をアップロードする場合は画像を選択してください。Gravatarよりこちらのプロフィール画像が優先されます。300×300以上の正方形の画像がお勧めです。', THEME_NAME ) ?></p>
+       <p class="description"><?php _e( '自前でプロフィール画像をアップロードする場合は画像を選択してください。Gravatarよりこちらのプロフィール画像が優先されます。300×300以上の正方形の画像がお勧めです。', THEME_NAME ) ?><?php _e( 'ページサイズ縮小のため<a href="https://tinypng.com/" target="_blank">TinyPNG</a>等で登録前にで圧縮することをおすすめします。', THEME_NAME ) ?></p>
       </td>
     </tr>
   </table>
@@ -38,5 +44,29 @@ function update_avatar_to_user_profile($user_id) {
   if ( current_user_can('edit_user',$user_id) ){
     update_user_meta($user_id, 'upladed_avatar', $_POST['upladed_avatar']);
   }
+}
+endif;
+
+//プロフィール画像を変更する
+add_filter( 'get_avatar' , 'get_uploaded_user_profile_avatar' , 1 , 5 );
+if ( !function_exists( 'get_uploaded_user_profile_avatar' ) ):
+function get_uploaded_user_profile_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
+  if ( is_numeric( $id_or_email ) )
+    $user_id = (int) $id_or_email;
+  elseif ( is_string( $id_or_email ) && ( $user = get_user_by( 'email', $id_or_email ) ) )
+    $user_id = $user->ID;
+  elseif ( is_object( $id_or_email ) && ! empty( $id_or_email->user_id ) )
+    $user_id = (int) $id_or_email->user_id;
+
+  if ( empty( $user_id ) )
+    return $avatar;
+
+  if (get_the_author_upladed_avatar_url($user_id)) {
+    $alt = !empty($alt) ? $alt : get_the_author_meta( 'display_name', $user_id );;
+    $author_class = is_author( $user_id ) ? ' current-author' : '' ;
+    $avatar = "<img alt='" . esc_attr( $alt ) . "' src='" . esc_url( get_the_author_upladed_avatar_url($user_id) ) . "' class='avatar avatar-{$size}{$author_class} photo' height='{$size}' width='{$size}' />";
+  }
+
+  return $avatar;
 }
 endif;
