90fa32393fd2a086e872fafac97d9689c7cc754a
yhira <info@nelog.jp>
2018-01-11 23:26:42 +0900

    AMPページにjson-ldが出ていなかった不具合修正

--- tmp/amp-header.php
+++ tmp/amp-header.php
@@ -45,7 +45,7 @@ if (!is_site_font_family_local()) {
 }
  ?>
 <?php //JSON-LDの読み込み
-get_template_part('json-ld'); ?>
+get_template_part('tmp/json-ld'); ?>
 <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 
 <?php //AMP用スタイルの出力
