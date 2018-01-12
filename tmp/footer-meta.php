087e9eee801096f992ba0dee86fd0065fc1253a8
yhira <info@nelog.jp>
2018-01-12 23:26:57 +0900

    著者部分の構造化マークアップ変更

--- tmp/footer-meta.php
+++ tmp/footer-meta.php
@@ -1,6 +1,10 @@
 <?php //本文下部分、投稿者など ?>
 <div class="footer-meta">
-  <span class="post-author vcard author" itemprop="editor author creator copyrightHolder" itemscope itemtype="http://schema.org/Person">
-    <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="author-link" itemprop="name"><?php the_author(); ?></a>
-  </span>
+  <div class="author-info">
+    <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="author-link">
+      <span class="post-author vcard author" itemprop="editor author creator copyrightHolder" itemscope itemtype="http://schema.org/Person">
+        <span class="author-name" itemprop="name"><?php the_author(); ?></span>
+      </span>
+    </a>
+  </div>
 </div>
