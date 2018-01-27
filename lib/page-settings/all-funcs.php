4a6cc53e3bd7f0695d76985a19d1a7d7e89720f4
yhira <info@nelog.jp>
2018-01-28 00:45:32 +0900

    デフォルトのファビコンやアップルタッチアイコンの実装

--- lib/page-settings/all-funcs.php
+++ lib/page-settings/all-funcs.php
@@ -108,30 +108,61 @@ function get_site_icon_url2(){
   return ;
 }
 endif;
+
 if ( !function_exists( 'get_default_site_icon_url' ) ):
 function get_default_site_icon_url(){
   return get_template_directory_uri().'/images/site-icon.png';
 }
 endif;
 
-//サイトアイコンの設定
-// add_action( 'wp_head', 'the_site_icon_tag' );
-// add_action( 'admin_print_styles', 'the_site_icon_tag' );
-if ( !function_exists( 'the_site_icon_tag' ) ):
-function the_site_icon_tag(){
-  if (get_site_icon_url2()) {
-    $url = get_site_icon_url2();
-  } else {
-    $url = get_default_site_icon_url();
-  }
-  if ($url) {
-    $tag = '<!-- '.THEME_NAME_CAMEL.' site icon -->'.PHP_EOL;
-    $tag .= '<link rel="shortcut icon" href="'.$url.'">'.PHP_EOL;
-    $tag .= '<link rel="apple-touch-icon" href="'.$url.'">'.PHP_EOL;
-    $tag .= '<meta name="msapplication-TileImage" content="'.$url.'">'.PHP_EOL;
-    echo $tag;
-  }
+//デフォルトサイトアイコンの設定
+if (!get_site_icon_url()) {
+  add_action( 'wp_head', 'add_default_site_icon_tag' );
+  add_action( 'admin_print_styles', 'add_default_site_icon_tag' );
+}
+if ( !function_exists( 'add_default_site_icon_tag' ) ):
+function add_default_site_icon_tag(){
+  $tag = '<!-- '.THEME_NAME_CAMEL.' site icon -->'.PHP_EOL;
+  $tag .= '<link rel="icon" href="'.get_template_directory_uri().'images/site-icon32x32.png'.'" sizes="32x32" />'.PHP_EOL;
+  $tag .= '<link rel="icon" href="'.get_template_directory_uri().'/images/site-icon192x192.png'.'" sizes="192x192" />'.PHP_EOL;
+  $tag .= '<link rel="apple-touch-icon" href="'.get_template_directory_uri().'/images/site-icon180x180.png'.'" />'.PHP_EOL;
+  $tag .= '<meta name="msapplication-TileImage" content="'.get_template_directory_uri().'/images/site-icon270x270.png'.'" />'.PHP_EOL;
+  echo $tag;
 }
 endif;
 
+// //サイトアイコンの設定
+// // add_action( 'wp_head', 'the_site_icon_tag' );
+// // add_action( 'admin_print_styles', 'the_site_icon_tag' );
+// if ( !function_exists( 'the_site_icon_tag' ) ):
+// function the_site_icon_tag(){
+//   if (get_site_icon_url2()) {
+//     $url = get_site_icon_url2();
+//   } else {
+//     $url = get_default_site_icon_url();
+//   }
+//   if ($url) {
+//     $tag = '<!-- '.THEME_NAME_CAMEL.' site icon -->'.PHP_EOL;
+//     $tag .= '<link rel="icon" href="'.$url.'">'.PHP_EOL;
+//     $tag .= '<link rel="apple-touch-icon" href="'.$url.'">'.PHP_EOL;
+//     $tag .= '<meta name="msapplication-TileImage" content="'.$url.'">'.PHP_EOL;
+//     echo $tag;
+//   }
+// }
+// endif;
 
+// //サイトアイコン出力の変更
+// add_filter('site_icon_meta_tags', 'filter_site_icon_meta_tags');
+// function filter_site_icon_meta_tags($meta_tags) {
+//   //_v(empty($meta_tags));
+//   if (empty($meta_tags)) {
+//     $url = get_default_site_icon_url();
+//     $meta_tags = array(
+//       '<link rel="icon" href="'.$url.'">',
+//       '<link rel="apple-touch-icon" href="'.$url.'">',
+//       '<meta name="msapplication-TileImage" content="'.$url.'">',
+//     );
+//    //_v($meta_tags);
+//   }
+//   return $meta_tags;
+// }
