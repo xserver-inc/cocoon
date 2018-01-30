e364860c53c5e602fbc5bbefb21b4f863eeedc7c
yhira <info@nelog.jp>
2018-01-31 08:52:42 +0900

    著者情報が取得できない場合のフッター部分のautorリンク変更

--- tmp/footer-meta.php
+++ tmp/footer-meta.php
@@ -1,9 +1,17 @@
-<?php //本文下部分、投稿者など ?>
+<?php //本文下部分、投稿者など
+if (get_the_author()) {
+  $url = get_author_posts_url( get_the_author_meta( 'ID' ) );
+  $name = get_the_author();
+} else {
+  $url = site_url();
+  $name = get_bloginfo('name');
+}
+ ?>
 <div class="footer-meta">
   <div class="author-info">
-    <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="author-link">
+    <a href="<?php echo $url; ?>" class="author-link">
       <span class="post-author vcard author" itemprop="editor author creator copyrightHolder" itemscope itemtype="http://schema.org/Person">
-        <span class="author-name" itemprop="name"><?php the_author(); ?></span>
+        <span class="author-name" itemprop="name"><?php echo $name; ?></span>
       </span>
     </a>
   </div>
