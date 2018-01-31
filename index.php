5a85dc9d202e8cb420a39460aa99ef2e40add599
yhira <info@nelog.jp>
2018-01-31 19:41:33 +0900

    コメント修正

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
 //一覧表示
 ///////////////////////
 if (!is_user_agent_live_writer()) {
-  //一般的なサイト表示
+  //通常表示
   get_template_part('tmp/list');
 } else {
-  //Windows Live Writer・Open Live Writerでテーマ取得の際
+  //ブログエディターLive Writerでテーマ取得の際
   get_template_part('tmp/live-writer');
 }
 ?>
-
 
 <?php get_footer(); ?>
