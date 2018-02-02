6d9a513006d2afb74ef547018dd07cb38ce2162b
yhira <info@nelog.jp>
2018-02-02 22:57:19 +0900

    テーマ自動アップデート用の処理

--- lib/update.php
+++ lib/update.php
@@ -1,27 +1,12 @@
 <?php //アップデート関係の処理
 
-
 //アップデートチェックの初期化
 require 'theme-update-checker.php'; //ライブラリのパス
 $example_update_checker = new ThemeUpdateChecker(
   strtolower(THEME_NAME), //テーマフォルダ名
-  'http://example.com/example-theme/update-info.json' //JSONファイルのURL
+  'https://wp-cocoon.com/wp-content/themes/cocoon-master/update-info.json' //JSONファイルのURL
 );
 
-// // function myplugin_activate() {
-// //     _v(THEME_NAME.'がアップデートされました');
-// // }
-// // register_activation_hook( __FILE__, 'myplugin_activate' );
-
-// add_action( 'after_switch_theme', 'after_switch_theme_custom' );
-// if ( !function_exists( 'after_switch_theme_custom' ) ):
-// function after_switch_theme_custom(){
-//   if ( function_exists( '_v' ) ):
-//     _v(THEME_NAME.'がアップデートされました');
-//   endif;
-//   update_function_texts_table();
-// }
-// endif;
 
 //テーマアップデート時の処理
 add_action( 'upgrader_process_complete', 'upgrader_process_complete_custom', 10, 2 );
