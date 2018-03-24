80a7a097f72851644492978ba0d63c768ca3e6b5
yhira <info@nelog.jp>
2018-03-24 19:10:25 +0900

    テーマ設定のリセットができない不具合修正

--- lib/page-settings/reset-funcs.php
+++ lib/page-settings/reset-funcs.php
@@ -4,7 +4,9 @@ define('OP_RESET_ALL_SETTINGS', 'reset_all_settings');
 if ( !function_exists( 'reset_all_settings' ) ):
 function reset_all_settings(){
   global $wpdb;
-  $wpdb->delete( 'wp_options', array( 'option_name' => 'theme_mods_'.THEME_NAME ) );
+  //テーマフォルダ名の取得
+  $dir_name = str_replace(get_theme_root_uri().'/', '', get_stylesheet_directory_uri());
+  $wpdb->delete( 'wp_options', array( 'option_name' => 'theme_mods_'.$dir_name  ) );
 }
 endif;
 
