8dddfe885e1e793f01293481dbe75e0feb4d4f30
yhira <info@nelog.jp>
2018-04-07 08:53:36 +0900

    関連記事の設定説明文変更

--- lib/page-settings/single-forms.php
+++ lib/page-settings/single-forms.php
@@ -162,16 +162,16 @@
           </td>
         </tr>
 
-        <!-- 最大抜粋文字数 -->
+        <!-- 最大自動生成抜粋文字数 -->
         <tr>
           <th scope="row">
-            <?php generate_label_tag(OP_RELATED_EXCERPT_MAX_LENGTH, __('最大抜粋文字数', THEME_NAME) ); ?>
+            <?php generate_label_tag(OP_RELATED_EXCERPT_MAX_LENGTH, __('最大自動生成抜粋文字数', THEME_NAME) ); ?>
           </th>
           <td>
             <?php
             generate_number_tag(OP_RELATED_EXCERPT_MAX_LENGTH,  get_related_excerpt_max_length(), 120, 0, 500);
-            generate_tips_tag(__( '「エントリーカード」で、抜粋文を表示する場合の最大文字数を
-              設定します。（最小：0、最大：500）', THEME_NAME ));
+            generate_tips_tag(__( '「関連記事エントリーカード」で、「本文から自動生成される抜粋文」を表示する場合の最大文字数を
+              設定します。※投稿編集画面の抜粋文ではありません。（最小：0、最大：500）', THEME_NAME ));
             ?>
           </td>
         </tr>
