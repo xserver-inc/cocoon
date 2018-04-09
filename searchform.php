2f1a715ad1d7edbc5b35eb56bdfe2b32e2e026a5
yhira <info@nelog.jp>
2018-04-09 17:16:18 +0900

    検索ボックスのプレースホルダ変更

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <?php if (!is_amp()): ?>
 <form method="get" class="search-box" action="<?php echo home_url('/'); ?>">
-  <input type="text" placeholder="<?php _e( 'ブログ内を検索', THEME_NAME ) ?>" name="s" class="search-edit">
+  <input type="text" placeholder="<?php _e( 'サイト内を検索', THEME_NAME ) ?>" name="s" class="search-edit">
   <button type="submit" class="search-submit"></button>
 </form>
 <?php endif ?>
