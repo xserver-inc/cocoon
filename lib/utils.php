b5f2be2b8f496470bfee3fe019cc4d1bd37abdd7
yhira <info@nelog.jp>
2018-04-20 20:21:51 +0900

    ブログカードのドメイン名取得方法変更

--- lib/utils.php
+++ lib/utils.php
@@ -814,10 +814,12 @@ endif;
 //サイトのドメインを取得
 if ( !function_exists( 'get_the_site_domain' ) ):
 function get_the_site_domain(){
-  // // //ドメイン情報を$results[1]に取得する
-  // preg_match( '/https?:\/\/(.+?)\//i', admin_url(), $results );
-  // return $results[1];
-  return get_domain_name(home_url());
+  //ドメイン情報を$results[1]に取得する
+  if (preg_match( '/https?:\/\/(.+?)\//i', admin_url(), $results )) {
+    return $results[1];
+  } else {
+    return get_domain_name(home_url());
+  }
 }
 endif;
 
