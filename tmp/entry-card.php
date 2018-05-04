b7a8a3c728435437f22d28197ea58aec2d072a63
yhira <info@nelog.jp>
2018-05-05 00:04:40 +0900

    エントリーカードサムネイル表示の不具合修正

--- tmp/entry-card.php
+++ tmp/entry-card.php
@@ -1,8 +1,11 @@
 <a href="<?php the_permalink(); ?>" class="entry-card-wrap a-wrap cf" title="<?php the_title() ?>">
   <article id="post-<?php the_ID(); ?>" class="post-<?php the_ID(); ?> entry-card e-card cf">
     <figure class="entry-card-thumb card-thumb">
-      <?php if ( has_post_thumbnail() ): // サムネイルを持っているとき?>
-        <?php the_post_thumbnail(get_entry_card_thumbnail_size() , array('class' => 'entry-card-thumb-image card-thumb-image', 'alt' => '') ); ?>
+      <?php //サムネイルタグを取得
+      $thumbnail_tag = get_the_post_thumbnail(get_the_ID(), get_entry_card_thumbnail_size() , array('class' => 'entry-card-thumb-image card-thumb-image', 'alt' => ''));
+      if ( has_post_thumbnail() && $thumbnail_tag ): // サムネイルを持っているとき?>
+        <?php echo $thumbnail_tag;
+        //the_post_thumbnail(get_entry_card_thumbnail_size() , array('class' => 'entry-card-thumb-image card-thumb-image', 'alt' => '') ); ?>
       <?php else: // サムネイルを持っていないとき ?>
         <img src="<?php echo get_template_directory_uri(); ?>/images/no-image-320.png" alt="NO IMAGE" class="entry-card-thumb-image no-image list-no-image" />
       <?php endif; ?>
