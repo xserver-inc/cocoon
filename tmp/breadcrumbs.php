8d2fa9c72f83367b50c291d00088a9a1ca509249
yhira <info@nelog.jp>
2018-05-06 22:04:11 +0900

    パンくずリスト修正

--- tmp/breadcrumbs.php
+++ tmp/breadcrumbs.php
@@ -12,8 +12,15 @@ if($cat && !is_wp_error($cat)){
     $echo = '<div class="breadcrumb-item" itemscope itemtype="http://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-folder fa-fw"></span><a href="'.get_category_link($par->term_id).'" itemprop="item"><span itemprop="name">'.$par->name.'</span></a><meta itemprop="position" content="'.$count.'" /><span class="sp"><span class="fa fa-angle-right"></span></span></div>'.$echo;
     $par = get_category($par->parent);
   }
+  // 親カテゴリまで表示する場合
   ++$count;
   echo $echo.'<div class="breadcrumb-item" itemscope itemtype="http://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-folder fa-fw"></span><a href="'.get_category_link($cat[0]->term_id).'" itemprop="item"><span itemprop="name">'.$cat[0]->name.'</span></a><meta itemprop="position" content="'.$count.'" /></div>';
+
+  // // ページ名まで表示する場合
+  // ++$count;
+  // echo $echo.'<div class="breadcrumb-item" itemscope itemtype="http://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-folder fa-fw"></span><a href="'.get_category_link($cat[0]->term_id).'" itemprop="item"><span itemprop="name">'.$cat[0]->name.'</span></a><meta itemprop="position" content="'.$count.'" /><span class="sp"><span class="fa fa-angle-right"></span></span></div>';
+  // ++$count;
+  // echo '<div class="breadcrumb-item" itemscope itemtype="http://schema.org/ListItem" itemprop="itemListElement"<span class="fa fa-file-o fa-fw"></span><span itemprop="name">'.get_the_title().'</span><meta itemprop="position" content="'.$count.'" /></div>';
   echo '</div><!-- /#breadcrumb -->';
 }
 endif //is_single_breadcrumbs_visible ?>
