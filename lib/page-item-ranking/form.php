440788ce7893dcc5649a376459888e14972fd761
yhira <info@nelog.jp>
2018-05-28 19:59:20 +0900

    ランキングデモのCSS調整

--- lib/page-item-ranking/form.php
+++ lib/page-item-ranking/form.php
@@ -16,7 +16,7 @@ if ( $id && ($action == 'item_delete') && isset($_GET['del_no']) && isset($_GET[
  ?>
 <?php if ($id): ?>
   <p class="preview-label" style="max-width: 1000px;"><?php _e( 'プレビュー', THEME_NAME ) ?></p>
-  <div class="demo" style="width: 1000px; height: 400px;margin-bottom: 2em;resize: both;">
+  <div class="demo" style="width: 1000px; height: 460px;margin-bottom: 2em;resize: both;">
   <?php generate_item_ranking_tag($id); ?>
   </div>
 <?php endif ?>
