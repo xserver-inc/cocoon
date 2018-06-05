f76b762f95904815648c9fa65fd02a15222d6255
yhira <info@nelog.jp>
2018-06-05 22:53:05 +0900

    本文中JavaScript読み込みの修正

--- lib/content.php
+++ lib/content.php
@@ -7,16 +7,18 @@ add_filter('the_content', 'get_the_content_all_scripts', 9999);
 if ( !function_exists( 'get_the_content_all_scripts' ) ):
 function get_the_content_all_scripts($the_content) {
   global $_THE_CONTENT_SCRIPTS;
-  if (preg_match_all('{<script.*?>(.+?)</script>}is', $the_content, $m)) {
+  if (preg_match_all('{<script.*?>(.*?)</script>}is', $the_content, $m)) {
     $all_index = 0;
     $js_index = 1;
     $i = 0;
     foreach ($m[$all_index] as $script) {
       $js_code = $m[$js_index][$i];
-      //本文中のスクリプトをまとめる
-      $_THE_CONTENT_SCRIPTS .= $js_code;
-      //まとめたスクリプトは取り除く
-      $the_content = str_replace($script, '', $the_content);
+      if (trim($js_code)) {
+        //本文中のスクリプトをまとめる
+        $_THE_CONTENT_SCRIPTS .= $js_code;
+        //まとめたスクリプトは取り除く
+        $the_content = str_replace($script, '', $the_content);
+      }
 
       $i++;
     }
