39625dd3b30a709665c1aabff7cb3f4dcec8cb2b
yhira <info@nelog.jp>
2018-06-26 17:39:28 +0900

    オリジナルスキン関係の内容を変更

--- lib/page-settings/skin-forms.php
+++ lib/page-settings/skin-forms.php
@@ -98,11 +98,11 @@
         <!-- 同梱スキン募集中 -->
         <tr>
           <th scope="row">
-            <?php generate_label_tag('', __('同梱スキン募集中', THEME_NAME) ); ?>
+            <?php generate_label_tag('', __('オリジナルスキン', THEME_NAME) ); ?>
           </th>
           <td>
-            <p><?php _e( 'もし「良いスキンができたからCocoonに同梱してもいいよ」という方がおられましたらご連絡ください。', THEME_NAME ) ?></p>
-            <p><?php _e( '詳しくはこちら', THEME_NAME ) ?> <span class="fa fa-arrow-right"></span> <a href="https://wp-cocoon.com/skin-make/" target="_blanl"><?php _e( 'スキンの同梱配布について', THEME_NAME ) ?></a></p>
+            <p><?php _e( 'もしスキンを作成された際には、是非ご連絡ください。サイトで紹介させていただければと思います。', THEME_NAME ) ?></p>
+            <p><?php _e( '詳しくはこちら', THEME_NAME ) ?> <span class="fa fa-arrow-right"></span> <a href="https://wp-cocoon.com/skin-make/" target="_blanl"><?php _e( 'オリジナルスキンのCocoonサイト紹介について', THEME_NAME ) ?></a></p>
           </td>
         </tr>
 
