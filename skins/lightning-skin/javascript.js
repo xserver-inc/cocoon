57c4e0a5accdcfd59210cd199f8cba5ce97de474
yhira <info@nelog.jp>
2018-06-26 19:03:10 +0900

    スキンの縮小化修正

--- skins/lightning-skin/javascript.js
+++ skins/lightning-skin/javascript.js
@@ -1,1 +1,19 @@
-jQuery(function(){jQuery("a[href^=#]").click(function(){var r=jQuery(this).attr("href"),t=jQuery("#"==r||""==r?"html":r),e=t.offset().top;return jQuery("body,html").animate({scrollTop:e},400,"swing"),!1})});
+//ここにスキンで利用するJavaScriptを記入する
+
+//スムーズスクロール
+jQuery(function(){
+   // #で始まるアンカーをクリックした場合に処理
+   jQuery('a[href^=#]').click(function() {
+	  // スクロールの速度
+	  var speed = 400; // ミリ秒
+	  // アンカーの値取得
+	  var href= jQuery(this).attr("href");
+	  // 移動先を取得
+	  var target = jQuery(href == "#" || href == "" ? 'html' : href);
+	  // 移動先を数値で取得
+	  var position = target.offset().top;
+	  // スムーススクロール
+	  jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
+	  return false;
+   });
+});
