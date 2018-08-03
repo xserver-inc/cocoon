2d62242f9f354ede62f0f50ec6b2608120638a9a
yhira <info@nelog.jp>
2018-08-03 20:40:41 +0900

    Gutenberg用CSSの調整

--- js/gutenberg.js
+++ js/gutenberg.js
@@ -5,6 +5,10 @@
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
 (function($){
+  //タイマーを使ったあまり美しくない方法
+  //tiny_mce_before_initフック引数配列のbody_classがなかったもので。
+  //もしWordPressフックを使った方法や、もうちょっと綺麗なjQueryの書き方があればフォーラムで教えていただければ幸いです。
+  //https://wp-cocoon.com/community/cocoon-theme/
   setInterval(function(){
     $('.mce-content-body').addClass('article');
   },1000);
