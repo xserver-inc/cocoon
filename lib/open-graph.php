97d3ee999d8181c4fffb9832fa10515f516400b4
yhira <info@nelog.jp>
2018-08-06 20:00:38 +0900

    Fatal error: Can't use function return value in write contex対応

--- lib/open-graph.php
+++ lib/open-graph.php
@@ -170,11 +170,13 @@ class OpenGraphGetter implements Iterator
 		if (empty($page->_values)) { return false; }
 
     //og:titleが空文字だったとき
-    if (empty(trim($page->_values['title']))) {
+    $page_title = trim($page->_values['title']);
+    if (empty($page_title)) {
       $page->_values['title'] = $title;
     }
     //og:descriptionが空文字だったとき
-    if (empty(trim($page->_values['description']))) {
+    $page_description = trim($page->_values['description']);
+    if (empty($page_description)) {
       $page->_values['description'] = $description;
     }
 
