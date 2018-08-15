7af23225bc3277cdd61853a31a324c320a14cc90
yhira <info@nelog.jp>
2018-08-15 19:12:52 +0900

    Gutenbergにスタイルを適用させるJavaScriptを変更

--- js/gutenberg.js
+++ js/gutenberg.js
@@ -4,13 +4,17 @@
  * @link: https://wp-cocoon.com/
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
-(function($){
-  //タイマーを使ったあまり美しくない方法
-  //tiny_mce_before_initフック引数配列のbody_classがなかったもので。
-  //もしWordPressフックを使った方法や、もうちょっと綺麗なjQueryの書き方があればフォーラムで教えていただければ幸いです。
-  //https://wp-cocoon.com/community/cocoon-theme/
-  setInterval(function(){
-    $('.mce-content-body').addClass('article');
-  },1000);
+window.addEventListener('DOMContentLoaded', function(){
+    [].forEach.call(document.querySelectorAll("div.edit-post-visual-editor"), e => e.classList.add('article'));
+});
 
-})(jQuery);
+// (function($){
+//   //タイマーを使ったあまり美しくない方法
+//   //tiny_mce_before_initフック引数配列のbody_classがなかったもので。
+//   //もしWordPressフックを使った方法や、もうちょっと綺麗なjQueryの書き方があればフォーラムで教えていただければ幸いです。
+//   //https://wp-cocoon.com/community/cocoon-theme/
+//   setInterval(function(){
+//     $('.mce-content-body').addClass('article');
+//   },1000);
+
+// })(jQuery);
