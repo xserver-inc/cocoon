f2427a4fbc136249b05acbb42424aba52e561391
yhira <info@nelog.jp>
2018-09-15 19:38:26 +0900

    商品リンクインプレッションタグ作成関数追加

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -290,6 +290,7 @@ function get_moshimo_yahoo_search_url($keyword, $moshimo_yahoo_id){
 }
 endif;
 
+//長い文字列だった場合MD5ハッシュにする
 if ( !function_exists( 'get_long_str_to_md5_hash' ) ):
 function get_long_str_to_md5_hash($str, $length = 50){
   if (strlen($str) > $length) {
@@ -423,6 +424,41 @@ function get_amazon_itemlookup_xml($asin){
     return $res;
   }
   return false;
+}
+endif;
+
+//Amazonインプレッションタブの取得
+if ( !function_exists( 'get_amazon_impression_tag' ) ):
+function get_amazon_impression_tag(){
+  $moshimo_amazon_id  = trim(get_moshimo_amazon_id());
+  return '<img src="https//i.moshimo.com/af/i/impression?a_id='.$moshimo_amazon_id.'&p_id=170&pc_id=185&pl_id=4062" width="1" height="1" style="border:none;">';
+}
+endif;
+
+//楽天インプレッションタブの取得
+if ( !function_exists( 'get_rakuten_impression_tag' ) ):
+function get_rakuten_impression_tag(){
+  $moshimo_rakuten_id  = trim(get_moshimo_rakuten_id());
+  return '<img src="https//i.moshimo.com/af/i/impression?a_id='.$moshimo_rakuten_id.'&p_id=54&pc_id=54&pl_id=616" width="1" height="1" style="border:none;">';
+}
+endif;
+
+//バリューコマースYahoo!インプレッションタブの取得
+if ( !function_exists( 'get_valucomace_yahoo_impression_tag' ) ):
+function get_valucomace_yahoo_impression_tag(){
+  //Yahoo!バリューコマースSID
+  $sid = trim(get_yahoo_valuecommerce_sid());
+  //Yahoo!バリューコマースPID
+  $pid = trim(get_yahoo_valuecommerce_pid());
+  return '<img src="//ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid='.$sid.'&pid='.$pid.'" width="1" height="1" border="0">';
+}
+endif;
+
+//Yahoo!インプレッションタブの取得
+if ( !function_exists( 'get_yahoo_impression_tag' ) ):
+function get_yahoo_impression_tag(){
+  $moshimo_yahoo_id  = trim(get_moshimo_yahoo_id());
+  return '<img src="https//i.moshimo.com/af/i/impression?a_id='.$moshimo_yahoo_id.'&p_id=1225&pc_id=1925&pl_id=18502" width="1" height="1" style="border:none;">';
 }
 endif;
 
