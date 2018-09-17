6b8204615fe24d546e9d65ae47e893191c62a641
yhira <info@nelog.jp>
2018-09-17 19:40:32 +0900

    カテゴリページにパンくずリストを表示させた際にページタイトルは表示されないように修正

--- tmp/breadcrumbs.php
+++ tmp/breadcrumbs.php
@@ -38,12 +38,12 @@ if($cat && !is_wp_error($cat)){
   ++$count;
   echo $echo.'<div class="breadcrumb-item" itemscope itemtype="https://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-folder fa-fw"></span><a href="'.get_category_link($cat[0]->term_id).'" itemprop="item"><span itemprop="name">'.$cat[0]->name.'</span></a><meta itemprop="position" content="'.$count.'" />';
   //ページタイトルを含める場合はセパレーターを表示
-  if (is_single_breadcrumbs_include_post()) {
+  if (is_single_breadcrumbs_include_post() && is_singular()) {
     echo '<span class="sp"><span class="fa fa-angle-right"></span></span>';
   }
   echo '</div>';
   //ページタイトルを含める場合
-  if (is_single_breadcrumbs_include_post()) {
+  if (is_single_breadcrumbs_include_post() && is_singular()) {
     // ++$count;
     // echo '<div class="breadcrumb-item" itemscope itemtype="https://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-file-o fa-fw"></span><span itemprop="item"><span itemprop="name">'.get_the_title().'</span></span><meta itemprop="position" content="'.$count.'" /></div>';
     echo '<div class="breadcrumb-item"><span class="fa fa-file-o fa-fw"></span><span>'.get_the_title().'</span></div>';
