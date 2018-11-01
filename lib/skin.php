0bb1e6de41fa4291bd0c08fb4ea577b3cef3614e
yhira <info@nelog.jp>
2018-11-01 18:40:16 +0900

    コメント削除

--- lib/skin.php
+++ lib/skin.php
@@ -6,15 +6,14 @@
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
 
-// _v('skin');
-// _v(get_skin_url());
+
+global $_THEME_OPTIONS;
+
 //スキン用のfunctions.phpがある場合
 $php_file_path = url_to_local(get_skin_php_url());
 if (file_exists($php_file_path)) {
   require_once $php_file_path;
 }
-
-global $_THEME_OPTIONS;
 
 //スキン用のoption.csvがある場合
 $csv_file_path = url_to_local(get_skin_csv_url());
@@ -29,7 +28,6 @@ if (file_exists($csv_file_path)) {
       $_THEME_OPTIONS[$name] = $value;
     }
   }
-  //_v($_THEME_OPTIONS);
 }
 
 //スキン用のoption.jsonがある場合
@@ -39,21 +37,7 @@ if (file_exists($json_file_path)) {
   if ($json) {
     $json_options = json_decode($json, true);
     $_THEME_OPTIONS = array_merge($_THEME_OPTIONS, $json_options);
-    //_v($_THEME_OPTIONS);
   }
 }
-
-// add_action('admin_menu', function (){
-//   _v('admin_menu9');
-// }, 9);
-// add_action( 'wp_loaded', function (){
-//   _v('wp_loaded');
-// } );
-
-// add_action( 'after_setup_theme', function (){  //スキンが有効な場合
-//   _v('after_setup_theme');
-//   _v(get_skin_url());
-//   _v($_THEME_OPTIONS);
-// } );
 
 
