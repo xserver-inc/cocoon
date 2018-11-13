b810b7aa30bcade5c857a9f95ab98bc06944399b
yhira <info@nelog.jp>
2018-11-13 19:02:42 +0900

    COLORS（レッド）作成

---
+++ skins/skin-colors-red/functions.php
@@ -0,0 +1,22 @@
+<?php //スキンから親テーマの定義済み関数をオーバーライドして設定の書き換えが可能
+/*
+///////////////////////////////////////////
+// 設定操作サンプル
+// lib\page-settings\内のXXXXX-funcs.phpに書かれている
+// 定義済み関数をオーバーライドして設定を上書きできます。
+///////////////////////////////////////////
+//ヘッダーロゴを「トップメニューにするコードサンプル
+function get_header_layout_type(){
+  return 'top_menu';
+}
+
+//メインカラム幅を680pxにするサンプル
+function get_main_column_contents_width(){
+  return 680;
+}
+
+//エントリーカードの枠線を表示するサンプル
+function is_entry_card_border_visible(){
+  return true;
+}
+*/
