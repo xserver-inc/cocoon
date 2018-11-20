558c1a3ba52d8c20d2ef6566b8846e4d0042054e
yhira <info@nelog.jp>
2018-11-20 23:55:58 +0900

    User Timing API の重複防止

--- lib/page-speed-up/minify-js.php
+++ lib/page-speed-up/minify-js.php
@@ -97,12 +97,16 @@ function tag_code_to_minify_js($buffer) {
 
             //JS URLからJSコードの取得
             $js = js_url_to_js_minify_code( $url );
-            $start_name = $url.'-start';
-            $start = 'performance.mark("'.$start_name.'");';
-            $end_name = $url.'-end';
-            $end = 'performance.mark("'.$end_name.'");';
-            $measure = 'performance.measure("'.$url.'", "'.$start_name.'", "'.$end_name.'");';
-            $js = $start.$js.$end.$measure;
+            if ($js) {
+              $start_name_url = $url.'-start';
+              $start_url = 'performance.mark("'.$start_name_url.'");';
+              $end_name_url = $url.'-end';
+              $end_url = 'performance.mark("'.$end_name_url.'");';
+              $measure_url = 'performance.measure("'.$url.'", "'.$start_name_url.'", "'.$end_name_url.'");';
+              $js = $start_url.$js.$end_url.$measure_url;
+              //_v($js);
+            }
+
 
             //縮小化可能なJSな時
             if ($js !== false) {
@@ -122,13 +126,16 @@ function tag_code_to_minify_js($buffer) {
         //インラインタイプのJavaScriptコードだった場合
         if ($js_code) {
           $js = minify_js($js_code);
-          $start_name = 'inline-js-'.$i.'-start';
-          $start = 'performance.mark("'.$start_name.'");';
-          $end_name = 'inline-js-'.$i.'-end';
-          $end = 'performance.mark("'.$end_name.'");';
-          $measure = 'performance.measure("inline-js-'.$i.'", "'.$start_name.'", "'.$end_name.'");';
-          $js = $start.$js.$end.$measure;
-          //_v($js);
+          if ($js) {
+            $start_name_in = 'inline-js-'.$i.'-start';
+            $start_in = 'performance.mark("'.$start_name_in.'");';
+            $end_name_in = 'inline-js-'.$i.'-end';
+            $end_in = 'performance.mark("'.$end_name_in.'");';
+            $measure_in = 'performance.measure("inline-js-'.$i.'", "'.$start_name_in.'", "'.$end_name_in.'");';
+            $js = $start_in.$js.$end_in.$measure_in;
+            //_v($js);
+          }
+
           //インラインタイプのscriptタグを縮小化して置換する
           $buffer = str_replace($script_tag, '<script'.$attr_code.'>'.$js.'</script>', $buffer);
         }
