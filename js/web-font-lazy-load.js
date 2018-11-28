0cb8568ef193e31e211da6f82ccc8bf982768a53
yhira <info@nelog.jp>
2018-11-28 20:12:41 +0900

    JavaScriptファイルのクレジット表記

--- js/web-font-lazy-load.js
+++ js/web-font-lazy-load.js
@@ -1,3 +1,10 @@
+/**
+ * Cocoon WordPress Theme
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+
 /////////////////////////////////
 // WEBフォントの非同期読み込み
 /////////////////////////////////
