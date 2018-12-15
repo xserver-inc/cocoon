3b3adced994fa28d86454e39354445c30b22462b
yhira <info@nelog.jp>
2018-12-15 22:48:14 +0900

    AMP出力CSS関係のフック追加

--- lib/amp.php
+++ lib/amp.php
@@ -743,11 +743,11 @@ function generate_style_amp_custom_tag(){?>
 <style amp-custom><?php
 
   $css_all = '';
-  //AMPスタイルの取得
+  //AMPスタイルの取得（SCSSで出力したAMP用のCSS）
   $css_url = get_template_directory_uri().'/amp.css';
   $css = css_url_to_css_minify_code($css_url);
   if ($css !== false) {
-    $css_all .= $css;
+    $css_all .= apply_filters( 'amp_parent_css', $css );
   }
 
   ///////////////////////////////////////////
@@ -755,7 +755,7 @@ function generate_style_amp_custom_tag(){?>
   ///////////////////////////////////////////
   $css_icomoom = css_url_to_css_minify_code(get_template_directory_uri().'/webfonts/icomoon/style.css');
   if ($css_icomoom !== false) {
-    $css_all .= $css_icomoom;
+    $css_all .= apply_filters( 'amp_icomoon_css', $css_icomoom );
   }
 
   ///////////////////////////////////////////
@@ -765,14 +765,14 @@ function generate_style_amp_custom_tag(){?>
     //通常のスキンスタイル
     $skin_css = css_url_to_css_minify_code($skin_url);
     if ($skin_css !== false) {
-      $css_all .= $skin_css;
+      $css_all .= apply_filters( 'amp_skin_css', $skin_css );
     }
 
     //AMPのスキンスタイル
     $amp_css_url = str_replace('style.css', 'amp.css', $skin_url);
     $amp_css = css_url_to_css_minify_code($amp_css_url);
     if ($amp_css !== false) {
-      $css_all .= $amp_css;
+      $css_all .= apply_filters( 'amp_skin_amp_css', $amp_css );
     }
   }
 
@@ -808,7 +808,7 @@ function generate_style_amp_custom_tag(){?>
   ob_start();//バッファリング
   get_template_part('tmp/css-custom');//カスタムテンプレートの呼び出し
   $css_custom = ob_get_clean();
-  $css_all .= minify_css($css_custom);
+  $css_all .= apply_filters( 'amp_custum_css', minify_css($css_custom) );
 
   ///////////////////////////////////////////
   //子テーマのスタイル
@@ -818,22 +818,22 @@ function generate_style_amp_custom_tag(){?>
     $css_child_url = get_stylesheet_directory_uri().'/style.css';
     $child_css = css_url_to_css_minify_code($css_child_url);
     if ($child_css !== false) {
-      $css_all .= $child_css;
+      $css_all .= apply_filters( 'amp_child_css', $child_css );
     }
 
     //AMPのスキンスタイル
     $css_child_url = get_stylesheet_directory_uri().'/amp.css';
     $child_amp_css = css_url_to_css_minify_code($css_child_url);
     if ($child_amp_css !== false) {
-      $css_all .= $child_amp_css;
+      $css_all .= apply_filters( 'amp_child_amp_css', $child_amp_css );
     }
   }
 
   ///////////////////////////////////////////
-  //カスタムCSS
+  //投稿・固定ページに記入されているカスタムCSS
   ///////////////////////////////////////////
   if ($custom_css = get_custom_css_code()) {
-    $css_all .= $custom_css;
+    $css_all .= apply_filters( 'amp_singular_custom_css', $custom_css );
   }
 
   //!importantの除去
@@ -843,7 +843,7 @@ function generate_style_amp_custom_tag(){?>
   $css_all = minify_css($css_all);
 
   //全てのCSSの出力
-  echo $css_all;
+  echo apply_filters( 'amp_all_css', $css_all );
   //}?></style>
 <?php
 }
