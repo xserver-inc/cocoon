c05afee0b0475d2758308a335665afdd713b5712
yhira <info@nelog.jp>
2018-12-18 15:25:27 +0900

    ブロックエディターライブラリー用のファイル作成

---
+++ lib/block-editor.php
@@ -0,0 +1,7 @@
+<?php //ブロックエディター（Gutenberg）関連
+/**
+ * Cocoon WordPress Theme
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
