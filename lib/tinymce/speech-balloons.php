0c291f051e244679a35bd2eb1ba6b2c549781f01
yhira <info@nelog.jp>
2019-01-02 20:20:06 +0900

    関数名修正

--- lib/tinymce/speech-balloons.php
+++ lib/tinymce/speech-balloons.php
@@ -9,7 +9,7 @@
 //テーブル内にレコードが存在するとき
 if (!is_speech_balloons_record_empty()) {
   add_action('admin_init', 'add_speech_balloons_dropdown');
-  add_action('admin_head', 'generate_speech_balloons_is');
+  add_action('admin_head', 'generate_speech_balloons_js');
 }
 
 
@@ -41,8 +41,8 @@ function register_speech_balloons( $buttons ){
 endif;
 
 //吹き出しの値渡し用のJavaScriptを生成
-if ( !function_exists( 'generate_speech_balloons_is' ) ):
-function generate_speech_balloons_is($value){
+if ( !function_exists( 'generate_speech_balloons_js' ) ):
+function generate_speech_balloons_js($value){
   $records = get_speech_balloons(null, 'title');
 
   echo '<script type="text/javascript">
