ceafe27a98fd3855ad5748acc98970af8a1408bf
yhira <info@nelog.jp>
2019-01-26 17:34:40 +0900

    新着記事ウィジェットで「固定表示」を非表示にできる機能追加

--- lib/widgets/new-entries.php
+++ lib/widgets/new-entries.php
@@ -24,31 +24,15 @@ class NewEntryWidgetItem extends WP_Widget {
   function widget($args, $instance) {
     extract( $args );
     //ウィジェットモード（全ての新着記事を表示するか、カテゴリ別に表示するか）
-    $widget_mode = apply_filters( 'widget_mode', empty($instance['widget_mode']) ? WM_DEFAULT : $instance['widget_mode'] );
+    $widget_mode = apply_filters( 'cocoon_new_widget_mode', empty($instance['widget_mode']) ? WM_DEFAULT : $instance['widget_mode'] );
     //タイトル名を取得
-    $title_new = apply_filters( 'widget_title_new', empty($instance['title_new']) ? '' : $instance['title_new'] );
-    $title_new = apply_filters( 'widget_title', $title_new, $instance, $this->id_base );
+    $title_new = apply_filters( 'cocoon_new_widget_title_new', empty($instance['title_new']) ? '' : $instance['title_new'] );
+    $title_new = apply_filters( 'cocoon_new_widget_title', $title_new, $instance, $this->id_base );
     //表示数を取得
-    $entry_count = apply_filters( 'widget_entry_count', empty($instance['entry_count']) ? EC_DEFAULT : $instance['entry_count'] );
-    //$is_top_visible = apply_filters( 'widget_is_top_visible', empty($instance['is_top_visible']) ? true : $instance['is_top_visible'] );
-    $entry_type = apply_filters( 'widget_entry_type', empty($instance['entry_type']) ? ET_DEFAULT : $instance['entry_type'] );
-    // //表示数をグローバル変数に格納
-    // //ウィジェットモード
-    // global $g_widget_mode;
-    // //後で使用するテンプレートファイルへの受け渡し
-    // global $g_entry_count;
-    // //ウィジェットモードが設定されてない場合はall（全て表示）にする
-    // if ( !$widget_mode ) $widget_mode = WM_DEFAULT;
-    // $g_widget_mode = $widget_mode;
-    // //表示数が設定されていない時は5にする
-    // if ( !$entry_count ) $entry_count = EC_DEFAULT;
-    // $g_entry_count = $entry_count;
-
-    // //表示タイプをグローバル変数に格納
-    // global $_ENTRY_TYPE;
-    // //表示タイプのデフォルト設定
-    // if ( !$entry_type ) $entry_type = ET_DEFAULT;
-    // $_ENTRY_TYPE = $entry_type;
+    $entry_count = apply_filters( 'cocoon_new_widget_entry_count', empty($instance['entry_count']) ? EC_DEFAULT : $instance['entry_count'] );
+    //$is_top_visible = apply_filters( 'cocoon_new_widget_is_top_visible', empty($instance['is_top_visible']) ? true : $instance['is_top_visible'] );
+    $entry_type = apply_filters( 'cocoon_new_widget_entry_type', empty($instance['entry_type']) ? ET_DEFAULT : $instance['entry_type'] );
+    $is_sticky_visible = apply_filters( 'cocoon_new_widget_is_sticky_visible', empty($instance['is_sticky_visible']) ? 0 : 1 );
 
     //現在のカテゴリを取得
     $categories = array();
@@ -85,6 +69,7 @@ class NewEntryWidgetItem extends WP_Widget {
         'entry_count' => $entry_count,
         'cat_ids' => $categories,
         'entry_type' => $entry_type,
+        'sticky' => $is_sticky_visible,
       );
       //新着記事リストの作成
       generate_widget_entries_tag($atts);
@@ -100,6 +85,7 @@ class NewEntryWidgetItem extends WP_Widget {
     $instance['title_new'] = strip_tags($new_instance['title_new']);
     $instance['entry_count'] = strip_tags($new_instance['entry_count']);
     $instance['entry_type'] = strip_tags($new_instance['entry_type']);
+    $instance['is_sticky_visible'] = strip_tags($new_instance['is_sticky_visible']);
       return $instance;
   }
   function form($instance) {
@@ -109,12 +95,14 @@ class NewEntryWidgetItem extends WP_Widget {
         'title_new'   => '',
         'entry_count' => EC_DEFAULT,
         'entry_type'  => ET_DEFAULT,
+        'is_sticky_visible'  => 1,
       );
     }
     $widget_mode = WM_DEFAULT;
     $title_new   = '';
     $entry_count = EC_DEFAULT;
     $entry_type  = ET_DEFAULT;
+    $is_sticky_visible  = 1;
     if (isset($instance['widget_mode']))
       $widget_mode = esc_attr($instance['widget_mode']);
     if (isset($instance['title_new']))
@@ -123,6 +111,7 @@ class NewEntryWidgetItem extends WP_Widget {
       $entry_count = esc_attr($instance['entry_count']);
     if (isset($instance['entry_type']))
       $entry_type = esc_attr($instance['entry_type']);
+    $is_sticky_visible = empty($instance['is_sticky_visible']) ? 0 : 1;
     ?>
     <?php //ウィジェットモード（全てか、カテゴリ別か） ?>
     <p>
@@ -154,6 +143,14 @@ class NewEntryWidgetItem extends WP_Widget {
       <input class="widefat" id="<?php echo $this->get_field_id('entry_type'); ?>" name="<?php echo $this->get_field_name('entry_type'); ?>"  type="radio" value="<?php echo ET_DEFAULT; ?>" <?php echo ( ($entry_type == ET_DEFAULT || !$entry_type ) ? ' checked="checked"' : ""); ?> /><?php _e( 'デフォルト', THEME_NAME ) ?><br />
       <input class="widefat" id="<?php echo $this->get_field_id('entry_type'); ?>" name="<?php echo $this->get_field_name('entry_type'); ?>"  type="radio" value="<?php echo ET_LARGE_THUMB; ?>"<?php echo ($entry_type == ET_LARGE_THUMB ? ' checked="checked"' : ""); ?> /><?php _e( '大きなサムネイル', THEME_NAME ) ?><br />
       <input class="widefat" id="<?php echo $this->get_field_id('entry_type'); ?>" name="<?php echo $this->get_field_name('entry_type'); ?>"  type="radio" value="<?php echo ET_LARGE_THUMB_ON; ?>"<?php echo ($entry_type == ET_LARGE_THUMB_ON ? ' checked="checked"' : ""); ?> /><?php _e( 'タイトルを重ねた大きなサムネイル', THEME_NAME ) ?><br />
+    </p>
+    <?php //固定表示記事を表示する ?>
+    <p>
+      <?php
+        generate_label_tag($this->get_field_id('is_sticky_visible'), __('固定表示', THEME_NAME) );
+        echo '<br>';
+        generate_checkbox_tag($this->get_field_name('is_sticky_visible') , $is_sticky_visible, __( '「固定表示」記事を表示する', THEME_NAME ));
+      ?>
     </p>
     <?php
   }
