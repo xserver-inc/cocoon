f6b5d821915544bd5f6a70a15cf4d22a3c7c2ba9
yhira <info@nelog.jp>
2019-02-08 18:15:51 +0900

    PWA設定ファイル追加

---
+++ lib/page-settings/pwa-forms.php
@@ -0,0 +1,31 @@
+<?php /**
+ * Cocoon WordPress Theme
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+if ( !defined( 'ABSPATH' ) ) exit; ?>
+
+<div class="metabox-holder">
+
+<!-- PWA -->
+<div id="page-pwa" class="postbox">
+  <h2 class="hndle"><?php _e( 'PWA設定', THEME_NAME ) ?></h2>
+  <div class="inside">
+
+    <p><?php _e( 'PWA（Progressive Web Apps）とは、モバイル向けWebサイトをスマートフォン向けアプリのように使える仕組みです。', THEME_NAME ) ?></p>
+
+
+
+    <table class="form-table">
+      <tbody>
+
+
+
+      </tbody>
+    </table>
+
+  </div>
+</div>
+
+</div><!-- /.metabox-holder -->
---
+++ lib/page-settings/pwa-funcs.php
@@ -0,0 +1,9 @@
+<?php //PWA設定に必要な定数や関数
+/**
+ * Cocoon WordPress Theme
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+if ( !defined( 'ABSPATH' ) ) exit;
+
