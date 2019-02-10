bbf76940b5422169ef2d07b81dd8686490f563bb
yhira <info@nelog.jp>
2019-02-10 19:14:08 +0900

    PWA有効か設定キャプションの変更

--- lib/page-settings/pwa-forms.php
+++ lib/page-settings/pwa-forms.php
@@ -27,7 +27,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
           </th>
           <td>
             <?php
-            generate_checkbox_tag(OP_PWA_ENABLE, is_pwa_enable(), __("PWA機能を有効化する",THEME_NAME ));
+            generate_checkbox_tag(OP_PWA_ENABLE, is_pwa_enable(), __("PWAを有効化する",THEME_NAME ));
             generate_tips_tag(__( '有効化することで、PWA機能が有効化されスマートフォンからサイトがアプリのように利用できます。', THEME_NAME ));
             ?>
           </td>
