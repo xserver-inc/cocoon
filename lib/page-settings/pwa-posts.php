ab47efb74b568ca4323433ecdd9f8196bab41850
yhira <info@nelog.jp>
2019-02-20 17:57:54 +0900

    Service Workerバージョン変更

--- lib/page-settings/pwa-posts.php
+++ lib/page-settings/pwa-posts.php
@@ -89,7 +89,7 @@ if (is_pwa_enable()) {
   wp_filesystem_put_contents($manifest_file, $manifest, 0);
 
   //service-worker.js
-  $service_worker_ver = THEME_NAME.'_20190220'; //PWAに変更を加えたらバージョン変更
+  $service_worker_ver = THEME_NAME.'_20190220_1'; //PWAに変更を加えたらバージョン変更
   $site_logo = get_the_site_logo_url();
   $jquery_core_url = get_jquery_core_url(get_jquery_version());
   $jquery_migrate_url = get_jquery_migrate_url(get_jquery_migrate_version());
