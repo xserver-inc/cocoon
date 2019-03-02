c06b3cb087dc864faf5a963d8d643cb8c422741a
yhira <info@nelog.jp>
2019-03-02 19:45:34 +0900

    誤字修正

--- lib/page-settings/ads-forms.php
+++ lib/page-settings/ads-forms.php
@@ -294,7 +294,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
             <?php
             generate_hierarchical_category_check_list( 0, OP_AD_EXCLUDE_CATEGORY_IDS, get_ad_exclude_category_ids(), 300 );
             //generate_textbox_tag(OP_AD_EXCLUDE_CATEGORY_IDS, get_ad_exclude_category_ids(), __( '例：111,222,3333', THEME_NAME ));
-            generate_tips_tag(__( '広告を表示するカテゴリを選択してください。', THEME_NAME ));
+            generate_tips_tag(__( '広告を非表示にするカテゴリを選択してください。', THEME_NAME ));
             ?>
           </td>
         </tr>
