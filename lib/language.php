3ef67a74a334631b146ab317ad5c0d2799332814
yhira <info@nelog.jp>
2019-03-09 17:39:28 +0900

    翻訳ファイルを子テーマで編集できるように処理追加

--- lib/language.php
+++ lib/language.php
@@ -8,31 +8,30 @@
 if ( !defined( 'ABSPATH' ) ) exit;
 
 global $locale;
-//言語の最初の文字がenだったら全てen.moを呼び出す
-if (strpos($locale,'en') !== false) {
+//_v($locale);
+//言語の最初の文字がenだったら全てen.moを呼び出す（英語）
+if (preg_match('/^en_/', $locale)) {
   $locale = 'en';
+}
+//言語の最初の文字がkoだったら全てko.moを呼び出す（韓国語）
+if (preg_match('/^ko_/', $locale)) {
+  $locale = 'ko';
 }
 
 //親テーマの翻訳ディレクトリ
 $language_dir = get_template_directory() . '/languages';
-load_theme_textdomain( THEME_NAME, $language_dir );
+//load_theme_textdomain( THEME_NAME, $language_dir );
 
-//以降は正式版公開後に
-// //子テーマの翻訳ディレクトリ
-// $child_language_dir = get_stylesheet_directory() . '/languages';
-// //子テーマの翻訳ファイル
-// $child_language_file = $child_language_dir.'/en.mo';
-
-// //Simplicityの多言語化
-// if (is_child_theme()) {
-//   //子テーマでは子テーマ内の翻訳ファイルを使用
-//   if (file_exists($child_language_file)) {
-//     load_theme_textdomain( THEME_NAME, $child_language_dir );
-//   } else {
-//     load_theme_textdomain( THEME_NAME, $language_dir );
-//   }
-// } else {
-//   //親テーマの翻訳ファイル
-//   load_theme_textdomain( THEME_NAME, $language_dir );
-// }
-
+//子テーマの翻訳ディレクトリ
+$child_language_dir = get_stylesheet_directory() . '/languages';
+//子テーマの翻訳ファイル
+$child_language_file = $child_language_dir.'/'.$locale.'.mo';
+// _v(file_exists($child_language_dir));
+// _v(file_exists($child_language_file));
+// _v(($child_language_file));
+//子テーマを利用していて子テーマにファイルがある場合
+if (is_child_theme() && file_exists($child_language_file)) {
+  load_theme_textdomain( THEME_NAME, $child_language_dir );
+} else {
+  load_theme_textdomain( THEME_NAME, $language_dir );
+}
