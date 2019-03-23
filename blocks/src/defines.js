8b7a6fc749565e7cfbc10710df1dae43bcbcbd62
yhira <info@nelog.jp>
2019-03-24 01:16:01 +0900

    srcフォルダの除外

--- blocks/src/defines.js
+++
@@ -1,9 +0,0 @@
-/**
- * Cocoon Blocks
- * @author: yhira
- * @link: https://wp-cocoon.com/
- * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
- */
-
-export const THEME_NAME = 'cocoon';
-export const BLOCK_CLASS = ' block-box';
