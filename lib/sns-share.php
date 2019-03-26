670085a41d8d9d067d5f2d734c8eac8d21876af0
yhira <info@nelog.jp>
2019-03-26 18:55:15 +0900

    シェアURLの修正

--- lib/sns-share.php
+++ lib/sns-share.php
@@ -398,12 +398,12 @@ function is_scc_push7_exists(){
 //シェア対象ページのURLを取得する
 if ( !function_exists( 'get_share_page_url' ) ):
 function get_share_page_url(){
-  // if ( is_singular() ) {
-  //   $url = get_the_permalink();
-  // } else {
-  //   $url = home_url();
-  // }
-  $url = get_requested_url();
+  if ( is_singular() ) {
+    $url = get_the_permalink();
+  } else {
+    $url = get_requested_url();
+  }
+  //$url = get_requested_url();
   return $url;
 }
 endif;
