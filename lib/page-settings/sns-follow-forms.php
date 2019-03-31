0ebdbb7bb5b8ba5fc5076442c33133911915a5ff
yhira <info@nelog.jp>
2019-03-31 14:39:34 +0900

    Google+案内の非表示

--- lib/page-settings/sns-follow-forms.php
+++ lib/page-settings/sns-follow-forms.php
@@ -71,7 +71,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
               <li><?php _e( 'ウェブサイト', THEME_NAME ) ?></li>
               <li><?php _e( 'Twitter', THEME_NAME ) ?></li>
               <li><?php _e( 'Facebook', THEME_NAME ) ?></li>
-              <li><?php _e( 'Google+', THEME_NAME ) ?></li>
+              <!-- <li><?php _e( 'Google+', THEME_NAME ) ?></li> -->
               <li><?php _e( 'はてなブックマーク', THEME_NAME ) ?></li>
               <li><?php _e( 'Instagram', THEME_NAME ) ?></li>
               <li><?php _e( 'Pinterest', THEME_NAME ) ?></li>
