bcef868f54107a4d64bbec9ae0a9a53342a2c577
yhira <info@nelog.jp>
2019-04-02 19:33:44 +0900

    カテゴリーウィジェットの除外設定項目追加

--- lib/utils.php
+++ lib/utils.php
@@ -2497,3 +2497,16 @@ function xmlspecialchars($string){
   return preg_replace ('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', ' ', $string);
 }
 endif;
+
+//カンマテキストを配列にする
+if ( !function_exists( 'comma_text_to_array' ) ):
+function comma_text_to_array($comma_text){
+  $comma_text = str_replace(' ', '', $comma_text);
+  if (empty($comma_text)) {
+    $array = array();
+  } else {
+    $array = explode(',', $comma_text);
+  }
+  return $array;
+}
+endif;
