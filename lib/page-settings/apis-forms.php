d4f9e074be1cf77782549130056069e2b6c54283
yhira <info@nelog.jp>
2019-04-05 01:13:13 +0900

    Amazon商品リンクレビュー機能の解説リンク追加

--- lib/page-settings/apis-forms.php
+++ lib/page-settings/apis-forms.php
@@ -64,7 +64,7 @@ $help_text = __( '取得方法', THEME_NAME );
             generate_checkbox_tag(OP_AMAZON_ITEM_CUSTOMER_REVIEWS_VISIBLE , is_amazon_item_customer_reviews_visible(), __( 'レビューを表示する', THEME_NAME ));
             echo '<br>';
             generate_textbox_tag(OP_AMAZON_ITEM_CUSTOMER_REVIEWS_TEXT, get_amazon_item_customer_reviews_text(), __( 'レビューページへのリンクテキストを入力', THEME_NAME ));
-            generate_tips_tag(__( 'レビューページへのリンクを表示します。', THEME_NAME ).get_image_preview_tag('https://im-cocoon.net/wp-content/uploads/amazon-review.png'));
+            generate_tips_tag(__( 'レビューページへのリンクを表示します。', THEME_NAME ).get_help_page_tag('https://wp-cocoon.com/amazon-review-link/').get_image_preview_tag('https://im-cocoon.net/wp-content/uploads/amazon-review.png'));
 
             generate_checkbox_tag(OP_AMAZON_ITEM_LOGO_VISIBLE , is_amazon_item_logo_visible(), __( 'ロゴを表示する', THEME_NAME ));
             generate_tips_tag(__( 'Amazon商品リンクのロゴの表示切り替え。', THEME_NAME ).get_image_preview_tag('https://im-cocoon.net/wp-content/uploads/amazon-logo.png'));
