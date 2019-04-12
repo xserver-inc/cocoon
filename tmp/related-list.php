41bf80628424fcb0ee070d5cf75468b67737a036
yhira <info@nelog.jp>
2019-04-12 19:20:40 +0900

    フックによるrelated-list.phpテンプレートの表示制御

--- tmp/related-list.php
+++ tmp/related-list.php
@@ -7,17 +7,20 @@
  */
 if ( !defined( 'ABSPATH' ) ) exit;
 
-$args = get_related_wp_query_args();
-$query = new WP_Query( $args ); ?>
-  <?php if( $query -> have_posts() && !empty($args) ): //関連記事があるとき?>
-  <?php while ($query -> have_posts()) : $query -> the_post(); ?>
-    <?php //関連記事表示タイプ
-    get_template_part('tmp/related-entry-card'); ?>
-  <?php endwhile;?>
+//フックによるrelated-list.phpテンプレートの表示制御
+if (apply_filters('cocoon_template_part_related_list', true)) {
+  $args = get_related_wp_query_args();
+  $query = new WP_Query( $args ); ?>
+    <?php if( $query -> have_posts() && !empty($args) ): //関連記事があるとき?>
+    <?php while ($query -> have_posts()) : $query -> the_post(); ?>
+      <?php //関連記事表示タイプ
+      get_template_part('tmp/related-entry-card'); ?>
+    <?php endwhile;?>
 
-  <?php else:?>
-  <p><?php _e( '関連記事は見つかりませんでした。', THEME_NAME ) ?></p>
-  <?php
-endif;
-wp_reset_postdata();
+    <?php else:?>
+    <p><?php _e( '関連記事は見つかりませんでした。', THEME_NAME ) ?></p>
+    <?php
+  endif;
+  wp_reset_postdata();
+}
 ?>
