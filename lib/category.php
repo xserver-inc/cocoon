7f881cb4bb5d6cebfbd9a9045f278dea4ac6bf1f
yhira <info@nelog.jp>
2019-04-20 23:28:11 +0900

    コメント削除

--- lib/category.php
+++ lib/category.php
@@ -35,8 +35,6 @@ function get_category_meta($cat_id = null){
     } else {
       return array();
     }
-
-
   }
 }
 endif;
@@ -221,72 +219,14 @@ add_action ( 'edited_term', 'save_extra_category_fileds');
 if ( !function_exists( 'save_extra_category_fileds' ) ):
 function save_extra_category_fileds( $term_id ) {
   if ( isset( $_POST['cat_meta'] ) ) {
-    // //_v($_POST['cat_meta']);
     $cat_id = $term_id;
     $cat_meta = $_POST['cat_meta'];
-    // //_v($cat_id);
-    // $cat_meta = get_category_meta($cat_id);
-    // $cat_keys = array_keys($_POST['cat_meta']);
-    // //_v($cat_meta);
-    // foreach ($cat_keys as $key){
-    //   if (isset($_POST['cat_meta'][$key])){
-    //     $cat_meta[$key] = $_POST['cat_meta'][$key];
-    //   }
-    // }
+
     update_term_meta( $cat_id, get_category_meta_key($cat_id), $cat_meta );
   }
 }
 endif;
 
-
-//カテゴリ説明文をビジュアルエディターにするカスタマイズ
-/*
-// remove the html filtering
-remove_filter( 'pre_term_description', 'wp_filter_kses' );
-remove_filter( 'term_description', 'wp_kses_data' );
-
-add_filter('edit_category_form_fields', 'cat_description');
-if ( !function_exists( 'cat_description' ) ):
-function cat_description($tag){
-  ?>
-    <table class="form-table">
-      <tr class="form-field">
-        <th scope="row" valign="top"><label for="description"><?php _e('Description'); ?></label></th>
-        <td>
-        <?php
-          $settings = array('wpautop' => true, 'media_buttons' => true, 'quicktags' => true, 'textarea_rows' => '15', 'textarea_name' => 'description' );
-          wp_editor(wp_kses_post($tag->description , ENT_QUOTES, 'UTF-8'), 'cat_description', $settings);
-        ?>
-        <br />
-        <span class="description"><?php _e('The description is not prominent by default; however, some themes may show it.'); ?></span>
-        </td>
-      </tr>
-    </table>
-  <?php
-}
-endif;
-
-
-*/
-/*
-//デフォルトのカテゴリ説明文を削除
-//add_action('admin_head', 'remove_default_category_description');
-if ( !function_exists( 'remove_default_category_description' ) ):
-function remove_default_category_description(){
-  global $current_screen;
-  if ( $current_screen->id == 'edit-category' )
-  {
-  ?>
-    <script type="text/javascript">
-    jQuery(function($) {
-        $('textarea#description').closest('tr.form-field').remove();
-    });
-    </script>
-  <?php
-  }
-}
-endif;
-*/
 //デフォルトのカテゴリ説明文の位置の移動
 add_action('admin_head', 'move_default_category_description');
 if ( !function_exists( 'move_default_category_description' ) ):
