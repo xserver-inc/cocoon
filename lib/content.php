29f8e6daf121de95c797cee83d2f83556880e2e2
yhira <info@nelog.jp>
2019-04-26 19:25:07 +0900

    もしもアフィリエイトのコードはフッターで読み込まない

--- lib/content.php
+++ lib/content.php
@@ -35,7 +35,8 @@ function get_the_content_all_scripts($the_content) {
         includes_string($script, 'trends.google.co') ||
         //もしもモーションウィジェット
         includes_string($script, 'MafRakutenWidgetParam') ||
-        includes_string($script, '/rakuten/widget.js');
+        includes_string($script, '/rakuten/widget.js') ||
+        includes_string($script, 'MoshimoAffiliate');
         $is_exclude = apply_filters('cocoon_exclude_script_movement_from_content', $is_exclude, $script);
       //除外設定
       if ($is_exclude) {
