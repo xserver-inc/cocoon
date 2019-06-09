2b59f6d205281f2f30d6c1cad5d13de20a06aed7
yhira <info@nelog.jp>
2019-06-09 22:13:26 +0900

    カテゴリー・タグページのフォーマット統一

--- tmp/category-content.php
+++ tmp/category-content.php
@@ -11,16 +11,18 @@ if ( !defined( 'ABSPATH' ) ) exit;
 $cat_id = get_query_var('cat');
 if ($cat_id && get_category_meta($cat_id)): ?>
 <article class="category-content article">
-  <?php //カテゴリタイトル
-  get_template_part('tmp/list-title'); ?>
-  <?php if ($eye_catch = get_category_eye_catch($cat_id)): ?>
-    <header class="article-header category-header">
-      <figure class="eye-catch">
-        <img src="<?php echo esc_url($eye_catch); ?>" alt="<?php echo esc_attr(get_category_title($cat_id)); ?>">
-        <?php echo '<span class="cat-label cat-label-'.$cat_id.'">'.single_cat_title( '', false ).'</span>'; //カテゴリラベル ?>
-      </figure>
-    </header>
-  <?php endif ?>
+  <header class="article-header category-header">
+    <?php //カテゴリタイトル
+    get_template_part('tmp/list-title'); ?>
+    <?php if ($eye_catch = get_category_eye_catch($cat_id)): ?>
+      <div class="eye-catch-wrap">
+        <figure class="eye-catch">
+          <img src="<?php echo esc_url($eye_catch); ?>" alt="<?php echo esc_attr(get_category_title($cat_id)); ?>">
+          <?php echo '<span class="cat-label cat-label-'.$cat_id.'">'.single_cat_title( '', false ).'</span>'; //カテゴリラベル ?>
+        </figure>
+      </div>
+    <?php endif ?>
+  </header>
   <?php if ($content = get_category_content($cat_id)): ?>
     <div class="category-page-content entry-content">
       <?php echo $content; ?>
--- tmp/tag-content.php
+++ tmp/tag-content.php
@@ -11,15 +11,17 @@ if ( !defined( 'ABSPATH' ) ) exit;
 $tag_id = get_query_var('tag_id');
 if ($tag_id && get_tag_meta($tag_id)): ?>
 <article class="tag-content article">
-  <?php //タイトル
-  get_template_part('tmp/list-title'); ?>
-  <?php if ($eye_catch = get_tag_eye_catch($tag_id)): ?>
-    <header class="article-header tag-header">
-      <figure class="eye-catch">
-        <img src="<?php echo esc_url($eye_catch); ?>" alt="<?php echo esc_attr(get_tag_title($tag_id)); ?>">
-      </figure>
-    </header>
-  <?php endif ?>
+  <header class="article-header tag-header">
+    <?php //タイトル
+    get_template_part('tmp/list-title'); ?>
+    <?php if ($eye_catch = get_tag_eye_catch($tag_id)): ?>
+      <div class="eye-catch-wrap">
+        <figure class="eye-catch">
+          <img src="<?php echo esc_url($eye_catch); ?>" alt="<?php echo esc_attr(get_tag_title($tag_id)); ?>">
+        </figure>
+      </div>
+    <?php endif ?>
+  </header>
   <?php if ($content = get_tag_content($tag_id)): ?>
     <div class="tag-page-content entry-content">
       <?php echo $content; ?>
