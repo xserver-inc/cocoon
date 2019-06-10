f948004b1012e302c32d45c7389d69116a498d54
yhira <info@nelog.jp>
2019-06-10 20:34:53 +0900

    Seasonスキンのエントリーカード等のカテゴリーラベルを表示するように変更

--- skins/skin-season-summer/javascript.js
+++ skins/skin-season-summer/javascript.js
@@ -2,8 +2,8 @@
 $('#footer').before('<div class="page-top"><p><a href="#container"><i class="fa fa-chevron-circle-up" aria-hidden="true"></i> PAGE TOP</a></p></div>');
 $( 'select' ).wrap( '<div class="select-box"></div>' );
 $(function () {$('.date-tags').insertAfter('.entry-title');});
-$(function () {$('.blogcard-footer').remove();});
-$(function () {$('.entry-card .cat-label').remove();});
-$(function () {$('.related-entry-card .cat-label').remove();});
-$(function () {$('.carousel-entry-card .cat-label').remove();});
+// $(function () {$('.blogcard-footer').remove();});
+// $(function () {$('.entry-card .cat-label').remove();});
+// $(function () {$('.related-entry-card .cat-label').remove();});
+// $(function () {$('.carousel-entry-card .cat-label').remove();});
 
