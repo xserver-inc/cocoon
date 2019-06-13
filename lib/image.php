e03782b9cda2dfb090c24bf495643e31a82bfefd
yhira <info@nelog.jp>
2019-06-13 20:36:09 +0900

    Lightboxフックの追加

--- lib/image.php
+++ lib/image.php
@@ -35,15 +35,15 @@ function add_lightbox_property( $content ) {
     return $content;
 
   //既に適用させているところは処理しない
-  //if ( false !== strpos( $content, 'data-lightbox="image-set"' ) )
   if ( is_lightbox_plugin_exist($content) )
     return $content;
-
   //Aタグを正規表現で置換
   $content = preg_replace(
     '/<a([^>]+?(\.jpe?g|\.png|\.gif)[\'\"][^>]*?)>([\s\w\W\d]+?)<\/a>/i',//Aタグの正規表現
     '<a${1} data-lightbox="image-set">${3}</a>',//置換する
     $content );//投稿本文（置換する文章）
+
+  $content = apply_filters('add_lightbox_property', $content);
 
   return $content;
 }
