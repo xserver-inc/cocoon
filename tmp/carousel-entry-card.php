ad4216607d06f17905c441550b935c590d09b953
yhira <info@nelog.jp>
2019-06-17 19:40:21 +0900

    get_carousel_entry_card_thumbnail_sizeフィルターフック追加

--- tmp/carousel-entry-card.php
+++ tmp/carousel-entry-card.php
@@ -12,7 +12,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
 
   <figure class="carousel-entry-card-thumb card-thumb">
     <?php if ( has_post_thumbnail() ): // サムネイルを持っているとき ?>
-    <?php echo get_the_post_thumbnail($post->ID, THUMB320, array('class' => 'carousel-entry-card-thumb-image card-thumb-image', 'alt' => '') ); //サムネイルを呼び出す?>
+    <?php echo get_the_post_thumbnail($post->ID, apply_filters('get_carousel_entry_card_thumbnail_size', THUMB320), array('class' => 'carousel-entry-card-thumb-image card-thumb-image', 'alt' => '') ); //サムネイルを呼び出す?>
           <?php else: // サムネイルを持っていないとき ?>
     <img src="<?php echo get_no_image_320x180_url(); ?>" alt="" class="no-image carousel-entry-card-no-image" width="<?php echo THUMB320WIDTH; ?>" height="<?php echo THUMB320HEIGHT; ?>" />
     <?php endif; ?>
