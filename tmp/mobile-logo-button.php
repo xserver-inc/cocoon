74bf9278863458b45279f3ea35e81a87e3ac5d0d
yhira <info@nelog.jp>
2019-06-17 18:30:04 +0900

    モバイルロゴボタンで画像がない場合の対応

--- tmp/mobile-logo-button.php
+++ tmp/mobile-logo-button.php
@@ -13,6 +13,13 @@ $icon_class = $_MENU_ICON ? $_MENU_ICON : 'logo-menu-icon'; ?>
 <!-- ロゴボタン -->
 <li class="logo-menu-button menu-button">
   <a href="<?php echo esc_url(get_home_url()); ?>" class="menu-button-in">
-    <img class="site-logo-image" src="<?php echo esc_url($logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>">
+    <?php
+    $logo_url = get_the_site_logo_url();
+    //ロゴが存在する場合は画像
+    if ($logo_url): ?>
+      <img class="site-logo-image" src="<?php echo esc_url($logo_url); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>">
+    <?php elseif: ?>
+      <?php bloginfo('name'); ?>
+    <?php endif; ?>
   </a>
 </li>
