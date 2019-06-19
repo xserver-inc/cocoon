0d4d7971f3f573d7c20800da6c4a66ebc2bef5fd
yhira <info@nelog.jp>
2019-06-19 19:41:22 +0900

    ヘッダー・フッターモバイルボタンを表示している際のフッターのデフォルトボタン設定

--- tmp/mobile-footer-default-buttons.php
+++ tmp/mobile-footer-default-buttons.php
@@ -9,19 +9,37 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
 
 <ul class="mobile-footer-menu-buttons mobile-menu-buttons">
 
-  <?php //ナビメニュー
-  get_template_part( 'tmp/mobile-navi-button' ); ?>
+  <?php //ヘッダー・フッターモバイルボタン双方を表示している場合
+  if (is_mobile_button_layout_type_header_and_footer_mobile_buttons()): ?>
+    <?php //ホームメニュー
+    get_template_part( 'tmp/mobile-home-button' ); ?>
 
-  <?php //ホームメニュー
-  get_template_part( 'tmp/mobile-home-button' ); ?>
+    <?php //シェアメニュー
+    get_template_part( 'tmp/mobile-share-button' ); ?>
 
-  <?php //検索メニュー
-  get_template_part( 'tmp/mobile-search-button' ); ?>
+    <?php //目次メニュー
+    get_template_part( 'tmp/mobile-toc-button' ); ?>
 
-  <?php //トップメニュー
-  get_template_part( 'tmp/mobile-top-button' ); ?>
+    <?php //トップメニュー
+    get_template_part( 'tmp/mobile-top-button' ); ?>
 
-  <?php //サイドバーメニュー
-  get_template_part( 'tmp/mobile-sidebar-button' ); ?>
+    <?php //サイドバーメニュー
+    get_template_part( 'tmp/mobile-sidebar-button' ); ?>
+  <?php else: ?>
+    <?php //ナビメニュー
+    get_template_part( 'tmp/mobile-navi-button' ); ?>
+
+    <?php //ホームメニュー
+    get_template_part( 'tmp/mobile-home-button' ); ?>
+
+    <?php //検索メニュー
+    get_template_part( 'tmp/mobile-search-button' ); ?>
+
+    <?php //トップメニュー
+    get_template_part( 'tmp/mobile-top-button' ); ?>
+
+    <?php //サイドバーメニュー
+    get_template_part( 'tmp/mobile-sidebar-button' ); ?>
+  <?php endif; ?>
 
 </ul>
