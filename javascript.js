8985fb601178b020bb3e72eda1397d74c0dc6717
yhira <info@nelog.jp>
2019-07-08 20:32:19 +0900

    アンカーリンクをクリックしたときにスムーズスクロール

--- javascript.js
+++ javascript.js
@@ -156,6 +156,23 @@
   }
   */
 
+  $(function(){
+    // #で始まるアンカーをクリックした場合に処理
+    $('a[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 800; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+    });
+  });
+
 })(jQuery);
 
 /*
