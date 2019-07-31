47bfb005932f1c891ff62de206914ad9f1a5b7e1
yhira <info@nelog.jp>
2019-07-31 11:06:08 +0900

    お勧め記事の準備

--- lib/_defins.php
+++ lib/_defins.php
@@ -106,9 +106,10 @@ define('DATA_AD_FORMAT_LINK', 'link');
 //ナビゲーションメニュー
 define('NAV_MENU_HEADER', 'navi-header');
 define('NAV_MENU_HEADER_MOBILE', 'navi-mobile');
+define('NAV_MENU_HEADER_MOBILE_BUTTONS', 'navi-header-mobile');
+define('NAV_MENU_RECOMMENDED', 'navi-recommended');
 define('NAV_MENU_FOOTER', 'navi-footer');
 define('NAV_MENU_FOOTER_MOBILE_BUTTONS', 'navi-footer-mobile');
-define('NAV_MENU_HEADER_MOBILE_BUTTONS', 'navi-header-mobile');
 define('NAV_MENU_MOBILE_SLIDE_IN', 'navi-mobile-slide-in');
 
 //親テーマのstyle.cssのURL
---
+++ tmp/recommended-cards.php
@@ -0,0 +1,15 @@
+<?php //グローバルナビ
+/**
+ * Cocoon WordPress Theme
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */ ?>
+
+<!-- Recommended -->
+<div id="recommended" class="recommended cf">
+  <div id="recommended-in" class="recommended-in wrap cf">
+
+  </div><!-- /#recommended-in -->
+</div><!-- /.recommended -->
+
