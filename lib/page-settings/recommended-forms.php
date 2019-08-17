2a0c8aa490a7a94873534bea2b8fa0529fedbe86
yhira <info@nelog.jp>
2019-08-17 19:22:44 +0900

    解説プレビュー画像追加

--- lib/page-settings/recommended-forms.php
+++ lib/page-settings/recommended-forms.php
@@ -96,7 +96,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
           </th>
           <td>
             <?php
-            generate_checkbox_tag(OP_RECOMMENDED_CARDS_MARGIN_ENABLE, is_recommended_cards_margin_enable(), __( 'おすすめカード毎に余白を設ける', THEME_NAME ));
+            generate_checkbox_tag(OP_RECOMMENDED_CARDS_MARGIN_ENABLE, is_recommended_cards_margin_enable(), get_image_preview_tag('https://im-cocoon.net/wp-content/uploads/recommended_cards_margin_enable.png').__( 'おすすめカード毎に余白を設ける', THEME_NAME ));
             generate_tips_tag(__( 'デフォルトだとカードはすべてくっついています。この機能を有効にすることにより、カードごとに余白を設けます。', THEME_NAME ));
             ?>
           </td>
@@ -109,7 +109,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
           </th>
           <td>
             <?php
-            generate_checkbox_tag(OP_RECOMMENDED_CARDS_AREA_BOTH_SIDES_MARGIN_ENABLE, is_recommended_cards_area_both_sides_margin_enable(), __( 'おすすめカードエリアの左右に余白を設ける', THEME_NAME ));
+            generate_checkbox_tag(OP_RECOMMENDED_CARDS_AREA_BOTH_SIDES_MARGIN_ENABLE, is_recommended_cards_area_both_sides_margin_enable(), get_image_preview_tag('https://im-cocoon.net/wp-content/uploads/recommended_cards_area_both_sides_margin_enable.png').__( 'おすすめカードエリアの左右に余白を設ける', THEME_NAME ));
             generate_tips_tag(__( 'デフォルトだと、カードエリアは画面幅いっぱいになっています。この設定を有効にすることにより、コンテンツ部分と同等に左右余白を設けます。※PC表示のみ。', THEME_NAME ));
             ?>
           </td>
