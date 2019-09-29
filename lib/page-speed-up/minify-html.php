4673f2866dffde73632b679f4542c7ba955f08eb
yhira <info@nelog.jp>
2019-09-29 19:50:54 +0900

    iframeのLazy Loadも有効に

--- lib/page-speed-up/minify-html.php
+++ lib/page-speed-up/minify-html.php
@@ -37,7 +37,7 @@ function code_minify_call_back($buffer) {
     //画像の変換
     $buffer = convert_lazy_load_tag($buffer, 'img');
     //iframeの変換
-    //$buffer = convert_lazy_load_tag($buffer, 'iframe');
+    $buffer = convert_lazy_load_tag($buffer, 'iframe');
   }
 
   //「Warning: Attribute aria-required is unnecessary for elements that have attribute required.」対策
@@ -245,7 +245,7 @@ function convert_lazy_load_tag($the_content, $media){
   $pattern = '{<'.$media.'.+?>}is';
 
   if (!$is_img) {
-    $pattern = '{<iframe.+?</iframe>}is';
+    $pattern = '{<iframe.+?>}is';
   }
 
   //imgタグをamp-imgタグに変更する
@@ -322,43 +322,16 @@ function convert_lazy_load_tag($the_content, $media){
         $replace = '<'.$media.' class="'.$classes.'" '.$chrome_lazy;
         $tag = str_replace($search, $replace, $tag);
       }
-      //$tag = convert_lazy_load_class($tag);
-
-      // //srcの削除
-      // $search = '{ src=["\'].+?["\']}i';
-      // $replace = '';
-      // $tag = preg_replace($search, $replace, $tag);
 
       //srcsetの変換宇
       $tag = str_replace(' srcset=', ' data-srcset=', $tag);
 
-      // //data-loadedの削除
-      // $tag = str_replace(' data-loaded="true"', '', $tag);
-      // $tag = str_replace(" data-loaded='true'", '', $tag);
-
-      // //srcsetの削除
-      // $search = '{ ?+srcset=["\'].+?["\']}i';
-      // $replace = '';
-      // $tag = preg_replace($search, $replace, $tag);
-
-      // //sizesの削除
-      // $search = '{ ?+sizes=["\'].+?["\']}i';
-      // $replace = '';
-      // $tag = preg_replace($search, $replace, $tag);
-
       //noscriptタグの追加
-      // if ($is_img) {
-        $tag = $tag.'<noscript>'.$match.'</noscript>';
-      // } else {
-      //   # code...
-      // }
-      //_v($tag);
+      $tag = $tag.'<noscript>'.$match.'</noscript>';
 
       //imgタグをLazy Load対応に置換
       $the_content = preg_replace('{'.preg_quote($match).'(?!<noscript>)}', $tag , $the_content);
-      //$the_content = str_replace($match, $tag , $the_content);
     }
-    //_v($img_tags);
   }
   return apply_filters('convert_lazy_load_tag', $the_content, $media);
 }
