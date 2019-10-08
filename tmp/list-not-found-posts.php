8996bfd9b13305ea92cebeecc776012b1ad7c9ef
yhira <info@nelog.jp>
2019-10-08 19:43:59 +0900

    posts_not_found_messageフィルターフックの追加

--- tmp/list-not-found-posts.php
+++ tmp/list-not-found-posts.php
@@ -5,9 +5,12 @@
  * @link: https://wp-cocoon.com/
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
+//見つからない時のメッセージ
+$message =  __( '投稿が見つかりませんでした。', THEME_NAME );
+$message = apply_filters('posts_not_found_message', $message);
 if ( !defined( 'ABSPATH' ) ) exit; ?>
 
 <div class="posts-not-found">
   <h2>NOT FOUND</h2>
-  <p><?php echo __( '投稿が見つかりませんでした。', THEME_NAME );//見つからない時のメッセージ ?></p>
+  <p><?php echo $message; ?></p>
 </div>
