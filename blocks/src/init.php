01211d4d00fea0ca37cb2e145df3d2b394485613
yhira <info@nelog.jp>
2019-10-23 13:32:15 +0900

    カスタムフォントサイズを無効

--- blocks/src/init.php
+++ blocks/src/init.php
@@ -238,7 +238,7 @@ function cocoon_editor_color_palette_setup() {
     // カラーパレットの設定
     add_theme_support('editor-color-palette', $colors);
     // カスタム色を無効
-    add_theme_support('disable-custom-colors');
+    //add_theme_support('disable-custom-colors');
     // カスタムフォントサイズを無効
     // add_theme_support('disable-custom-font-sizes');
     return $colors;
