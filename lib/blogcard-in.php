652c1640882c929cb922aff6acec67416d558e6a
yhira <info@nelog.jp>
2019-10-24 17:46:52 +0900

    ブログカードラベルのFont Awesome5対応

--- lib/blogcard-in.php
+++ lib/blogcard-in.php
@@ -166,7 +166,7 @@ function url_to_internal_blogcard_tag($url){
   //_v($url);
   $tag =
   '<a href="'.$url.'" title="'.esc_attr($title).'" class="blogcard-wrap internal-blogcard-wrap a-wrap cf"'.$target.'>'.
-    '<div class="blogcard internal-blogcard'.$additional_class.' cf">'.
+    '<div class="blogcard internal-blogcard'.$additional_class.'">'.
       '<figure class="blogcard-thumbnail internal-blogcard-thumbnail">'.$thumbnail.'</figure>'.
       '<div class="blogcard-content internal-blogcard-content">'.
         '<div class="blogcard-title internal-blogcard-title">'.$title.'</div>'.
