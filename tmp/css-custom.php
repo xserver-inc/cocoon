699a962b416329636d22bf64934d4092d1361d33
yhira <info@nelog.jp>
2019-12-21 19:33:02 +0900

    サイドバーを左にした時に「カラム」の「カラム間の幅」が未設定だとときにメインとサイドバーカラムがくっつく不具合修正

--- tmp/css-custom.php
+++ tmp/css-custom.php
@@ -369,11 +369,16 @@ if (get_sidebar_border_color()): ?>
   border-color: <?php echo get_sidebar_border_color(); ?>;
 }
 <?php endif ?>
+<?php if (!is_sidebar_position_right() && !get_main_sidebar_margin()): ?>
+.main{
+  margin-right: 0;
+}
+<?php endif; ?>
 <?php //カラム間の幅
 $main_sidebar_margin = get_main_sidebar_margin();
 if (is_numeric($main_sidebar_margin)): ?>
 .main{
-  <?php if(is_sidebar_position_right()): ?>
+  <?php if (is_sidebar_position_right()): ?>
   margin-right: <?php echo $main_sidebar_margin; ?>px;
   margin-left: 0;
   <?php else: ?>
