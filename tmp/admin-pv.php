6615563f87294167cfd473496e213b99b2ef90ba
yhira <info@nelog.jp>
2019-12-24 23:24:09 +0900

    インデックスPV表示の余白調整

--- tmp/admin-pv.php
+++ tmp/admin-pv.php
@@ -17,23 +17,25 @@ if (is_admin_panel_pv_area_visible()): ?>
         by Jetpack
       <?php endif ?>
     </span>
+    <?php ob_start(); ?>
     <span class="fa fa-bar-chart fa-fw" aria-hidden="true"></span>
     <span class="today-pv">
-      <span class="today-pv-label"><?php _e( '本日', THEME_NAME ) ?></span>
+      <span class="today-pv-label"><?php _e( '本日:', THEME_NAME ) ?></span>
       <span class="today-pv-count"><?php echo get_todays_pv(); ?></span>
     </span>
     <span class="week-pv">
-      <span class="week-pv-label"><?php _e( '週', THEME_NAME ) ?></span>
+      <span class="week-pv-label"><?php _e( '週:', THEME_NAME ) ?></span>
       <span class="week-pv-count"><?php echo get_last_7days_pv(); ?></span>
     </span>
     <span class="month-pv">
-      <span class="month-pv-label"><?php _e( '月', THEME_NAME ) ?></span>
+      <span class="month-pv-label"><?php _e( '月:', THEME_NAME ) ?></span>
       <span class="month-pv-count"><?php echo get_last_30days_pv(); ?></span>
     </span>
     <span class="all-pv">
-      <span class="all-pv-label"><?php _e( '全体', THEME_NAME ) ?></span>
+      <span class="all-pv-label"><?php _e( '全体:', THEME_NAME ) ?></span>
       <span class="all-pv-count"><?php echo get_all_pv(); ?></span>
     </span>
+    <?php echo minify_html(ob_get_clean()); ?>
     <?php //Jetpackチャート表示
     if ((get_admin_panel_pv_type() == 'jetpack')
         //Jetpackが有効の場合
