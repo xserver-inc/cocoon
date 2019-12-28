5da5fc1cd75effdf0bedd851dec20fe84c418828
yhira <info@nelog.jp>
2019-12-28 20:30:34 +0900

    Veilnui Simplogスキンの追加

---
+++ skins/veilnui-simplog-purple/functions.php
@@ -0,0 +1,40 @@
+<?php
+/*!
+  Skin Name: Veilnui Simplog (purple)
+  Description: veilnuiが作成したシンプルなブログスキン（紫色）
+  Skin URI: https://simplog.veilnui.org
+  Author: veilnui
+  Author URI: https://veilnui.net/
+  Screenshot URI: images/image.png
+  Version: 1.1
+  License: http://www.gnu.org/licenses/gpl-2.0.html/ GPL v2 or later
+*/
+/**
+ * Skin control
+ * スキン制御
+ */
+global $_THEME_OPTIONS;
+$_THEME_OPTIONS = [
+  // all 全体
+  'site_key_color' => '',
+  'site_key_text_color' => '',
+  'site_text_color' => '',
+  'site_background_color' => '',
+  'sidebar_position' => 'sidebar_right',
+  // header ヘッダー
+  'header_container_background_color' => '',
+  'header_container_text_color' => '',
+  'header_background_color' => '',
+  'header_text_color' => '',
+  'global_navi_background_color' => '',
+  'global_navi_text_color' => '',
+  // column カラム
+  'main_column_border_color' => '',
+  'sidebar_border_color' => '',
+  // footer フッター
+  'footer_background_color' => '',
+  'footer_text_color' => '',
+  // go to top Go to topボタン
+  'go_to_top_background_color' => '',
+  'go_to_top_text_color' => '',
+];
