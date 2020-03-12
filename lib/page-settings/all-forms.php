5fd5c45de26c78ea73fe1c58c104849e09237409
yhira <info@nelog.jp>
2020-03-12 22:53:58 +0900

    recent_comments_widget_date_formatフックの追加

--- lib/page-settings/all-forms.php
+++ lib/page-settings/all-forms.php
@@ -321,7 +321,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
             generate_textbox_tag(OP_SITE_DATE_FORMAT, get_site_date_format(), '');
             generate_tips_tag(__( 'テーマ全体に使われている日付のフォーマット形式を入力してください（初期値：Y.m.d）。', THEME_NAME ));
             ?>
-            <p><?php _e( '<a href="https://wpdocs.osdn.jp/Formatting_Date_and_Time">日付と時刻の書式の解説</a>', THEME_NAME ) ?></p>
+            <p><?php _e( '<a href="https://ja.wordpress.org/support/article/formatting-date-and-time/">日付と時刻の書式の解説</a>', THEME_NAME ) ?></p>
           </td>
         </tr>
 
