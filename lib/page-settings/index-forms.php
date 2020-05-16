c6bb1d69ae8f2ee9a22ea36816275f1c3318355f
yhira <info@nelog.jp>
2020-05-16 21:00:20 +0900

    プレビューの非表示

--- lib/page-settings/index-forms.php
+++ lib/page-settings/index-forms.php
@@ -18,7 +18,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
     <table class="form-table">
       <tbody>
 
-        <?php if (DEBUG_ADMIN_DEMO_ENABLE && apply_filters('cocoon_setting_preview_index', true)): ?>
+        <?php if (DEBUG_ADMIN_DEMO_ENABLE && apply_filters('cocoon_setting_preview_index', false)): ?>
         <!-- プレビュー画面 -->
         <tr>
           <th scope="row">
