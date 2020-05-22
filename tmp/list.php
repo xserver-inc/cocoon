b2c5560c3286c426507bab6d83c5f2cf23ad8dc2
yhira <info@nelog.jp>
2020-05-22 20:48:27 +0900

    インデクスリスト表示でページネーションが表示されていなかった不具合修正

--- tmp/list.php
+++ tmp/list.php
@@ -115,7 +115,7 @@ if (is_sns_follow_buttons_visible() && !is_paged() &&
 ////////////////////////////
 //ページネーション
 ////////////////////////////
-if (!is_front_index_page()) {
+if (is_front_page_type_index()) {
   get_template_part('tmp/pagination');
 }
 
