8b2769543ab4940d88c28fd62436c985cf0fb575
yhira <info@nelog.jp>
2020-05-28 20:41:53 +0900

    タブ一覧カテゴリー一覧のHTML5エラー修正

--- tmp/entry-card.php
+++ tmp/entry-card.php
@@ -5,10 +5,15 @@
  * @link: https://wp-cocoon.com/
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
-if ( !defined( 'ABSPATH' ) ) exit; ?>
+if ( !defined( 'ABSPATH' ) ) exit;
+$article_id_attr = null;
+if (!is_front_page_type_index()) {
+  $article_id_attr = ' id="post-'.get_the_ID().'"';
+}
+?>
 
 <a href="<?php echo esc_url(get_the_permalink()); ?>" class="entry-card-wrap a-wrap border-element cf" title="<?php echo esc_attr(get_the_title()); ?>">
-  <article id="post-<?php the_ID(); ?>" <?php post_class( array('post-'.get_the_ID(), 'entry-card','e-card', 'cf') ); ?>>
+  <article<?php echo $article_id_attr; ?> <?php post_class( array('post-'.get_the_ID(), 'entry-card','e-card', 'cf') ); ?>>
     <figure class="entry-card-thumb card-thumb e-card-thumb">
       <?php
       //サムネイルタグを取得
