a90201c4c9cdc4adaa2e7919886837a8fa337a7c
yhira <info@nelog.jp>
2020-06-19 17:31:15 +0900

    パンくずリストのカテゴリ変更機能を実装

--- tmp/breadcrumbs.php
+++ tmp/breadcrumbs.php
@@ -9,14 +9,23 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 if (is_single_breadcrumbs_visible() && (is_single() || is_category())){
   $cats = get_the_category();
-  $cat = (is_single() && isset($cats[0])) ? $cats[0] : get_category(get_query_var("cat"));
+  //メインカテゴリが指定してある場合は該当カテゴリーを適用
+  $main_cat_id = get_the_page_main_category();
+  if ($main_cat_id) {
+    $cat = get_category($main_cat_id);
+  }
+  //メインカテゴリがない場合は先頭のカテゴリを適用
+  if (!$cat) {
+    $cat = $cats[0];
+  }
+  // $cat = (is_single() && isset($cats[0])) ? $cats[0] : get_category(get_query_var("cat"));
   if($cat && !is_wp_error($cat)){
     $echo = null;
     $root_text = __( 'ホーム', THEME_NAME );
     $root_text = apply_filters('breadcrumbs_single_root_text', $root_text);
     //var_dump($par);
     echo '<div id="breadcrumb" class="breadcrumb breadcrumb-category'.get_additional_single_breadcrumbs_classes().'" itemscope itemtype="https://schema.org/BreadcrumbList">';
-    echo '<div class="breadcrumb-home" itemscope itemtype="https://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-home fa-fw" aria-hidden="true"></span><a href="'.esc_url(get_home_url()).'" itemprop="item"><span itemprop="name">'.esc_html($root_text).'</span></a><meta itemprop="position" content="1" /><span class="sp"><span class="fa fa-angle-right" aria-hidden="true"></span></span></div>';
+    echo '<div class="breadcrumb-home" itemscope itemtype="https://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-home fa-fw" aria-hidden="true"></span><a href="'.esc_url(get_home_url()).'" itemprop="item"><span itemprop="name" class="breadcrumb-caption">'.esc_html($root_text).'</span></a><meta itemprop="position" content="1" /><span class="sp"><span class="fa fa-angle-right" aria-hidden="true"></span></span></div>';
     $count = 1;
     $par = get_category($cat->parent);
     //カテゴリ情報の取得
@@ -31,11 +40,11 @@ if (is_single_breadcrumbs_visible() && (is_single() || is_category())){
     foreach ($cats as $par) {
       ++$count;
       //var_dump($par->name);
-      $echo .= '<div class="breadcrumb-item" itemscope itemtype="https://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-folder fa-fw" aria-hidden="true"></span><a href="'.esc_url(get_category_link($par->term_id)).'" itemprop="item"><span itemprop="name">'.esc_html($par->name).'</span></a><meta itemprop="position" content="'.$count.'" /><span class="sp"><span class="fa fa-angle-right" aria-hidden="true"></span></span></div>';
+      $echo .= '<div class="breadcrumb-item" itemscope itemtype="https://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-folder fa-fw" aria-hidden="true"></span><a href="'.esc_url(get_category_link($par->term_id)).'" itemprop="item"><span itemprop="name" class="breadcrumb-caption">'.esc_html($par->name).'</span></a><meta itemprop="position" content="'.$count.'" /><span class="sp"><span class="fa fa-angle-right" aria-hidden="true"></span></span></div>';
     }
     // 現カテゴリの出力
     ++$count;
-    echo $echo.'<div class="breadcrumb-item" itemscope itemtype="https://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-folder fa-fw" aria-hidden="true"></span><a href="'.esc_url(get_category_link($cat->term_id)).'" itemprop="item"><span itemprop="name">'.esc_html($cat->name).'</span></a><meta itemprop="position" content="'.$count.'" />';
+    echo $echo.'<div class="breadcrumb-item" itemscope itemtype="https://schema.org/ListItem" itemprop="itemListElement"><span class="fa fa-folder fa-fw" aria-hidden="true"></span><a href="'.esc_url(get_category_link($cat->term_id)).'" itemprop="item"><span itemprop="name" class="breadcrumb-caption">'.esc_html($cat->name).'</span></a><meta itemprop="position" content="'.$count.'" />';
     //ページタイトルを含める場合はセパレーターを表示
     if (is_single_breadcrumbs_include_post() && is_singular()) {
       echo '<span class="sp"><span class="fa fa-angle-right" aria-hidden="true"></span></span>';
@@ -43,7 +52,7 @@ if (is_single_breadcrumbs_visible() && (is_single() || is_category())){
     echo '</div>';
     //ページタイトルを含める場合
     if (is_single_breadcrumbs_include_post() && is_singular()) {
-      echo '<div class="breadcrumb-item"><span class="fa fa-file-o fa-fw" aria-hidden="true"></span><span>'.esc_html(get_the_title()).'</span></div>';
+      echo '<div class="breadcrumb-item"><span class="fa fa-file-o fa-fw" aria-hidden="true"></span><span class="breadcrumb-caption">'.esc_html(get_the_title()).'</span></div>';
     }
 
     echo '</div><!-- /#breadcrumb -->';
