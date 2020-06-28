55eeee477500f8456d6da38619d7922cf4b6b1d7
yhira <info@nelog.jp>
2020-06-28 19:27:08 +0900

    IcoMoonのアップデート

--- webfonts/icomoon/liga.js
+++ webfonts/icomoon/liga.js
@@ -39,6 +39,8 @@
             'brand65': '&#xeaca;',
             'codepen': '&#xeae8;',
             'brand81': '&#xeae8;',
+            'bubble': '&#xe96b;',
+            'comment': '&#xe96b;',
             'soundcloud': '&#xeac3;',
             'brand58': '&#xeac3;',
             'linkedin2': '&#xeacb;',
