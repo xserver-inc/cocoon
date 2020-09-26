e973436038f5d90625e318fb36f487007f00d390
yhira <info@nelog.jp>
2020-09-26 14:11:13 +0900

    SmartNews出力からブログカードリンクを取り除く

--- tmp/smartnews.php
+++ tmp/smartnews.php
@@ -133,6 +133,9 @@ do_action( 'rss_tag_pre', 'rss2' );
 		//https://publishers.smartnews.com/ja/smartformat/specification_rss/
 		$content = preg_replace('{<a [^>]+?>}i', '', $content);
 		$content = str_replace('</a>', '', $content);
+		//ブログカードリンクは取り除く
+		//https://wp-cocoon.com/community/postid/24925/
+		$content = preg_replace('/^.*blogcard.*$(\r\n|\r|\n)/um', ' ', $content);
 		$content = apply_filters('get_the_smartnews_content', $content);
 		 ?>
 		<content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
