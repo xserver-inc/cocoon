5bdae1ebed14e7100247ccd87808e17d4cf2f32f
yhira <info@nelog.jp>
2020-10-10 19:31:27 +0900

    <meta name="referrer" content="no-referrer-when-downgrade"/>タグの追加

--- header.php
+++ header.php
@@ -13,6 +13,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
+<meta name="referrer" content="no-referrer-when-downgrade"/>
 
 <?php //ヘッドタグ内挿入用のアクセス解析用テンプレート
 get_template_part('tmp/head-analytics'); ?>
