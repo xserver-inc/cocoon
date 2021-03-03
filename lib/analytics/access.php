564ce23e35afe73fbcefca726bbc349419015823
yhira <info@nelog.jp>
2021-03-03 19:56:25 +0900

    不要なコメント削除

--- lib/analytics/access.php
+++ lib/analytics/access.php
@@ -7,7 +7,6 @@
  */
 
 require_once('../../../../../wp-load.php');
-//_v($_GET);
 $post_id = !empty($_GET['post_id']) ? $_GET['post_id'] : null;
 $post_type = !empty($_GET['post_type']) ? $_GET['post_type'] : null;
 if ($post_id && $post_type) {
