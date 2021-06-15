7927e1e0d6e2e0f3466a61ae8801f8e71727364c
yhira <info@nelog.jp>
2021-06-15 20:15:27 +0900

    Revert "クラス名修正：commets-list→comments-list"
    This reverts commit 064ed42524edb89d11cef664aca2cd33671a207b.

--- comments.php
+++ comments.php
@@ -21,7 +21,7 @@ if ( is_comment_allow() || have_comments() ): ?>
     <?php
     if(have_comments()): // コメントがあったら
     ?>
-        <ol class="comments-list">
+        <ol class="commets-list">
         <?php
         $args = get_wp_list_comments_args();
         wp_list_comments($args); //コメント一覧を表示 ?>
