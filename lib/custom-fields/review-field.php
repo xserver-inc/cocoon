ac7afae3d801e73635b8eb32eed298785140802c
yhira <info@nelog.jp>
2021-07-25 20:00:43 +0900

    レビュースニペットの仕様変更による不具合修正

--- lib/custom-fields/review-field.php
+++ lib/custom-fields/review-field.php
@@ -47,7 +47,7 @@ function review_custom_box_view(){
     'CreativeWorkSeries' => __( 'メディア出版物（書籍や定期刊行物、テレビ、ラジオ、ゲームなど）
     ', THEME_NAME ),
     'Episode' => __( 'シリーズもののエピソード（テレビ、ラジオ、ビデオゲームなど）', THEME_NAME ),
-    'MediaObjectWeb' => __( 'ダウンロードデータ（画像、ビデオ、オーディオなど）', THEME_NAME ),
+    'MediaObject' => __( 'ダウンロードデータ（画像、ビデオ、オーディオなど）', THEME_NAME ),
     'Organization' => __( '学校、NGO、企業、クラブなどの組織', THEME_NAME ),
   );
   generate_selectbox_tag('the_review_type', $options, $the_review_type, '');
