81cc88154d3b0ebc92792ddc1ff5aadd1e93fba9
taako-502 <i.am.armorknight@gmail.com>
2022-01-08 01:08:45 +0900

    シンプルダークモードのCSSおよびfunctions.phpとソースを共通化する

--- skins/simple-darkmode-always/functions.php
+++ skins/simple-darkmode-always/functions.php
@@ -1,10 +1,11 @@
 <?php
 // simple-darkmodeのfunctionsを参照する
-include_once get_template_directory() . '/skins/simple-darkmode/functions.php';
+include_once get_template_directory() . '/skins/simple-darkmode/inc/func-style.php';
 
-/**
- * エディタ側エンキュー
- */
-add_action( 'enqueue_block_editor_assets', function() {
-  wp_enqueue_style( 'simple-darkmode-always-editor', get_template_directory_uri().'/skins/simple-darkmode-always/css/editor.css' );
+add_action( 'wp_enqueue_scripts', function() {
+  // スタイルシート読み込み
+  wp_enqueue_style(
+    'cocoon-skins-sda',
+    esc_url(get_template_directory_uri() . '/skins/simple-darkmode-always/css/style.css'),
+  );
 });
