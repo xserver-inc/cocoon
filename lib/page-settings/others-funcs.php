dbd709ecba44ef18b3bcedf90956d8f344dc9d0c
yhira <info@nelog.jp>
2022-01-12 21:53:48 +0900

    jQuery CoreとjQuery Migrateのデフォルト値変更

--- lib/page-settings/others-funcs.php
+++ lib/page-settings/others-funcs.php
@@ -61,7 +61,7 @@ endif;
 define('OP_JQUERY_VERSION', 'jquery_version');
 if ( !function_exists( 'get_jquery_version' ) ):
 function get_jquery_version(){
-  return get_theme_option(OP_JQUERY_VERSION, '1');
+  return get_theme_option(OP_JQUERY_VERSION, '3');
 }
 endif;
 
@@ -69,6 +69,6 @@ endif;
 define('OP_JQUERY_MIGRATE_VERSION', 'jquery_migrate_version');
 if ( !function_exists( 'get_jquery_migrate_version' ) ):
 function get_jquery_migrate_version(){
-  return get_theme_option(OP_JQUERY_MIGRATE_VERSION, '1');
+  return get_theme_option(OP_JQUERY_MIGRATE_VERSION, '3');
 }
 endif;
