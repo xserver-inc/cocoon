be68805e8c8ec75a4d1f150f76ad37b381501102
yhira <yhira@ma.mrr.jp>
2022-06-12 21:50:02 +0900

    access.phpでwp-load.phpの存在確認

--- lib/analytics/access.php
+++ lib/analytics/access.php
@@ -6,9 +6,17 @@
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
 
-require_once('../../../../../wp-load.php');
-$post_id = !empty($_GET['post_id']) ? $_GET['post_id'] : null;
-$post_type = !empty($_GET['post_type']) ? $_GET['post_type'] : null;
-if ($post_id && $post_type) {
-  logging_page_access($post_id, $post_type);
+$wp_load = '../../../../../wp-load.php';
+if (!file_exists($wp_load)) {
+  //AWS Bitnamiパッケージのマルチサイト構成の場合
+  $wp_load = '/opt/bitnami/wordpress/wp-load.php';
+}
+//wp-load.phpが存在する時だけ実行
+if (file_exists($wp_load)) {
+  require_once($wp_load);
+  $post_id = !empty($_GET['post_id']) ? $_GET['post_id'] : null;
+  $post_type = !empty($_GET['post_type']) ? $_GET['post_type'] : null;
+  if ($post_id && $post_type) {
+    logging_page_access($post_id, $post_type);
+  }
 }
