c18989a47e957d921fdc6e73c9733401bc6575d7
yhira <yhira@ma.mrr.jp>
2022-09-10 23:29:09 +0900

    Cocoon 設定 > 高速化の「アイコンフォントの非同期読み込みを有効にする」を廃止

--- lib/page-speed-up/speed-up-forms.php
+++ lib/page-speed-up/speed-up-forms.php
@@ -190,22 +190,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
             ?>
           </td>
         </tr>
-
-
-        <!-- Lazy Load WEBフォント -->
-        <tr>
-          <th scope="row">
-            <?php generate_label_tag(OP_WEB_FONT_LAZY_LOAD_ENABLE, __('WEBフォント（β版）', THEME_NAME) ); ?>
-          </th>
-          <td>
-            <?php
-            generate_checkbox_tag(OP_WEB_FONT_LAZY_LOAD_ENABLE , is_web_font_lazy_load_enable(), __( 'アイコンフォントの非同期読み込みを有効にする', THEME_NAME ));
-            generate_tips_tag(__( 'サイズが大きくなりがちなアイコンフォントを非同期読み込みしてページ表示を高速化します。ただし、JavaScriptによりアイコンフォントが正常に読み込まれるまでは、多少タイムラグがあります。', THEME_NAME ));
-
-            ?>
-          </td>
-        </tr>
-
+        
 
       </tbody>
     </table>
