0dba2c641b2090e9dfc3a08f114e7e4edede5fe5
yhira <yhira@ma.mrr.jp>
2022-10-22 22:24:02 +0900

    モバイルのサイドバーIDが#slide-in-sidebarになるようにjQuery処理を変更 .slide-in-sidebarから#slide-in-sidebarにCSSを変更

--- javascript.js
+++ javascript.js
@@ -8,13 +8,13 @@
 // JavaScriptコード
 /////////////////////////////////
 
-(function($){
+(function ($) {
   /////////////////////////////////
   //TOPへ戻るボタン
   /////////////////////////////////
   var prevScrollTop = -1;
   var $window = $(window);
-  $window.scroll(function(){
+  $window.scroll(function () {
     //最上部から現在位置までの距離を取得して、変数[scrollTop]に格納
     var scrollTop = $window.scrollTop();
     var threashold = 600;
@@ -36,24 +36,24 @@
   });
 
   //ボタン(.go-to-top-common)のクリックイベント
-  $('.go-to-top-common').click(function(){
-  //ページトップへ移動する
+  $('.go-to-top-common').click(function () {
+    //ページトップへ移動する
     $('body,html').animate({
-            scrollTop: 1
-        }, 800);
+      scrollTop: 1
+    }, 800);
   });
   //ボタン(.go-to-toc-common)のクリックイベント
-  $('.go-to-toc-common').click(function(){
-  //目次へ移動する
+  $('.go-to-toc-common').click(function () {
+    //目次へ移動する
     $('body,html').animate({
-            scrollTop: $('.entry-content .toc').offset().top
-        }, 800);
+      scrollTop: $('.entry-content .toc').offset().top
+    }, 800);
   });
 
   //検索ボタンクリックでフォーカスを入力エリアに移す
-  $('#search-menu-input').change(function(e){
+  $('#search-menu-input').change(function (e) {
     var searchEdit = $('#search-menu-content .search-edit').first();
-    if( e.target.checked ) {
+    if (e.target.checked) {
       searchEdit.focus();
     } else {
       searchEdit.blur();
@@ -65,10 +65,10 @@
   var adminMenu = $("#admin-panel");
   var adminHeight = adminMenu.outerHeight();
   var adminStartPos = 0;
-  $(window).scroll(function(){
+  $(window).scroll(function () {
     var adminCurrentPos = $(this).scrollTop();
     if (adminCurrentPos > adminStartPos) {
-      if(adminCurrentPos >= 200) {
+      if (adminCurrentPos >= 200) {
         adminMenu.css("bottom", "-" + adminHeight + "px");
       }
     } else {
@@ -83,10 +83,10 @@
     var headerMenu = $('.mobile-header-menu-buttons');
     var headerHight = headerMenu.outerHeight();
     var headerStartPos = 0;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
       var headerCurrentPos = $(this).scrollTop();
-      if ( headerCurrentPos > headerStartPos ) {
-        if(headerCurrentPos >= 100) {
+      if (headerCurrentPos > headerStartPos) {
+        if (headerCurrentPos >= 100) {
           headerMenu.css('top', '-' + headerHight + 'px');
         }
       } else {
@@ -99,20 +99,12 @@
     var footerMenu = $(".mobile-footer-menu-buttons");
     var footerHeight = footerMenu.outerHeight();
     var footerStartPos = 0;
-    $(window).scroll(function(){
+    $(window).scroll(function () {
       var footerCurrentPos = $(this).scrollTop();
 
-      // if (footerCurrentPos - footerStartPos > 20) {
-      //   if(footerCurrentPos >= 100) {
-      //     footerMenu.css("bottom","calc( -1 * (env(safe-area-inset-bottom) + " + footerHeight + "px) )");
-      //   }
-      // } else if (footerCurrentPos - footerStartPos < -8) {
-      //   footerMenu.css("bottom", 0);
-      // }
-
       if (footerCurrentPos > footerStartPos) {
-        if(footerCurrentPos >= 100) {
-          footerMenu.css("bottom","calc( -1 * (env(safe-area-inset-bottom) + " + footerHeight + "px) )");
+        if (footerCurrentPos >= 100) {
+          footerMenu.css("bottom", "calc( -1 * (env(safe-area-inset-bottom) + " + footerHeight + "px) )");
         }
       } else if (footerCurrentPos - footerStartPos < -8) {
         footerMenu.css("bottom", 0);
@@ -123,43 +115,43 @@
 
     var headerButtons = $(".mobile-header-menu-buttons");
     var footerButtons = $(".mobile-footer-menu-buttons");
-    headerButtons.click(function() {
+    headerButtons.click(function () {
       headerButtons.css("z-index", "3");
       footerButtons.css("z-index", "2");
     });
 
-    footerButtons.click(function() {
+    footerButtons.click(function () {
       headerButtons.css("z-index", "2");
       footerButtons.css("z-index", "3");
     })
   }
 
   //コメントボタンがクリックされたとき
-  $('#comment-reply-btn, .comment-reply-link').click(function() {
+  $('#comment-reply-btn, .comment-reply-link').click(function () {
     $('#comment-reply-btn').slideUp();
     $('#respond').slideDown();
   });
 
   //Google検索ボタン
-  $('.sbtn').click(function(){
-		var w = $(this).prev('.sform').text();
-		if(w) window.open('https://www.google.co.jp/search?q='+encodeURIComponent(w),'_blank');
+  $('.sbtn').click(function () {
+    var w = $(this).prev('.sform').text();
+    if (w) window.open('https://www.google.co.jp/search?q=' + encodeURIComponent(w), '_blank');
   });
 
   //スライドインサイドバーのアーカイブセレクトボックス選択処理
-  $('.sidebar-menu-content .widget_archive select').change(function(){
-		document.location.href = this.options[this.selectedIndex].value;
+  $('.sidebar-menu-content .widget_archive select').change(function () {
+    document.location.href = this.options[this.selectedIndex].value;
   });
 
   //スライドインサイドバーのカテゴリーセレクトボックス選択処理
-  $('.sidebar-menu-content .widget_categories select').change(function(){
-    if ( this.options[ this.selectedIndex ].value > 0 ) {
+  $('.sidebar-menu-content .widget_categories select').change(function () {
+    if (this.options[this.selectedIndex].value > 0) {
       this.parentNode.submit();
     }
   });
 
-  function drawerCloser(selecter, checkbox){
-    $(selecter).click(function() {
+  function drawerCloser(selecter, checkbox) {
+    $(selecter).click(function () {
       href = $(this).attr('href');
       url = location.href;
       url = url.replace(/#.*$/, '');
@@ -173,56 +165,56 @@
   drawerCloser('.menu-drawer .menu-item a', '#navi-menu-input');
 
   //モバイルサイドバーをクリックしたら閉じる
-  drawerCloser('#sidebar-menu-content #sidebar a', '#sidebar-menu-input');
+  drawerCloser('#slide-in-sidebar a', '#sidebar-menu-input');
 
   //モバイルヘッダーメニューのロゴ処理
   //console.log($('.mobile-menu-buttons'));
-  $('.mobile-menu-buttons').each(function(){
+  $('.mobile-menu-buttons').each(function () {
     if ($(this).has('.logo-menu-button').length) {
       $(this).addClass('has-logo-button');
     }
   });
 
-  $(window).on('load', function(){
+  $(window).on('load', function () {
     $('#carousel').addClass('loaded');
   });
 
   //FAQボックスのアコーディオン化用
   $('.is-style-accordion .faq-answer').hide();
-  $('.is-style-accordion .faq-question').click(function(){
+  $('.is-style-accordion .faq-question').click(function () {
     $(this).next('.is-style-accordion .faq-answer').slideToggle();
     $(this).toggleClass('active');
   });
 
   //モバイルサイドバー表示時にサイドバーを移動する・モバイルサイドバーを閉じる時に元の位置に戻す
-  $('#sidebar-menu-input').change(function() {
-    if($(this).prop('checked')){
+  $('#sidebar-menu-input').change(function () {
+    if ($(this).prop('checked')) {
       $('#sidebar').appendTo('#sidebar-menu-content');
-      $('#sidebar').addClass('slide-in-sidebar');
-    }else{
-      $('#sidebar').removeClass('slide-in-sidebar');
+      $('#sidebar').attr('id', 'slide-in-sidebar');
+    } else {
+      $('#slide-in-sidebar').attr('id', 'sidebar');
       $('#sidebar').insertAfter('#main');
     }
   });
 
   //リサイズした時はサイドバーを元に戻す
-  $(window).resize(function(){
+  $(window).resize(function () {
     $('#sidebar-menu-input').prop('checked', false).change();
   });
-  
+
 })(jQuery);
 
 /*
 * Cocoon WordPress Theme incorporates code from "Youtube SpeedLoad" WordPress Plugin, Copyright 2017 Alexufo[http://habrahabr.ru/users/alexufo/]
 "Youtube SpeedLoad" WordPress Plugin is distributed under the terms of the GNU GPL v2
 */
-(function(){
-    var f = document.querySelectorAll(".video-click");
-    for (var i = 0; i < f.length; ++i) {
-      f[i].onclick = function () {
-        var iframe = this.getAttribute("data-iframe");
-        this.parentElement.innerHTML = '<div class="video">' + iframe + '</div>';
-      }
+(function () {
+  var f = document.querySelectorAll(".video-click");
+  for (var i = 0; i < f.length; ++i) {
+    f[i].onclick = function () {
+      var iframe = this.getAttribute("data-iframe");
+      this.parentElement.innerHTML = '<div class="video">' + iframe + '</div>';
     }
+  }
 })();
 
