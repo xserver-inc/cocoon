5d2f012957a0c9a0b3a4c1a6d524f2f793383498
yhira <yhira@ma.mrr.jp>
2022-11-22 14:00:32 +0900

    Revert "Revert "box-menuフォルダの作成""
    This reverts commit 5f1f0753ac675c77692f74c41346da60c738211e.

---
+++ blocks/src/block/box-menu/index.php
@@ -0,0 +1,23 @@
+<?php
+
+function render_ranking_list($attributes, $content) {
+  $id = $attributes['id'];
+  $classes = $attributes['classNames'];
+  if ($id) {
+    //返り値がないechoとかのHTML出力結果を取得する
+    ob_start();
+    echo '<div class="'.$classes.'">';
+    generate_item_ranking_tag($id);
+    echo '</div>';
+    return ob_get_clean();
+  }
+}
+
+if( function_exists('register_block_type')) {
+  register_block_type_from_metadata(
+    __DIR__,
+     array(
+      'render_callback' => 'render_ranking_list',
+    )
+  );
+}
--- blocks/src/init.php
+++ blocks/src/init.php
@@ -172,7 +172,7 @@ function cocoon_blocks_cgb_editor_assets() { // phpcs:ignore
     $rankings //プロバティ
   );
 
-  //ナビカード情報渡す
+  //メニュー情報を渡す
   $menus = wp_get_nav_menus();
   wp_localize_script(
     'cocoon-blocks-js', //値を渡すjsファイルのハンドル名
