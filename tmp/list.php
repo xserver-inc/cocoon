5c59f541d277c483dd2838d337157b19a91939e9
yhira <yhira@ma.mrr.jp>
2022-11-23 19:23:58 +0900

    カテゴリページのコンテンツの不具合修正

--- tmp/list.php
+++ tmp/list.php
@@ -10,11 +10,13 @@ if ( !defined( 'ABSPATH' ) ) exit;
 ////////////////////////////
 //アーカイブのタイトル
 ////////////////////////////
-if ( is_category() && is_single_breadcrumbs_position_main_top() ){
+if ( is_category() ){
   ////////////////////////////
   //カテゴリページのパンくずリスト
   ////////////////////////////
-  get_template_part('tmp/breadcrumbs');
+  if (is_single_breadcrumbs_position_main_top()) {
+    get_template_part('tmp/breadcrumbs');
+  }
 
   ////////////////////////////
   //カテゴリページのコンテンツ
