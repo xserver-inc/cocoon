6c332b30f6ae880907e5f89c4ec9bf3e9dfb595f
yhira <yhira@ma.mrr.jp>
2023-01-13 20:05:35 +0900

    Revert "metadataのBlock Stylesを適用"
    This reverts commit 2299467518d3dfcb12acdd6f2e5048749b35d3fd.

--- blocks/src/block/icon-box/index.php
+++ blocks/src/block/icon-box/index.php
@@ -1,32 +1,11 @@
 <?php
-/**
- * Server-side rendering of the `cocoon-blocks/icon-box` block.
- *
- * @package Cocoon
- * @author yhira
- * @link https://wp-cocoon.com/
- * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
- */
 
-/**
- * Renders the `cocoon-blocks/icon-box` block on the server.
- *
- * @param  array  $attributes The block attributes.
- * @param  string $content    The block content.
- * @return string Returns the block content.
- */
-// function render_block_cocoon_block_icon_box($attributes, $content)
-// {
-// 	return;
-// }
-
-/**
- * Registers the `cocoon-blocks/icon-box` block on server.
- */
-function register_block_cocoon_block_icon_box()
-{
-	register_block_type(__DIR__, [
-		//'render_callback' => 'render_block_cocoon_block_icon_box',
-	]);
+if( function_exists('register_block_type_from_metadata')) {
+  add_action( 'init', 'register_block_cocoon_icon_box', 99 );
+  function register_block_cocoon_icon_box() {
+    register_block_type_from_metadata(
+      __DIR__,
+      array()
+    );
+  }
 }
-add_action('init', 'register_block_cocoon_block_icon_box', 99);
