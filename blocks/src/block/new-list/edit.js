0505945e8a4afe95a089785ad3df43b77f1153ec
vic322 <vic322.tech@gmail.com>
2023-01-22 22:48:49 +0900

    new-list: Add new-list block

--- blocks/src/block/new-list/edit.js
+++ blocks/src/block/new-list/edit.js
@@ -1,4 +1,18 @@
-import { THEME_NAME } from '../../helpers';
+/**
+ * Cocoon Blocks
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import {
@@ -6,118 +20,264 @@ import {
 	PanelBody,
 	TextControl,
 	ToggleControl,
+	__experimentalNumberControl as NumberControl,
+	__experimentalDivider as Divider,
+	Disabled,
 } from '@wordpress/components';
 import { Fragment } from '@wordpress/element';
 import { ServerSideRender } from '@wordpress/editor';
-import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import { THEME_NAME } from '../../helpers';
 
 export default function edit( props ) {
 	const { attributes, setAttributes, className } = props;
-	const { id, label, isImageCircle } = attributes;
-	const classes = classnames( 'profile-block-box', 'block-box', {
-		[ 'profile-block-box-' + id ]: !! ( id !== '-1' ),
+	const {
+		count,
+		type,
+		bold,
+		arrow,
+		showAllCats,
+		cats,
+		children,
+		horizontal,
+		showAllTags,
+		tags,
+		modified,
+		order,
+		offset,
+		sticky,
+		snippet,
+		post_type,
+		taxonomy,
+		author,
+	} = attributes;
+
+	const classes = classnames( 'new-list-box', 'block-box', {
 		[ className ]: !! className,
 		[ attributes.className ]: !! attributes.className,
 	} );
 	setAttributes( { classNames: classes } );
 
-	// attributesのidが存在するかしないかを判断するフラグ
-	let isProfileIdExist = false;
-
-	// ドロップダウンリストに表示される有効なランキングアイテムの数
-	let abledDropdownListItemCount = 0;
-
-	function createOptions() {
-		var options = [];
-		options.push( { value: '-1', label: __( '未選択', THEME_NAME ) } );
-		if ( typeof gbUsers !== 'undefined' ) {
-			gbUsers.forEach( ( user ) => {
-				if ( isProfileIdExist === false && user.id == id ) {
-					isProfileIdExist = true;
-				}
-				options.push( {
-					value: user.id,
-					label: user.user_nicename,
-					disabled: false,
-				} );
-				abledDropdownListItemCount += 1;
-			} );
-		}
-
-		return options;
+	// 可変コントロールの定義
+	let catsTextControl = (
+		<TextControl
+			label={ __( '表示するカテゴリをカンマ区切りで指定', THEME_NAME ) }
+			value={ cats }
+			onChange={ ( value ) => setAttributes( { cats: value } ) }
+		/>
+	);
+	if ( showAllCats ) {
+		catsTextControl = <Disabled>{ catsTextControl }</Disabled>;
 	}
 
-	const getProfileMessage = () => {
-		let msg = '';
-		if (
-			typeof gbUsers !== 'undefined' &&
-			abledDropdownListItemCount === 0
-		) {
-			msg = __( '選択できるユーザーがありません。', THEME_NAME );
-		} else if ( typeof gbUsers !== 'undefined' ) {
-			msg = __( 'ユーザーを選択してください。', THEME_NAME );
-		} else {
-			return '';
-		}
+	let tagsTextControl = (
+		<TextControl
+			label={ __( 'タグ', THEME_NAME ) }
+			value={ tags }
+			onChange={ ( value ) => setAttributes( { tags: value } ) }
+		/>
+	);
+	if ( showAllTags ) {
+		tagsTextControl = <Disabled>{ tagsTextControl } </Disabled>;
+	}
+
+	console.log( attributes );
+
+	const getNewListContent = () => {
 		return (
-			<div class="cocoon-render-message editor-profile-block-box-message">
-				{ msg }
-			</div>
+			<ServerSideRender block={ props.name } attributes={ attributes } />
 		);
 	};
-
-	const getProfileContent = () => {
-		if ( id == '-1' ) {
-			return getProfileMessage();
-		} else {
-			return (
-				<ServerSideRender
-					block={ props.name }
-					attributes={ attributes }
-				/>
-			);
-		}
-	};
-
-	var options = createOptions();
-
-	if ( ! isProfileIdExist ) {
-		setAttributes( { id: '-1' } );
-	}
 
 	return (
 		<Fragment>
 			<InspectorControls>
-				<PanelBody>
-					<TextControl
-						label={ __( 'ラベル', THEME_NAME ) }
-						value={ label }
-						onChange={ ( value ) =>
-							setAttributes( { label: value } )
+				<PanelBody
+					title={ __( '基本設定', THEME_NAME ) }
+					initialOpen={ true }
+				>
+					<NumberControl
+						label={ __( '表示する記事の数', THEME_NAME ) }
+						isShiftStepEnabled={ false }
+						value={ count }
+						onChange={ ( newValue ) =>
+							setAttributes( { count: newValue } )
+						}
+						min={ 0 }
+					/>
+					<SelectControl
+						label={ __( '表示タイプ', THEME_NAME ) }
+						value={ type }
+						onChange={ ( newType ) =>
+							setAttributes( { type: newType } )
+						}
+						options={ [
+							{
+								label: __( '通常のリスト', THEME_NAME ),
+								value: 'default',
+							},
+							{
+								label: __(
+									'カードの上下に区切り線を入れる',
+									THEME_NAME
+								),
+								value: 'border_partition',
+							},
+							{
+								label: __(
+									'カードに枠線を表示する',
+									THEME_NAME
+								),
+								value: 'border_square',
+							},
+							{
+								label: __( '大きなサムネイル表示', THEME_NAME ),
+								value: 'large_thumb',
+							},
+							{
+								label: __(
+									'大きなサムネイルにタイトルを重ねる',
+									THEME_NAME
+								),
+								value: 'large_thumb_on',
+							},
+						] }
+					/>
+					<SelectControl
+						label={ __( '表示順', THEME_NAME ) }
+						value={ modified ? '1' : '0' }
+						options={ [
+							{
+								label: __( '更新日順', THEME_NAME ),
+								value: '1',
+							},
+							{
+								label: __( '投稿日順', THEME_NAME ),
+								value: '0',
+							},
+						] }
+						onChange={ ( value ) => {
+							if ( value === '0' ) {
+								setAttributes( { modified: false } );
+							} else {
+								setAttributes( { modified: true } );
+							}
+						} }
+					/>
+					<SelectControl
+						label={ __( '並び替え', THEME_NAME ) }
+						value={ order }
+						options={ [
+							{ label: __( '昇順', THEME_NAME ), value: 'asc' },
+							{ label: __( '降順', THEME_NAME ), value: 'desc' },
+						] }
+						onChange={ ( value ) => {
+							setAttributes( { order: value } );
+						} }
+					/>
+					<NumberControl
+						label={ __( '読み飛ばし', THEME_NAME ) }
+						isShiftStepEnabled={ false }
+						value={ offset }
+						onChange={ ( newValue ) =>
+							setAttributes( { offset: newValue } )
+						}
+						min={ 0 }
+					/>
+					<Divider />
+					<ToggleControl
+						label={ __( '記事タイトルを太字にする', THEME_NAME ) }
+						checked={ bold }
+						onChange={ ( isChecked ) =>
+							setAttributes( { bold: isChecked } )
 						}
 					/>
 					<ToggleControl
-						label={ __( '画像を円形にする', THEME_NAME ) }
-						checked={ isImageCircle }
+						label={ __( 'カードに矢印を表示する', THEME_NAME ) }
+						checked={ arrow }
 						onChange={ ( isChecked ) =>
-							setAttributes( { isImageCircle: isChecked } )
+							setAttributes( { arrow: isChecked } )
+						}
+					/>
+					<ToggleControl
+						label={ __( '横並び表示', THEME_NAME ) }
+						checked={ horizontal }
+						onChange={ ( isChecked ) =>
+							setAttributes( { horizontal: isChecked } )
+						}
+					/>
+					<ToggleControl
+						label={ __( '説明文の表示', THEME_NAME ) }
+						checked={ snippet }
+						onChange={ ( isChecked ) =>
+							setAttributes( { snippet: isChecked } )
+						}
+					/>
+				</PanelBody>
+				<PanelBody
+					title={ __( 'フィルタ', THEME_NAME ) }
+					initialOpen={ false }
+				>
+					<ToggleControl
+						label={ __( '全カテゴリ表示', THEME_NAME ) }
+						checked={ showAllCats }
+						onChange={ ( isChecked ) => {
+							setAttributes( { showAllCats: isChecked } );
+						} }
+					/>
+					{ catsTextControl }
+					<ToggleControl
+						label={ __(
+							'子カテゴリの内容を含めて表示',
+							THEME_NAME
+						) }
+						checked={ children }
+						onChange={ ( isChecked ) =>
+							setAttributes( { children: isChecked } )
+						}
+					/>
+					<ToggleControl
+						label={ __( '全タグ表示', THEME_NAME ) }
+						checked={ showAllTags }
+						onChange={ ( isChecked ) => {
+							setAttributes( { showAllTags: isChecked } );
+						} }
+					/>
+					{ tagsTextControl }
+					<ToggleControl
+						label={ __( '固定記事の表示', THEME_NAME ) }
+						checked={ sticky }
+						onChange={ ( isChecked ) =>
+							setAttributes( { sticky: isChecked } )
+						}
+					/>
+					<TextControl
+						label={ __( '投稿タイプ', THEME_NAME ) }
+						value={ post_type }
+						onChange={ ( newValue ) =>
+							setAttributes( { post_type: newValue } )
+						}
+					/>
+					<TextControl
+						label={ __( '検索グループ', THEME_NAME ) }
+						value={ taxonomy }
+						onChange={ ( newValue ) =>
+							setAttributes( { taxonomy: newValue } )
+						}
+					/>
+					<TextControl
+						label={ __( '投稿ユーザ', THEME_NAME ) }
+						value={ author }
+						onChange={ ( newValue ) =>
+							setAttributes( { author: newValue } )
 						}
 					/>
 				</PanelBody>
 			</InspectorControls>
-			<div { ...useBlockProps() }>
-				<SelectControl
-					label={ __( 'ユーザー', THEME_NAME ) }
-					labelPosition="side"
-					className="cocoon-render-dropdown editor-profile-block-box-dropdown"
-					value={ id }
-					onChange={ ( value ) =>
-						setAttributes( { id: value, classNames: classes } )
-					}
-					options={ options }
-				/>
-				{ getProfileContent() }
-			</div>
+			<div { ...useBlockProps() }>{ getNewListContent() }</div>
 		</Fragment>
 	);
 }
--- blocks/src/block/new-list/index.php
+++ blocks/src/block/new-list/index.php
@@ -1,25 +1,55 @@
 <?php
+/**
+ * Server-side rendering of the `cocoon-blocks/new-list` block.
+ *
+ * @package Cocoon
+ * @author yhira
+ * @link https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
 
-function render_profile($attributes, $content) {
-  $id = $attributes['id'];
-  $classes = $attributes['classNames'];
-  $label = $attributes['label'];
-  $isImageCircle = $attributes['isImageCircle'] ? 1 : 0;
+/**
+ * Renders the `cocoon-blocks/new-list` block on the server.
+ *
+ * @param  array  $attributes The block attributes.
+ * @param  string $content    The block content.
+ * @return string Returns the block content.
+ */
+function render_block_cocoon_block_new_list($attributes, $content)
+{
+	$classes = $attributes['classNames'];
+	$atts = [
+		'count' => $attributes['count'],
+		'cats' => $attributes['showAllCats'] ? 'all' : $attributes['cats'],
+		'tags' => $attributes['showAllTags'] ? 'all' : $attributes['tags'],
+		'type' => $attributes['type'],
+		'children' => $attributes['children'] ? 1 : 0,
+		'post_type' => $attributes['post_type'],
+		'taxonomy' => $attributes['taxonomy'],
+		'sticky' => $attributes['sticky'] ? 1 : 0,
+		'modified' => $attributes['modified'] ? 1 : 0,
+		'order' => $attributes['order'],
+		'bold' => $attributes['bold'] ? 1 : 0,
+		'arrow' => $attributes['arrow'] ? 1 : 0,
+		'snippet' => $attributes['snippet'] ? 1 : 0,
+		'author' => $attributes['author'],
+		'offset' => $attributes['offset'],
+		'horizontal' => $attributes['horizontal'] ? 1 : 0,
+	];
 
-  if ($id) {
-    ob_start();
-    echo '<div class="'.$classes.'">';
-    generate_author_box_tag($id, $label, $isImageCircle);
-    echo '</div>';
-    return ob_get_clean();
-  }
+	$new_list_content = new_entries_shortcode($atts);
+	ob_start();
+	echo '<div class="' . $classes . '">';
+	echo $new_list_content;
+	echo '</div>';
+	return ob_get_clean();
 }
 
-if( function_exists('register_block_type')) {
-  register_block_type_from_metadata(
-    __DIR__,
-     array(
-      'render_callback' => 'render_profile',
-    )
-  );
+/**
+ * Registers the `cocoon-blocks/new-list` block on server.
+ */
+if (function_exists('register_block_type')) {
+	register_block_type(__DIR__, [
+		'render_callback' => 'render_block_cocoon_block_new_list',
+	]);
 }
