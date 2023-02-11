ca1f53e73fca1b86a3ad8bbf4bdbcab2ac6b7a5f
yhira <yhira@ma.mrr.jp>
2023-02-11 17:00:25 +0900

    新着情報ブロックにアイコンフォント設定

--- blocks/src/block/info-list/index.js
+++ blocks/src/block/info-list/index.js
@@ -27,7 +27,7 @@ export { metadata, name };
 
 export const settings = {
 	title: __( '新着情報', THEME_NAME ),
-	//icon: <FontAwesomeIcon icon={['far', 'newspaper']} />,
+	icon: <FontAwesomeIcon icon={['far', 'file-alt']} />,
 	description: __( '新着情報の一覧を表示します。', THEME_NAME ),
 
 	edit,
