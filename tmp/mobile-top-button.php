4e637e03c18331d250b9f663acc3b20a8c5ce47e
yhira <yhira@ma.mrr.jp>
2023-03-09 20:49:23 +0900

    PageSpeed InsightsのSEO項目のリンククロール対策（SNSボタンやモバイルメニューボタン） ※問題が出る場合はリバートする

--- tmp/mobile-top-button.php
+++ tmp/mobile-top-button.php
@@ -19,10 +19,10 @@ if (is_amp()) {
   ?>
 <!-- トップボタン -->
 <li class="top-menu-button menu-button">
-  <a class="go-to-top-common top-menu-a menu-button-in"<?php echo $on; ?>>
+  <div class="go-to-top-common top-menu-a menu-button-in"<?php echo $on; ?>>
     <span class="top-menu-icon menu-icon">
       <span class="<?php echo esc_attr($icon_class); ?>" aria-hidden="true"></span>
     </span>
     <span class="top-menu-caption menu-caption"><?php echo $_MENU_CAPTION ? $_MENU_CAPTION : __( 'トップ', THEME_NAME ); ?></span>
-  </a>
+  </div>
 </li>
