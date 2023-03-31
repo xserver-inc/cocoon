f5f44aa325acb8922be4591046c6e01e0dffdd5b
yhira <yhira@ma.mrr.jp>
2023-03-31 19:33:14 +0900

    tmp/entry-card.phpの 'loading' => 'lazy',を削除

--- tmp/entry-card.php
+++ tmp/entry-card.php
@@ -24,7 +24,6 @@ if (is_front_page_type_index()) {
           array(
             'class' => 'entry-card-thumb-image card-thumb-image',
             'alt' => '',
-            'loading' => 'lazy',
             'decoding' => 'async',
           )
         );
