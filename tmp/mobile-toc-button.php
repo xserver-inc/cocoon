04cf5b739f16a6a7168291ac8bdc26cf9a5760ca
yhira <yhira@ma.mrr.jp>
2023-04-11 20:49:42 +0900

    PageSpeed InsightsのSEO項目のリンククロール対策

--- tmp/mobile-toc-button.php
+++ tmp/mobile-toc-button.php
@@ -21,7 +21,7 @@ if (is_amp()) {
   ?>
 <!-- 目次へボタン -->
 <li class="toc-menu-button menu-button">
-  <a href="#" class="go-to-toc-common toc-menu-a menu-button-in"<?php echo $on; ?>>
+  <a href="#toc" class="go-to-toc-common toc-menu-a menu-button-in"<?php echo $on; ?>>
     <span class="toc-menu-icon menu-icon">
       <span class="<?php echo esc_attr($icon_class); ?>" aria-hidden="true"></span>
     </span>
