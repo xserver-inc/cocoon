a710d86efd236f6f23cb6a434e58da3e7da7e653
yhira <yhira@ma.mrr.jp>
2023-06-07 18:35:23 +0900

    インデックスエントリーカード・関連記事エントリーカードの「更新日が存在しない場合は投稿日を表示」を削除

--- lib/page-settings/single-forms.php
+++ lib/page-settings/single-forms.php
@@ -251,12 +251,14 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
             echo '</div>';
 
             generate_checkbox_tag(OP_RELATED_ENTRY_CARD_POST_DATE_VISIBLE , is_related_entry_card_post_date_visible(), __( '投稿日の表示', THEME_NAME ));
-            //表示しないとき
-            $is_not_allowed = is_related_entry_card_post_date_visible();
-            $is_not_allowed = $is_not_allowed || !is_related_entry_card_post_update_visible();
-            echo '<div class="indent'.get_not_allowed_form_class(!$is_not_allowed, true).'">';
-              generate_checkbox_tag(OP_RELATED_ENTRY_CARD_POST_DATE_OR_UPDATE_VISIBLE , is_related_entry_card_post_date_or_update_visible(), __( '更新日が存在しない場合は投稿日を表示', THEME_NAME ));
-            echo '</div>';
+            echo '<br>';
+
+            // //表示しないとき
+            // $is_not_allowed = is_related_entry_card_post_date_visible();
+            // $is_not_allowed = $is_not_allowed || !is_related_entry_card_post_update_visible();
+            // echo '<div class="indent'.get_not_allowed_form_class(!$is_not_allowed, true).'">';
+            //   generate_checkbox_tag(OP_RELATED_ENTRY_CARD_POST_DATE_OR_UPDATE_VISIBLE , is_related_entry_card_post_date_or_update_visible(), __( '更新日が存在しない場合は投稿日を表示', THEME_NAME ));
+            // echo '</div>';
 
             generate_checkbox_tag(OP_RELATED_ENTRY_CARD_POST_UPDATE_VISIBLE , is_related_entry_card_post_update_visible(), __( '更新日の表示', THEME_NAME ));
             echo '<br>';
