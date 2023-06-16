93680322e7588fe5305f82a55d0bca3a31266b27
vic322 <vic322.tech@gmail.com>
2023-06-17 01:30:27 +0900

    Change SelectControl to RangeControl

--- blocks/src/block-extension/style-extension/margin.js
+++ blocks/src/block-extension/style-extension/margin.js
@@ -2,7 +2,7 @@ import { addFilter } from '@wordpress/hooks';
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { Fragment } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, SelectControl } from '@wordpress/components';
+import { PanelBody, Button, RangeControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 import classnames from 'classnames';
@@ -24,6 +24,21 @@ addFilter(
   addCustomAttributes
 );
 
+const marks = [
+  {
+    value: 0,
+    label: '0em',
+  },
+  {
+    value: 10,
+    label: '10em',
+  },
+  {
+    value: 20,
+    label: '20em',
+  },
+];
+
 // Edit拡張
 const addCustomEdit = createHigherOrderComponent( ( BlockEdit ) => {
   return ( props ) => {
@@ -33,9 +48,9 @@ const addCustomEdit = createHigherOrderComponent( ( BlockEdit ) => {
 
       var title = '';
       if ( props.name.includes( BLOCK_SERIES ) ) {
-        title = __( 'ブロック下の余白量', THEME_NAME );
+        title = __( '余白', THEME_NAME );
       } else {
-        title = __( '[C] ブロック下の余白量', THEME_NAME );
+        title = __( '[C] 余白', THEME_NAME );
       }
 
       return (
@@ -44,40 +59,33 @@ const addCustomEdit = createHigherOrderComponent( ( BlockEdit ) => {
           { isSelected && (
             <InspectorControls>
               <PanelBody title={ title } initialOpen={ false }>
-                <SelectControl
+                { /* <div class="__clearBtn">
+                  <button
+                    type="button"
+                    class="components-button is-small"
+                    onClick={ () => setAttributes( { extraBottomMargin: '' } ) }
+                  >
+                    <span class="dashicons dashicons-editor-removeformatting"></span>
+                    { __( 'ブロック下部余白をクリア', THEME_NAME ) }
+                  </button>
+                </div> */ }
+                <RangeControl
+                  label={ __( 'ブロック下部', THEME_NAME ) }
                   value={ extraBottomMargin }
-                  options={ [
-                    {
-                      label: __( '-- 余白量を選択して下さい --', THEME_NAME ),
-                      value: '',
-                      disabled: true,
-                    },
-                    { label: '0em', value: '0em' },
-                    { label: '1em', value: '1em' },
-                    { label: '2em', value: '2em' },
-                    { label: '3em', value: '3em' },
-                    { label: '4em', value: '4em' },
-                    { label: '5em', value: '5em' },
-                    { label: '6em', value: '6em' },
-                    { label: '7em', value: '7em' },
-                    { label: '8em', value: '8em' },
-                    { label: '9em', value: '9em' },
-                    { label: '10em', value: '10em' },
-                    { label: '11em', value: '11em' },
-                    { label: '12em', value: '12em' },
-                    { label: '13em', value: '13em' },
-                    { label: '14em', value: '14em' },
-                    { label: '15em', value: '15em' },
-                    { label: '16em', value: '16em' },
-                    { label: '17em', value: '17em' },
-                    { label: '18em', value: '18em' },
-                    { label: '19em', value: '19em' },
-                    { label: '20em', value: '20em' },
-                  ] }
-                  onChange={ ( value ) =>
-                    setAttributes( { extraBottomMargin: value } )
-                  }
-                  __nextHasNoMarginBottom
+                  onChange={ ( value ) => {
+                    if ( value === -1 ) {
+                      setAttributes( { extraBottomMargin: '' } );
+                    } else {
+                      setAttributes( { extraBottomMargin: value + 'em' } );
+                    }
+                  } }
+                  min={ -1 }
+                  max={ 20 }
+                  step={ 0.1 }
+                  allowReset={ true }
+                  resetFallbackValue={ -1 }
+                  initialPosition={ 0 }
+                  marks={ marks }
                 />
               </PanelBody>
             </InspectorControls>
