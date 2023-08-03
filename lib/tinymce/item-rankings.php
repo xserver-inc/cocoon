601ae96fd25376e3551a6c6efbe576cf7d042109
yhira <yhira@ma.mrr.jp>
2023-08-03 23:56:32 +0900

    ランキング機能のスクリプトをエディター画面上だけで読み込むように修正

--- lib/tinymce/item-rankings.php
+++ lib/tinymce/item-rankings.php
@@ -8,7 +8,7 @@
 if ( !defined( 'ABSPATH' ) ) exit;
 
 //テーブル内にレコードが存在するとき
-if (!is_item_rankings_record_empty()) {
+if (!is_item_rankings_record_empty() && is_admin_post_page()) {
   add_action('admin_init', 'add_item_rankings_dropdown');
   add_action('admin_head', 'generate_item_rankings');
 }
