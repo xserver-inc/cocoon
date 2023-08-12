e17bb184918b4772cf77e77cec76eff1fa716860
yhira <yhira@ma.mrr.jp>
2023-08-12 16:40:28 +0900

    スニペットの抜粋関数（get_content_excerpt）に半角スペースやタブ除去が含まれていなかった不具合修正

--- functions.php
+++ functions.php
@@ -63,7 +63,7 @@ function get_content_excerpt($content, $length = 120){
   $content = str_replace('&nbsp;', '', $content);//特殊文字の削除（今回はスペースのみ）
   $content = preg_replace('/\[.+?\]/i', '', $content); //ショートコードを取り除く
   $content = preg_replace(URL_REG, '', $content); //URLを取り除く
-  $content = str_replace(PHP_EOL, '', $content); //改行を取り除く
+  $content = preg_replace('/\s/','',$content); //半角スペースやタブ、改行を取り除く
   $content = html_entity_decode($content); //HTML エンティティを対応する文字に変換する
 
   //$lengthが整数じゃなかった場合の処理
