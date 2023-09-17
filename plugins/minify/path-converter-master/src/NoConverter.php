b79b2ba3b77cebe677a476f360a900e7c04910a7
yhira <yhira@ma.mrr.jp>
2023-09-17 13:56:52 +0900

    CSSの縮小化ライブラリをphp-html-css-js-minifierからMatthiasMullie\Minifyに変更

---
+++ plugins/minify/path-converter-master/src/NoConverter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MatthiasMullie\PathConverter;
+
+/**
+ * Don't convert paths.
+ *
+ * Please report bugs on https://github.com/matthiasmullie/path-converter/issues
+ *
+ * @author Matthias Mullie <pathconverter@mullie.eu>
+ * @copyright Copyright (c) 2015, Matthias Mullie. All rights reserved
+ * @license MIT License
+ */
+class NoConverter implements ConverterInterface
+{
+    public function convert($path)
+    {
+        return $path;
+    }
+}
