f29822e150bf67cd2c9bf91cfea225773575603b
yhira <yhira@ma.mrr.jp>
2023-10-18 18:41:50 +0900

    ブロックエディターと公開ページ上でカテゴリーブロックのカテゴリ数の表記が異なる不具合修正

--- lib/widget.php
+++ lib/widget.php
@@ -27,10 +27,6 @@ add_filter( 'wp_list_categories', 'remove_post_count_parentheses', 10, 2 );
 add_filter( 'get_archives_link',  'remove_post_count_parentheses', 10, 2 );
 if ( !function_exists( 'remove_post_count_parentheses' ) ):
 function remove_post_count_parentheses( $output, $var ) {
-  //カテゴリーの場合（wp_list_categoriesは配列を返す、get_archives_linkはURLを返す）
-  if (is_array($var)) {
-    $output = str_replace('<a href=','<a class="cf" href=',$output);
-  }
   $output = preg_replace('/<\/a>.*\(([0-9,]+)\)/','<span class="post-count">$1</span></a>',$output);
   return $output;
 }
