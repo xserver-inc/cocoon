5090c375064bff385bfb13fa7b2a2e828fe7cede
xs-khosomi <k.hosomi@gsuite.xserver.co.jp>
2023-10-20 14:42:14 +0900

    生成されたビルドファイルをアップ

---
+++ blocks/dist/blocks.build.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => '3a2810a2cc15209f2ea3');
