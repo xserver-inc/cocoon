8375b4c476cdd3ef7eec8edead6ccdd16b8b22ca
yhira <yhira@ma.mrr.jp>
2024-02-20 01:07:21 +0900

    外部ブログカードエンコードを一旦mb_convert_encodingに戻す

--- lib/open-graph.php
+++ lib/open-graph.php
@@ -123,15 +123,15 @@ class OpenGraphGetter implements Iterator
     //   mb_language("Japanese");
     // }
 
-    // mb_encode_numericentityを使用するための変換オプションを設定
-    $convmap = array(
-      0x80, 0xFFFF, 0, 0xFFFF
-    );
+    // // mb_encode_numericentityを使用するための変換オプションを設定
+    // $convmap = array(
+    //   0x80, 0xFFFF, 0, 0xFFFF
+    // );
 
-    // UTF-8エンコーディングを指定して、数値エンティティに変換
-    $HTML = @mb_encode_numericentity($HTML, $convmap, 'UTF-8');
+    // // UTF-8エンコーディングを指定して、数値エンティティに変換
+    // $HTML = @mb_encode_numericentity($HTML, $convmap, 'UTF-8');
 
-    // $HTML = @mb_convert_encoding($HTML,'HTML-ENTITIES', 'ASCII, JIS, UTF-8, EUC-JP, SJIS');
+    $HTML = @mb_convert_encoding($HTML,'HTML-ENTITIES', 'ASCII, JIS, UTF-8, EUC-JP, SJIS');
     //$HTML = mb_convert_encoding($HTML,'HTML-ENTITIES', 'ASCII, JIS, UTF-8');
     if (!$HTML) {
       return false;
