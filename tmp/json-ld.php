01af9e5cfc468ffa44f48976ae0f59d109c22129
yhira <yhira@ma.mrr.jp>
2024-03-16 12:12:51 +0900

    publisher設定表示部分を削除

--- tmp/json-ld.php
+++ tmp/json-ld.php
@@ -62,44 +62,6 @@ if ($image_url && file_exists($image_file)) {
     "url": "<?php echo esc_url(get_the_auther_profile_page_url()); ?>"<?php // 投稿者URL ?>
 
   },
-  "publisher": {
-    "@type": "Organization",
-    "name": "<?php echo esc_attr(get_bloginfo( 'name' )); ?>",<?php // サイト名 ?>
-
-<?php //後で修正
-$image_url = get_amp_logo_image_url();
-$image_file = url_to_local($image_url);
-if ($image_url && file_exists($image_file)) {//ロゴ画像がある場合
-  $size = get_image_width_and_height($image_url);
-  $width = $size ? $size['width'] : 600;
-  $height = $size ? $size['height'] : 60;
-  // //ロゴの幅が大きすぎる場合は仕様の範囲内（600px）にする
-  // if ($width > 600) {
-  //   $height = round($height * (600/$width));
-  //   $width = 600;
-  // }
-  // //ロゴの高さが大きすぎる場合は仕様の範囲内（60px）にする
-  // if ($height > 60) {
-  //   $width = round($width * (60/$height));
-  //   $height = 60;
-  // }
-  $sizes = calc_publisher_image_sizes($width, $height);
-  $width = $sizes ? $sizes['width'] : 600;
-  $height = $sizes ? $sizes['height'] : 60;
-} else {//ロゴ画像がない場合
-  $image_url = get_template_directory_uri().'/images/no-amp-logo.png';
-  $width = 206;
-  $height = 60;
-}?>
-    "logo": {
-      "@type": "ImageObject",
-      "url": "<?php echo esc_url($image_url); ?>",<?php  // ロゴ画像 ?>
-
-      "width": <?php echo $width; ?>,
-      "height": <?php echo $height; ?>
-
-    }
-  },
   "description": "<?php
     $description = get_meta_description_text();
     $description = str_replace('\\', '', $description);
