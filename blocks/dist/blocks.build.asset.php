87f76bffecd1f358d955f76c38a4390b46e74d81
xs-khosomi <k.hosomi@gsuite.xserver.co.jp>
2024-03-19 12:13:37 +0900

    ビルドファイル作成

--- blocks/dist/blocks.build.asset.php
+++ blocks/dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => 'd34c8fb68b9d7d6a86b4');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => '5c28077d12472591d6dc');
