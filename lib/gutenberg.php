fccc2dc96549c2298f597c1051132258273a62d0
yhira <yhira@ma.mrr.jp>
2024-04-09 14:23:28 +0900

    FAQブロック関連のスタイルを同じファイルにまとめ書き

--- lib/gutenberg.php
+++ lib/gutenberg.php
@@ -610,47 +610,6 @@ html .body .btn-wrap.has-larger-font-size > a {
 .cocoon-block-toggle.has-border-color.not-nested-style > .toggle-button::before {
     color: var(--cocoon-white-color);
 }
-<?php //FAQ ?>
-.cocoon-block-faq.not-nested-style {
-    background-color: var(--cocoon-custom-background-color) !important;
-    border-color: var(--cocoon-custom-border-color) !important;
-    color: var(--cocoon-custom-text-color);
-}
-.cocoon-block-faq.is-style-square.not-nested-style > .faq > * > .faq-item-label{
-    color: #fff !important;
-    background-color: var(--cocoon-custom-question-color);
-    font-size: 20px;
-    padding: 5px 8px;
-    height: 100%;
-}
-.cocoon-block-faq.is-style-square.not-nested-style > .faq > * > .faq-answer-label{
-    background-color: var(--cocoon-custom-answer-color);
-}
-.cocoon-block-faq.is-style-accordion.not-nested-style > .faq > .faq-question {
-    padding: .5em;
-    margin: 0;
-    cursor: pointer;
-    position: relative;
-    background-color: #e8ecef;
-    border-color: var(--cocoon-three-d-border-colors);
-}
-.cocoon-block-faq.is-style-accordion.not-nested-style > .faq > .faq-question::before {
-    content: "+";
-    position: absolute;
-    right: 1em;
-    opacity: 0.5;
-}
-.cocoon-block-faq.is-style-accordion.not-nested-style > .faq > .faq-question.active::before {
-    content: "-";
-}
-.cocoon-block-faq.is-style-accordion.not-nested-style > .faq > .faq-answer {
-    padding: 1em .5em;
-}
-.cocoon-block-faq.is-style-accordion.has-border-color.not-nested-style > .faq > .faq-question {
-    color: var(--cocoon-white-color);
-    border: none;
-    background-color: var(--cocoon-custom-border-color);
-}
 <?php //タイムライン ?>
 .cocoon-block-timeline.not-nested-style {
     background-color: var(--cocoon-custom-background-color)!important;
