8ac2473cf7f0a304b74d1eddc8e95e37f51fd9bb
yhira <yhira@ma.mrr.jp>
2024-05-27 22:50:19 +0900

    メイド・イン・ヘブンスキンのアップデート v2.7.0

--- skins/skin-made-in-heaven/assets/js/block.js
+++ skins/skin-made-in-heaven/assets/js/block.js
@@ -42,3 +42,33 @@ wp.blocks.registerBlockStyle('cocoon-blocks/caption-box-1', {
   name: 'accordion',
   label: 'アコーディオン'
 });
+
+wp.blocks.registerBlockStyle('cocoon-blocks/new-list', {
+  name: '2-columns',
+  label: '2カラム'
+});
+
+wp.blocks.registerBlockStyle('cocoon-blocks/new-list', {
+  name: '3-columns',
+  label: '3カラム'
+});
+
+wp.blocks.registerBlockStyle('cocoon-blocks/popular-list', {
+  name: '2-columns',
+  label: '2カラム'
+});
+
+wp.blocks.registerBlockStyle('cocoon-blocks/popular-list', {
+  name: '3-columns',
+  label: '3カラム'
+});
+
+wp.blocks.registerBlockStyle('cocoon-blocks/navicard', {
+  name: '2-columns',
+  label: '2カラム'
+});
+
+wp.blocks.registerBlockStyle('cocoon-blocks/navicard', {
+  name: '3-columns',
+  label: '3カラム'
+});
