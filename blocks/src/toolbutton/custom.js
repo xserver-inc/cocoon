fae7f69868f573d53c6e8de143b60934b302d911
yhira <yhira@ma.mrr.jp>
2024-06-15 17:28:13 +0900

    ブロックエディターツールバーの「文字」オプションに「カスタムテキスト」メニューを追加

---
+++ blocks/src/toolbutton/custom.js
@@ -0,0 +1,47 @@
+/**
+ * Cocoon Blocks
+ * @author: yhira
+ * @link: https://wp-cocoon.com/
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+
+import { THEME_NAME, LetterToolbarButton } from '../helpers.js';
+import { Fragment } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { registerFormatType, toggleFormat } from '@wordpress/rich-text';
+import { ToolbarButton } from '@wordpress/components';
+import { Icon, pencil } from '@wordpress/icons';
+
+console.log(gbSettings.customTextCount);
+//表示数
+const isCustomTexVisible = Number(
+  gbSettings.customTextCount ? gbSettings.customTextCount : 0
+);
+if (isCustomTexVisible) {
+  Array.from({ length: gbSettings.customTextCount }, (_, i) => i + 1).map(number => {
+    let name = 'cocoon-custom-text-' + number;
+    let formatTypeName = 'cocoon-blocks/' + name;
+    let caption = __( 'カスタムテキスト', THEME_NAME ) + ' ' + number;
+    registerFormatType( formatTypeName, {
+      title: caption,
+      tagName: 'span',
+      className: name,
+      edit( { isActive, value, onChange } ) {
+        const onToggle = () =>
+          onChange( toggleFormat( value, { type: formatTypeName } ) );
+
+        return (
+          <Fragment>
+            <LetterToolbarButton
+              icon={ <Icon icon={ pencil } size={ 32 } /> }
+              title={ <span className={name}>{ caption }</span> }
+              onClick={ onToggle }
+              isActive={ isActive }
+            />
+          </Fragment>
+        );
+      },
+    } );
+  })
+}
+
