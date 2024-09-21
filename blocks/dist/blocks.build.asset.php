6d15784bcc3acb088d24cbfea6e33bdf927de508
yhira <yhira@ma.mrr.jp>
2024-09-21 12:27:13 +0900

    レーダーチャートブロックのチャートサイズ変更機能の追加

--- blocks/dist/blocks.build.asset.php
+++ blocks/dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => '56d9b9b40187ffa00133');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => '797fc90c28b57bc56fcc');
--- blocks/src/block/radar/edit.js
+++ blocks/src/block/radar/edit.js
@@ -1,107 +1,127 @@
 import { THEME_NAME, hexToRgba } from '../../helpers';
-import { useRef, useEffect } from '@wordpress/element';
+import { useRef, useEffect, useState } from '@wordpress/element';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { store as blockEditorStore, InspectorControls, PanelColorSettings } from '@wordpress/block-editor';
-import { PanelBody, TextControl } from '@wordpress/components';
+import {
+  store as blockEditorStore,
+  InspectorControls,
+  PanelColorSettings,
+} from '@wordpress/block-editor';
+import {
+  PanelBody,
+  TextControl,
+  RangeControl,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+
+const MIN_SIZE = 200;
+const MAX_SIZE = 1920;
+const DEFAULT_COLOR = 'rgba(255, 99, 132, 0.2)';
+const DEFAULT_BORDER_COLOR = 'rgba(255, 99, 132, 0.9)';
 
 export default function edit( props ) {
   const { attributes, setAttributes, clientId } = props;
-  const { labels, data, chartId, chartColor } = attributes; // chartColorを追加
+  const { imageSize, labels, data, chartId, chartColor } = attributes;
   const canvasRef = useRef(null);
-
-  let chartInstance = null;
+  const chartInstanceRef = useRef(null); // useRefで管理
 
   const { isSelected } = useSelect((select) => ({
-  isSelected: select(blockEditorStore).isBlockSelected(clientId),
+    isSelected: select(blockEditorStore).isBlockSelected(clientId),
   }));
 
   const { selectBlock } = useDispatch(blockEditorStore);
 
   useEffect(() => {
-  if (!chartId) {
-    const newChartId = `radarChart-${Math.random().toString(36).substr(2, 9)}`;
-    setAttributes({ chartId: newChartId });
-  }
+    if (!chartId) {
+      const newChartId = `radarChart-${Math.random().toString(36).substr(2, 9)}`;
+      setAttributes({ chartId: newChartId });
+    }
   }, []);
 
-  const renderChart = () => {
-  const ctx = canvasRef.current.getContext('2d');
-
-  if (chartInstance) {
-    chartInstance.destroy();
-  }
-
-  chartInstance = new Chart(ctx, {
-    type: 'radar',
-    data: {
-    labels: labels,
-    datasets: [{
-      data: data,
-      backgroundColor: chartColor ? hexToRgba(chartColor, 0.2) : 'rgba(255, 99, 132, 0.2)', // 色をchartColorに変更
-      borderColor: chartColor ? hexToRgba(chartColor, 0.9) : 'rgba(255, 99, 132, 0.9)', // 色をchartColorに変更
-      borderWidth: 1
-    }]
-    },
-    options: {
-    scales: {
-      r: {
-      min: 0,
-      max: 5,
-      ticks: {
-        stepSize: 1,
-      },
-      }
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-      display: false
-      },
-    },
-    },
-  });
-  };
-
-  useEffect(() => {
-  if (!chartId) return;
-
-  renderChart();
-
-  const handleResize = () => {
-    renderChart();
-  };
-
-  window.addEventListener('resize', handleResize);
-
-  return () => {
-    window.removeEventListener('resize', handleResize);
-    if (chartInstance) {
-    chartInstance.destroy();
+  // チャートの破棄関数
+  const destroyChart = () => {
+    if (chartInstanceRef.current) {
+      chartInstanceRef.current.destroy();
+      chartInstanceRef.current = null;
     }
   };
-  }, [labels, data, chartColor, chartId]); // chartColorも依存配列に追加
 
-  // Add click event to canvas to focus/select block
+  const renderChart = () => {
+    const ctx = canvasRef.current.getContext('2d');
+
+    destroyChart(); // 既存のチャートがあれば破棄
+
+    // 新しいチャートを生成
+    chartInstanceRef.current = new Chart(ctx, {
+      type: 'radar',
+      data: {
+        labels: labels,
+        datasets: [{
+          data: data,
+          backgroundColor: chartColor ? hexToRgba(chartColor, 0.2) : DEFAULT_COLOR,
+          borderColor: chartColor ? hexToRgba(chartColor, 0.9) : DEFAULT_BORDER_COLOR,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          r: {
+            min: 0,
+            max: 5,
+            ticks: {
+              stepSize: 1,
+            },
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false
+          },
+        },
+      },
+    });
+  };
+
+  // チャートのレンダリングとサイズ調整
   useEffect(() => {
-  if (canvasRef.current) {
-    const handleClick = () => {
-    selectBlock(clientId);
-    };
-    canvasRef.current.addEventListener('click', handleClick);
+    if (canvasRef.current && imageSize) {
+      canvasRef.current.width = imageSize;
+      canvasRef.current.height = imageSize;
+      renderChart();
+    }
 
     return () => {
-    canvasRef.current.removeEventListener('click', handleClick);
+      destroyChart(); // クリーンアップ時にチャートを破棄
     };
-  }
+  }, [imageSize, labels, data, chartColor, chartId]);
+
+  // マウスクリックでブロックを選択（フォーカス）する
+  useEffect(() => {
+    if (canvasRef.current) {
+      const handleClick = () => {
+        selectBlock(clientId);
+      };
+      canvasRef.current.addEventListener('click', handleClick);
+
+      return () => {
+        canvasRef.current.removeEventListener('click', handleClick);
+      };
+    }
   }, [canvasRef.current]);
 
   return (
     <div className="radar-chart-block">
-      <canvas ref={canvasRef} id={chartId} width="400" height="400" tabindex="0" />
+      <canvas ref={canvasRef} id={chartId} width={imageSize} height={imageSize} tabIndex="0" />
       <InspectorControls>
         <PanelBody title={ __( 'チャート設定', THEME_NAME ) }>
+          <RangeControl
+            label={ __( 'チャートサイズ', THEME_NAME ) }
+            value={ imageSize }
+            onChange={ ( value ) => setAttributes({ imageSize: value }) }
+            min={ MIN_SIZE }
+            max={ MAX_SIZE }
+          />
           <TextControl
             label={ __( '項目', THEME_NAME ) + __( '（カンマ区切り）', THEME_NAME ) }
             value={labels.join(', ')}
@@ -111,27 +131,20 @@ export default function edit( props ) {
             label={ __( '値', THEME_NAME ) + __( '（カンマ区切り）', THEME_NAME ) }
             value={data.join(', ')}
             onChange={(value) => {
-              const newData = value.split(',').map(d => {
-                let num = parseFloat(d.trim());
-                num = Math.max(0, Math.min(5, num));
-                if (isNaN(num)) {
-                num = '';
-                }
-                return num;
-              });
+              const newData = value.split(',').map(d => parseFloat(d.trim()) || 0);
               setAttributes({ data: newData });
             }}
           />
         </PanelBody>
         <PanelColorSettings
           title={ __( '色設定', THEME_NAME ) }
-          colorSettings={ [
+          colorSettings={[
             {
               label: __( 'チャートカラー', THEME_NAME ),
-              onChange: ( newColor ) => setAttributes( { chartColor: newColor } ) ,
+              onChange: ( newColor ) => setAttributes({ chartColor: newColor }),
               value: chartColor,
             },
-          ] }
+          ]}
         />
       </InspectorControls>
     </div>
