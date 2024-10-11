35abbcbeae190faf352b528b2885066cf41871e3
yhira <yhira@ma.mrr.jp>
2024-10-11 21:58:26 +0900

    Gutenberg 19.4プラグインをインストールした時点でタブブロックの子ブロックであるタブアイテムブロックがブロック挿入ツールに表示される不具合修正

--- blocks/dist/blocks.build.asset.php
+++ blocks/dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => '5cc2e68e3f34f9892cc4');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => 'ad3fac42a91b82594606');
