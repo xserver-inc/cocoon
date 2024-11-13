edd1162fe2eacf9235646a6df815477406e8f818
yhira <yhira@ma.mrr.jp>
2024-11-13 18:56:10 +0900

    //「Function _load_textdomain_just_in_time was called incorrectly. Translation loading for the health-check domain was triggered too early. This is usually an indicator for some code in the plugin or theme running too early. Translations should be loaded at the init action or later. (This message was added in version 6.7.0.)」警告対策

--- lib/language.php
+++ lib/language.php
@@ -7,45 +7,23 @@
  */
 if ( !defined( 'ABSPATH' ) ) exit;
 
-global $locale;
+//「Function _load_textdomain_just_in_time was called incorrectly. Translation loading for the health-check domain was triggered too early. This is usually an indicator for some code in the plugin or theme running too early. Translations should be loaded at the init action or later. (This message was added in version 6.7.0.)」警告対策
+add_action( 'after_setup_theme', function (){
+  global $locale;
 
-// if (preg_match('/^ja/', $locale)){
-//   //日本語環境では何もしない
-// } elseif (preg_match('/^en/', $locale)) {
-//   //言語の最初の文字がenだったら全てen.moを呼び出す（英語）
-//   $locale = 'en';
-// }
-// elseif (preg_match('/^de/', $locale)) {
-//   //言語の最初の文字がdeだったら全てde.moを呼び出す（ドイツ語）
-//   $locale = 'de';
-// }
-// elseif (preg_match('/^fr/', $locale)) {
-//   //言語の最初の文字がfrだったら全てfr.moを呼び出す（フランス語）
-//   $locale = 'fr';
-// }
-// elseif (preg_match('/^pt/', $locale)) {
-//   //言語の最初の文字がptだったら全てpt.moを呼び出す（ポルトガル語）
-//   $locale = 'pt';
-// }
-// elseif (preg_match('/^ko_/', $locale)) {
-//   //言語の最初の文字がkoだったら全てko.moを呼び出す（韓国語）
-//   $locale = 'ko';
-// }
+  //親テーマの翻訳ディレクトリ
+  $language_dir = get_template_directory() . '/languages';
 
-//親テーマの翻訳ディレクトリ
-$language_dir = get_template_directory() . '/languages';
-//load_theme_textdomain( THEME_NAME, $language_dir );
+  //子テーマの翻訳ディレクトリ
+  $child_language_dir = get_stylesheet_directory() . '/languages';
+  //子テーマの翻訳ファイル
+  $child_language_file = $child_language_dir.'/'.$locale.'.mo';
 
-//子テーマの翻訳ディレクトリ
-$child_language_dir = get_stylesheet_directory() . '/languages';
-//子テーマの翻訳ファイル
-$child_language_file = $child_language_dir.'/'.$locale.'.mo';
-// _v(file_exists($child_language_dir));
-// _v(file_exists($child_language_file));
-// _v(($child_language_file));
-//子テーマを利用していて子テーマにファイルがある場合
-if (is_child_theme() && file_exists($child_language_file)) {
-  load_theme_textdomain( THEME_NAME, $child_language_dir );
-} else {
-  load_theme_textdomain( THEME_NAME, $language_dir );
-}
+  //子テーマを利用していて子テーマにファイルがある場合
+  if (is_child_theme() && file_exists($child_language_file)) {
+    load_theme_textdomain( THEME_NAME, $child_language_dir );
+  } else {
+    load_theme_textdomain( THEME_NAME, $language_dir );
+  }
+} );
+
