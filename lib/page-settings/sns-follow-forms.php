5edf839029d03b7dedba1360b629471712d82520
yhira <yhira@ma.mrr.jp>
2024-12-01 01:08:57 +0900

    フォローボタン表示の説明文を変更

--- lib/page-settings/sns-follow-forms.php
+++ lib/page-settings/sns-follow-forms.php
@@ -40,7 +40,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
           <td>
             <?php
             generate_checkbox_tag( OP_SNS_FOLLOW_BUTTONS_VISIBLE, is_sns_follow_buttons_visible(), __( 'フォローボタンを表示する', THEME_NAME ));
-            generate_tips_tag(__( 'フォローボタンの表示を切り替えます。', THEME_NAME ));
+            generate_tips_tag(__( 'フォローボタンの表示をまとめて切り替えます。', THEME_NAME ));
             ?>
           </td>
         </tr>
