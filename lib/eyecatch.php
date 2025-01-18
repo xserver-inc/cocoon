c6fc587928dba95c996c5baa96a08139bb8eeaf9
yhira <yhira@ma.mrr.jp>
2025-01-19 04:40:01 +0900

    タイトルアイキャッチのデバッグモード終了

--- lib/eyecatch.php
+++ lib/eyecatch.php
@@ -66,8 +66,8 @@ function generate_dynamic_featured_image($post_id) {
   $post_title_hash = md5($post_title . $avatar_url . $author_name . $width . 'x' . $height);
   $new_image_path = trailingslashit($upload_path) . 'featured-image-' . $post_id . '-' . $width . 'x' . $height . '-' . $post_title_hash . '.png';
   // デバッグ用のファイル名
-  $current_time = current_time('YmdHis');
-  $new_image_path = trailingslashit($upload_path) . 'featured-image-' . $post_id . '-' . $width . 'x' . $height . '-' . $current_time . '-' . $post_title_hash . '.png';
+  // $current_time = current_time('YmdHis');
+  // $new_image_path = trailingslashit($upload_path) . 'featured-image-' . $post_id . '-' . $width . 'x' . $height . '-' . $current_time . '-' . $post_title_hash . '.png';
 
   // すでにアイキャッチ画像が設定されている場合は処理を終了
   $current_thumbnail_id = get_post_thumbnail_id($post_id);
