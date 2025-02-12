4bd674dfe24bd5d3dcda68ee7d89bc2997156834
yhira <yhira@ma.mrr.jp>
2025-02-13 00:24:42 +0900

    categories-tags.phpのテンプレートタグコードを元に戻す

--- functions.php
+++ functions.php
@@ -530,3 +530,48 @@ endif;
 
 //sizes="auto"対策
 add_filter ( 'wp_img_tag_add_auto_sizes' ,  '__return_false' );
+
+
+add_filter('cocoon_part__tmp/categories-tags', function($content) {
+  $post_type = get_post_type();
+
+  // カスタム投稿の場合
+  if ($post_type !== 'post') {
+
+    // タクソノミーを取得
+    $taxonomies = get_object_taxonomies($post_type);
+    // タームを取得
+
+    $args = array(
+      'order'   => 'ASC',
+      'orderby' => 'name',
+    );
+    $terms = wp_get_post_terms(get_the_ID(), $taxonomies, $args);
+
+    // 階層的なタクソノミーか判定
+    if (is_taxonomy_hierarchical($taxonomies[0])) {
+      $cat_tag = 'cat';
+      $icon = '<span class="fa fa-folder cat-icon tax-icon" aria-hidden="true"></span>';
+    } else {
+      $cat_tag = 'tag';
+      $icon = '<span class="fa fa-tag tag-icon tax-icon" aria-hidden="true"></span>';
+    }
+
+    if ($terms && !is_wp_error($terms)) {
+      $html = '';
+      foreach ($terms as $term) {
+        $html .= '<a class="' . $cat_tag . '-link ' . $cat_tag . '-link-' . $term->term_id . '" href="' . esc_url(get_term_link($term->slug, $term->taxonomy)) . '">' . $icon . esc_html($term->name) . '</a>';
+      }
+
+      // 階層的なタクソノミーはカテゴリーとする
+      if ($cat_tag == 'cat') {
+        $html = '<div class="entry-categories">' . $html . '</div>';
+      } else {
+        $html = '<div class="entry-tags">' . $html . '</div>';
+      }
+      $content = '<div class="entry-categories-tags ' . esc_attr(get_additional_categories_tags_area_classes() ?: '') . '">' . $html . '</div>';
+    }
+  }
+
+  return $content;
+});
--- tmp/categories-tags.php
+++ tmp/categories-tags.php
@@ -5,48 +5,11 @@
  * @link: https://wp-cocoon.com/
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( !defined( 'ABSPATH' ) ) exit; ?>
 
-add_filter('cocoon_part__tmp/categories-tags', function($content) {
-  $post_type = get_post_type();
-
-  // カスタム投稿の場合
-  if ($post_type !== 'post') {
-
-    // タクソノミーを取得
-    $taxonomies = get_object_taxonomies($post_type);
-    // タームを取得
-
-    $args = array(
-      'order'   => 'ASC',
-      'orderby' => 'name',
-    );
-    $terms = wp_get_post_terms(get_the_ID(), $taxonomies, $args);
-
-    // 階層的なタクソノミーか判定
-    if (is_taxonomy_hierarchical($taxonomies[0])) {
-      $cat_tag = 'cat';
-      $icon = '<span class="fa fa-folder cat-icon tax-icon" aria-hidden="true"></span>';
-    } else {
-      $cat_tag = 'tag';
-      $icon = '<span class="fa fa-tag tag-icon tax-icon" aria-hidden="true"></span>';
-    }
-
-    if ($terms && !is_wp_error($terms)) {
-      $html = '';
-      foreach ($terms as $term) {
-        $html .= '<a class="' . $cat_tag . '-link ' . $cat_tag . '-link-' . $term->term_id . '" href="' . esc_url(get_term_link($term->slug, $term->taxonomy)) . '">' . $icon . esc_html($term->name) . '</a>';
-      }
-
-      // 階層的なタクソノミーはカテゴリーとする
-      if ($cat_tag == 'cat') {
-        $html = '<div class="entry-categories">' . $html . '</div>';
-      } else {
-        $html = '<div class="entry-tags">' . $html . '</div>';
-      }
-      $content = '<div class="entry-categories-tags ' . esc_attr(get_additional_categories_tags_area_classes() ?: '') . '">' . $html . '</div>';
-    }
-  }
-
-  return $content;
-});
+<div class="entry-categories-tags<?php echo get_additional_categories_tags_area_classes(); ?>">
+  <div class="entry-categories"><?php the_category_links() ?></div>
+  <?php if (get_the_tags()): ?>
+  <div class="entry-tags"><?php the_tag_links() ?></div>
+  <?php endif; ?>
+</div>
