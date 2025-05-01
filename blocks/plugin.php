fe4cb79e82f18b9dfdb4baa6e4bff053d5e7f212
わいひら <yhira@ma.mrr.jp>
2025-05-01 17:05:24 +0900

    Cocoon Blocksのプラグインヘッダー情報を削除

--- blocks/plugin.php
+++ blocks/plugin.php
@@ -1,17 +1,4 @@
-<?php
-/**
- * Plugin Name: Cocoon Blocks
- * Plugin URI: https://wp-cocoon.com/cocoon-blocks/
- * Description: Cocoonテーマ専用のブロックエディター（Gutenberg）対応プラグイン
- * Author: わいひら
- * Author URI: https://nelog.jp/
- * Version: 1.0.0
- * Text Domain: cocoon-blocks
- * License: GPL2+
- * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
- *
- * @package CGB
- */
+<?php //Cocoon Blocks
 
 // Exit if accessed directly.
 if ( !defined( 'ABSPATH' ) ) exit;
