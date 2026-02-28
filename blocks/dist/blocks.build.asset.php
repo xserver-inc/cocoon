cec46839617f08f0c027b2737b31f556658adaf2
わいひら <yhira@ma.mrr.jp>
2026-02-28 17:12:52 +0900

    fix: 楽天商品リンクブロックのアイコン下部が欠ける不具合を修正

--- blocks/dist/blocks.build.asset.php
+++ blocks/dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => '571d83b921add0e28ea2');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives', 'wp-rich-text'), 'version' => '36de69572496417420a4');
