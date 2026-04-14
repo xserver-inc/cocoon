672a3501d306c90a7adafe5a87e786d45743d10f
xs-stakazawa <s.takazawa@gsuite.xserver.co.jp>
2026-04-14 20:42:05 +0900

    通知URLの有無によって、通知エリアのnotice-area-wrapの出力がなくなったりしないように調整 (#352)

--- tmp/notice.php
+++ tmp/notice.php
@@ -8,26 +8,25 @@
 if ( !defined( 'ABSPATH' ) ) exit;
 
 $msg = get_notice_area_message();
-if (is_notice_area_visible() && $msg && !is_amp() && apply_filters('notice_area_visible', true)):
-  $url = get_notice_area_url();
- ?>
+$visible = is_notice_area_visible() && $msg && !is_amp() && apply_filters('notice_area_visible', true);
 
-<?php //リンクの開始タグ
-if ($url):
+if ($visible):
+  $url = get_notice_area_url();
   $target = is_notice_link_target_blank() ? ' target="_blank" rel="noopener"' : '';
 ?>
 <div id="notice-area-wrap" class="notice-area-wrap">
-  <a href="<?php echo $url; ?>" id="notice-area-link" class="notice-area-link"<?php echo $target; ?>>
-  <?php endif ?>
+  <?php //リンクの開始タグ
+  if ($url): ?>
+    <a href="<?php echo $url; ?>" id="notice-area-link" class="notice-area-link"<?php echo $target; ?>>
+  <?php endif; ?>
 
-    <div id="notice-area" class="notice-area nt-<?php echo get_notice_type(); ?>">
-      <span class="notice-area-message"><?php echo $msg; ?></span>
-    </div>
+  <div id="notice-area" class="notice-area nt-<?php echo get_notice_type(); ?>">
+    <span class="notice-area-message"><?php echo $msg; ?></span>
+  </div>
 
   <?php //aリンクの閉じタグ
   if ($url): ?>
-  </a>
+    </a>
+  <?php endif; ?>
 </div>
-<?php endif ?>
-
-<?php endif ?>
+<?php endif; ?>
